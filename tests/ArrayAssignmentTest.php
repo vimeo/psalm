2e91b27b92204c15724ca230369de4f4bd98718f
Matthew Brown <github@muglug.com>
2016-09-12 00:02:50 -0400

    Add test for array assignment code

---
+++ tests/ArrayAssignmentTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Psalm\Tests;
+
+use PhpParser;
+use Psalm\Context;
+use PhpParser\ParserFactory;
+use PHPUnit_Framework_TestCase;
+
+class ArrayAssignmentTest extends PHPUnit_Framework_TestCase
+{
+    protected static $_parser;
+
+    public static function setUpBeforeClass()
+    {
+        self::$_parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+
+        $config = \Psalm\Config::getInstance();
+        $config->throw_exception = true;
+        $config->use_docblock_types = true;
+    }
+
+    public function setUp()
+    {
+        \Psalm\Checker\FileChecker::clearCache();
+    }
+
+    public function testImplicitIntArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo[] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<int,string>', (string) $context->vars_in_scope['foo']);
+
+    }
+
+    public function testImplicit2DIntArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo[][] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<int,array<int,string>>', (string) $context->vars_in_scope['foo']);
+
+    }
+
+    public function testImplicit3DIntArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo[][][] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<int,array<int,array<int,string>>>', (string) $context->vars_in_scope['foo']);
+    }
+
+    public function testImplicit4DIntArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo[][][][] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<int,array<int,array<int,array<int,string>>>>', (string) $context->vars_in_scope['foo']);
+    }
+
+    public function testImplicitStringArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo["bar"] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<string,string>', (string) $context->vars_in_scope['foo']);
+
+    }
+
+    public function testImplicit2DStringArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo["bar"]["baz"] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<string,array<string,string>>', (string) $context->vars_in_scope['foo']);
+
+    }
+
+    public function testImplicit3DStringArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo["bar"]["baz"]["bat"] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<string,array<string,array<string,string>>>', (string) $context->vars_in_scope['foo']);
+    }
+
+    public function testImplicit4DStringArrayCreation()
+    {
+        $stmts = self::$_parser->parse('<?php
+        $foo = [];
+        $foo["bar"]["baz"]["bat"]["bap"] = "hello";
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('array<string,array<string,array<string,array<string,string>>>>', (string) $context->vars_in_scope['foo']);
+    }
+}
