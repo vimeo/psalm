e14cadd32e18d86d5e3a3485ec9057bf28a5968e
Matthew Brown <github@muglug.com>
2016-10-03 11:40:42 -0400

    Add tests for elseif reconciliation

--- tests/PropertyTypeTest.php
+++ tests/PropertyTypeTest.php
@@ -2,6 +2,7 @@
 
 namespace Psalm\Tests;
 
+use Psalm\Context;
 use Psalm\Type;
 
 use PhpParser;
@@ -50,5 +51,60 @@ class PropertyTypeTest extends PHPUnit_Framework_TestCase
 
         $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
         $file_checker->check();
+    }
+
+    public function testSharedPropertyInIf()
+    {
+        $stmts = self::$_parser->parse('<?php
+        class A {
+            /** @var int */
+            public $foo;
+        }
+        class B {
+            /** @var string */
+            public $foo;
+        }
+
+        $a = null;
+        $b = null;
+
+        if ($a instanceof A || $a instanceof B) {
+            $b = $a->foo;
+        }
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('null|string|int', (string) $context->vars_in_scope['b']);
+    }
+
+    public function testSharedPropertyInElseIf()
+    {
+        $stmts = self::$_parser->parse('<?php
+        class A {
+            /** @var int */
+            public $foo;
+        }
+        class B {
+            /** @var string */
+            public $foo;
+        }
+
+        $a = null;
+        $b = null;
+
+        if (rand(0, 10) === 4) {
+            // do nothing
+        }
+        elseif ($a instanceof A || $a instanceof B) {
+            $b = $a->foo;
+        }
+        ');
+
+        $file_checker = new \Psalm\Checker\FileChecker('somefile.php', $stmts);
+        $context = new Context('somefile.php');
+        $file_checker->check(true, true, $context);
+        $this->assertEquals('null|string|int', (string) $context->vars_in_scope['b']);
     }
 }
