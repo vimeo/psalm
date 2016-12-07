2bf9f17b5820b8a4697bc4970f0b8ba32c0e0be9
Matthew Brown <github@muglug.com>
2016-12-07 00:38:09 -0500

    Add PHP 5.4-5.5 travis support

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+foreach ([__DIR__ . '/../../../autoload.php', __DIR__ . '/../vendor/autoload.php'] as $file) {
+    if (file_exists($file)) {
+        require $file;
+        break;
+    }
+}
