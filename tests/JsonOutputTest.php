1f7ad3ed96ab5d9df0c72806f1e0aeb1eb2b98a6
Matthew Brown <github@muglug.com>
2016-12-20 17:29:52 +0000

    Suppress test issue

--- tests/JsonOutputTest.php
+++ tests/JsonOutputTest.php
@@ -20,7 +20,6 @@ class JsonOutputTest extends PHPUnit_Framework_TestCase
 
         $config = new TestConfig();
         $config->throw_exception = false;
-        $config->cache_directory = null;
         $config->stop_on_first_error = false;
     }
 
