5f332a5500b69fbcdcc49a496c72a9fc368f4042
Matthew Brown <github@muglug.com>
2016-12-20 17:29:52 +0000

    Suppress test issue

--- tests/TestConfig.php
+++ tests/TestConfig.php
@@ -5,6 +5,9 @@ use Psalm\Config;
 
 class TestConfig extends Config
 {
+    /**
+     * @psalm-suppress InvalidPropertyAssignment because cache_directory isn't strictly nullable
+     */
     public function __construct()
     {
         parent::__construct();
@@ -12,5 +15,6 @@ class TestConfig extends Config
         $this->throw_exception = true;
         $this->use_docblock_types = true;
         $this->totally_typed = true;
+        $this->cache_directory = null;
     }
 }
