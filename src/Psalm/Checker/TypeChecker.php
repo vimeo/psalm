8915a49334a37e92b3154d3941f769f6d60a8850
Matthew Brown <github@muglug.com>
2017-01-15 12:16:58 -0500

    Break away from master (again)

--- src/Psalm/Checker/TypeChecker.php
+++ src/Psalm/Checker/TypeChecker.php
@@ -732,24 +732,6 @@ class TypeChecker
             return true;
         }
 
-        if ($container_type_part instanceof TNamedObject &&
-            strtolower($container_type_part->value) === 'iterable' &&
-            (
-                $input_type_part instanceof TArray ||
-                ($input_type_part instanceof TNamedObject &&
-                    (strtolower($input_type_part->value) === 'traversable' ||
-                        ClassChecker::classExtendsOrImplements(
-                            $project_checker,
-                            $input_type_part->value,
-                            'Traversable'
-                        )
-                    )
-                )
-            )
-        ) {
-            return true;
-        }
-
         if ($container_type_part instanceof TScalar && $input_type_part instanceof Scalar) {
             return true;
         }
