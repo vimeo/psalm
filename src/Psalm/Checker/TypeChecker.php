2132a244893abdc927f4b11c80f6a177465e6c7b
Matt Brown <mattb@vimeo.com>
2017-01-19 14:49:49 -0500

    Fix scalar assignment

--- src/Psalm/Checker/TypeChecker.php
+++ src/Psalm/Checker/TypeChecker.php
@@ -909,7 +909,7 @@ class TypeChecker
             }
         }
 
-        if ($input_type_part instanceof Scalar || $input_type_part instanceof TScalar) {
+        if ($input_type_part instanceof Scalar) {
             if ($container_type_part instanceof Scalar) {
                 $has_scalar_match = true;
             }
