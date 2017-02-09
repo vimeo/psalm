66d98a023f93c46a8529151c5c78c84ccb2ead29
Matthew Brown <github@muglug.com>
2017-02-08 22:45:18 -0500

    Allow false return value

--- src/Psalm/CallMap.php
+++ src/Psalm/CallMap.php
@@ -9009,7 +9009,7 @@ return [
 'stream_is_local' => ['bool', 'stream'=>''],
 'stream_notification_callback' => ['callback', 'notification_code'=>'int', 'severity'=>'int', 'message'=>'string', 'message_code'=>'int', 'bytes_transferred'=>'int', 'bytes_max'=>'int'],
 'stream_resolve_include_path' => ['string', 'filename'=>'string'],
-'stream_select' => ['int', '&read_streams'=>'array', '&write_streams'=>'array|null', '&except_streams'=>'array|null', 'tv_sec'=>'int|null', 'tv_usec='=>'int|null'],
+'stream_select' => ['int|false', '&read_streams'=>'array', '&write_streams'=>'array|null', '&except_streams'=>'array|null', 'tv_sec'=>'int|null', 'tv_usec='=>'int|null'],
 'stream_set_blocking' => ['bool', 'socket'=>'', 'mode'=>'bool'],
 'stream_set_chunk_size' => ['int', 'fp'=>'', 'chunk_size'=>'int'],
 'stream_set_read_buffer' => ['int', 'fp'=>'', 'buffer'=>'int'],
