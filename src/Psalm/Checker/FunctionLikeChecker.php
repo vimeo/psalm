bcbab7e7939732b4418b1fbaf62e062948dd7d07
Matthew Brown <github@muglug.com>
2017-03-20 01:27:17 -0400

    Ignore isVariadic check

--- src/Psalm/Checker/FunctionLikeChecker.php
+++ src/Psalm/Checker/FunctionLikeChecker.php
@@ -1468,8 +1468,7 @@ abstract class FunctionLikeChecker extends SourceChecker implements StatementsSo
             $param_type,
             null,
             $is_optional,
-            $is_nullable,
-            $param->isVariadic()
+            $is_nullable
         );
     }
 
