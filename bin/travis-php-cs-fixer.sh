5569ceb8a58c5bab891847ff0f940f4c78978ef2
SignpostMarv <me@signpostmarv.name>
2017-05-07 00:01:54 +0100

    filter output then perform exact match

--- bin/travis-php-cs-fixer.sh
+++ bin/travis-php-cs-fixer.sh
@@ -1,5 +1,5 @@
 set -e
-if [[ $(git diff --name-only $TRAVIS_COMMIT_RANGE) != *".php_cs.dist"* ]];
+if [[ $(git diff --name-only $TRAVIS_COMMIT_RANGE | grep '\.php_cs.dist$') != ".php_cs.dist" ]];
 then
   echo ".php_cs.dist has not changed";
   ./vendor/bin/php-cs-fixer fix --verbose --allow-risky=yes --dry-run --config ./.php_cs.dist -- $(git diff --name-only $TRAVIS_COMMIT_RANGE | grep '\.php$') .php_cs.dist;
