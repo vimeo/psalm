6b2b80d0bbdfcff2b7217e81caf1c6ae4725b580
Matthew Brown <github@muglug.com>
2017-06-13 00:09:15 -0400

    Assign var to correct context

--- src/Psalm/Checker/Statements/Block/ForeachChecker.php
+++ src/Psalm/Checker/Statements/Block/ForeachChecker.php
@@ -230,7 +230,7 @@ class ForeachChecker
             );
 
             if ($var_comment && $var_comment->var_id) {
-                $context->vars_in_scope[$var_comment->var_id] = $var_comment->type;
+                $foreach_context->vars_in_scope[$var_comment->var_id] = $var_comment->type;
             }
         }
 
