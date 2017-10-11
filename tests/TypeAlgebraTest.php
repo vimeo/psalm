deb489e82da89b8a0f4e451a1b87c0530939b8c3
Matthew Brown <github@muglug.com>
2017-10-10 23:09:19 -0400

    Fix #232 - discard clauses when evaluating catch block

--- tests/TypeAlgebraTest.php
+++ tests/TypeAlgebraTest.php
@@ -363,6 +363,29 @@ class TypeAlgebraTest extends TestCase
                         return $errors;
                     }'
             ],
+            'noParadoxInCatch' => [
+                '<?php
+                    function maybe_returns_array() : ?array {
+                        if (rand() % 2 > 0) {
+                            return ["key" => "value"];
+                        }
+                        if (rand() % 3 > 0) {
+                            throw new Exception("An exception occurred");
+                        }
+                        return null;
+                    }
+
+                    function try_catch_check() : array {
+                        $arr = null;
+                        try {
+                            $arr = maybe_returns_array();
+                            if (!$arr) { return [];  }
+                        } catch (Exception $e) {
+                            if (!$arr) { return []; }
+                        }
+                        return $arr;
+                    }'
+            ]
         ];
     }
 
