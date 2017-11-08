d422bc85d6ff6b8e13e6781ab0b96809754a9867
Matthew Brown <github@muglug.com>
2017-11-07 23:49:04 -0500

    Add (skipped) test for #270

--- tests/ArrayAssignmentTest.php
+++ tests/ArrayAssignmentTest.php
@@ -464,6 +464,29 @@ class ArrayAssignmentTest extends TestCase
                     '$e' => 'int',
                 ],
             ],
+            'SKIPPED-failingMerge' => [
+                '<?php
+                    /**
+                     * @param array[][] $args
+                     * @return array[]
+                     */
+                    function get_merged_dict(array $args) {
+                        $merged = array();
+
+                        foreach ($args as $group) {
+                            foreach ($group as $key => $value) {
+                                if (isset($merged[$key])) {
+                                    $merged[$key] = array_merge($merged[$key], $value);
+                                } else {
+                                    $merged[$key] = $value;
+                                }
+                            }
+                        }
+
+                        return $merged;
+                    }',
+
+            ],
         ];
     }
 
