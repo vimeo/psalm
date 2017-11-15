bce8bdb46e0c4125902cd93aeb18c25ebf2d04f0
Matt Brown <github@muglug.com>
2017-11-15 12:09:13 -0500

    Ignore traversable, iterable etc for RawObjectIteration

--- src/Psalm/Checker/Statements/Block/ForeachChecker.php
+++ src/Psalm/Checker/Statements/Block/ForeachChecker.php
@@ -204,7 +204,10 @@ class ForeachChecker
                         'Traversable'
                     )) {
                         // @todo try and get value type
-                    } else {
+                    } elseif (!in_array(
+                        strtolower($iterator_type->value),
+                        ['iterator', 'iterable', 'traversable'], true
+                    )) {
                         if (IssueBuffer::accepts(
                             new RawObjectIteration(
                                 'Possibly undesired iteration over regular object ' . $iterator_type->value,
