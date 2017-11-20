aca23d236b498fad2d2ac1f1dd23b43d48ebef45
Matthew Brown <github@muglug.com>
2017-11-20 00:37:45 -0500

    Check for ? after replacement

--- src/Psalm/Type.php
+++ src/Psalm/Type.php
@@ -40,11 +40,11 @@ abstract class Type
             $type_string = self::convertSquareBrackets($type_string);
         }
 
-        if (preg_match('/[\[\]()]/', $type_string)) {
+        $type_string = preg_replace('/\?(?=[a-zA-Z])/', 'null|', $type_string);
+
+        if (preg_match('/[\[\]()\?]/', $type_string)) {
             throw new TypeParseTreeException('Invalid characters in type');
         }
-
-        $type_string = preg_replace('/\?(?=[a-zA-Z])/', 'null|', $type_string);
 
         $type_tokens = self::tokenize($type_string);
 
