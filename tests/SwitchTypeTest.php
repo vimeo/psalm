f4212d897c6eca6d0b511342f48607361e0ef36d
Matthew Brown <github@muglug.com>
2017-11-28 22:33:37 -0500

    Fix erroneous empty array creation in SwitchChecker
    Fixes #354

--- tests/SwitchTypeTest.php
+++ tests/SwitchTypeTest.php
@@ -201,6 +201,28 @@ class SwitchTypeTest extends TestCase
                             throw new RuntimeException("none found");
                     }',
             ],
+            'switchBools' => [
+                '<?php
+                    $x = false;
+                    $y = false;
+
+                    foreach ([1, 2, 3] as $v)  {
+                        switch($v) {
+                            case 3:
+                                $y = true;
+                                break;
+                            case 2:
+                                $x = true;
+                                break;
+                            default:
+                                break;
+                        }
+                    }',
+                'assertions' => [
+                    '$x' => 'bool',
+                    '$y' => 'bool',
+                ],
+            ],
         ];
     }
 
