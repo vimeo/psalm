c112ca9a63c4f97977a8ea5b3246959d1d77f452
Matt Brown <github@muglug.com>
2017-11-30 19:14:52 -0500

    Suppress issue in tests

--- tests/MethodMutationTest.php
+++ tests/MethodMutationTest.php
@@ -96,7 +96,7 @@ class MethodMutationTest extends TestCase
 
         $this->assertSame('UserViewData', (string)$method_context->vars_in_scope['$this->user_viewdata']);
         $this->assertSame('string', (string)$method_context->vars_in_scope['$this->user_viewdata->name']);
-        /** @psalm-suppress */
+        /** @psalm-suppress InvalidScalarArgument */
         $this->assertTrue($method_context->vars_possibly_in_scope['$this->title']);
     }
 
