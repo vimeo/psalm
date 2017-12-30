d964f96439339f594777667a6d86a529fe62b536
Matthew Brown <github@muglug.com>
2017-12-30 10:42:06 -0500

    Add test that analyses a class with a mapped property

--- tests/PropertyTypeTest.php
+++ tests/PropertyTypeTest.php
@@ -611,6 +611,38 @@ class PropertyTypeTest extends TestCase
                         public function __construct() { }
                     }',
             ],
+            'analyzePropertyMappedClass' => [
+                '<?php
+                    namespace PhpParser\Node\Stmt;
+
+                    use PhpParser\Node;
+
+                    class Finally_ extends Node\Stmt
+                    {
+                        /** @var Node[] Statements */
+                        public $stmts;
+
+                        /**
+                         * Constructs a finally node.
+                         *
+                         * @param Node[] $stmts      Statements
+                         * @param array  $attributes Additional attributes
+                         */
+                        public function __construct(array $stmts = array(), array $attributes = array()) {
+                            parent::__construct($attributes);
+                            $this->stmts = $stmts;
+                        }
+
+                        public function getSubNodeNames() {
+                            return array("stmts");
+                        }
+                    }',
+                'assertions' => [],
+                'error_levels' => [
+                    'InvalidPropertyAssignment',
+                    'MissingReturnType',
+                ],
+            ],
         ];
     }
 
