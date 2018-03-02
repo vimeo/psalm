0d079257553fe3c5430954b55cc07c1956e5b196
Bruce Weirdan <weirdan@gmail.com>
2018-03-02 02:16:10 +0200

    increased FD limit

--- bin/build-phar.sh
+++ bin/build-phar.sh
@@ -2,6 +2,8 @@
 composer global require humbug/box:dev-master
 composer install --no-dev
 [ -d build ] || mkdir build
+# increase FD limit, or Phar compression will fail
+ulimit -Sn 4096
 box build
 
 # reinstall deps (to regenerate autoloader and bring back dev deps)
