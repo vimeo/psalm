c2275f388a1f172dba662903ae97441182898c9f
Bruce Weirdan <weirdan@gmail.com>
2018-03-05 09:45:00 +0200

    box.json -> box.json.dist, use non-deprecated command to build

--- bin/build-phar.sh
+++ bin/build-phar.sh
@@ -4,12 +4,13 @@ if ! php -r 'extension_loaded("bz2") or exit(1);' ; then
 	exit 1
 fi
 
+composer global require 'humbug/php-scoper:^1.0@dev'
 composer global require humbug/box:dev-master
 composer install --no-dev
 [ -d build ] || mkdir build
 # increase FD limit, or Phar compression will fail
 ulimit -Sn 4096
-php -dphar.readonly=0 `which box` build
+php -dphar.readonly=0 `which box` compile
 
 # reinstall deps (to regenerate autoloader and bring back dev deps)
 rm -Rf vendor/*
