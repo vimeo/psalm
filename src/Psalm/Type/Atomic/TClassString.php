e5ccdc7fa09ccee72370ce0991b2c804f581c6dc
Matt Brown <github@muglug.com>
2018-03-07 11:35:48 -0500

    Make sure php string from TClassString is accurate

--- src/Psalm/Type/Atomic/TClassString.php
+++ src/Psalm/Type/Atomic/TClassString.php
@@ -22,6 +22,25 @@ class TClassString extends TString
     }
 
     /**
+     * @param  string|null   $namespace
+     * @param  array<string> $aliased_classes
+     * @param  string|null   $this_class
+     * @param  int           $php_major_version
+     * @param  int           $php_minor_version
+     *
+     * @return string
+     */
+    public function toPhpString(
+        $namespace,
+        array $aliased_classes,
+        $this_class,
+        $php_major_version,
+        $php_minor_version
+    ) {
+        return 'string';
+    }
+
+    /**
      * @return bool
      */
     public function canBeFullyExpressedInPhp()
