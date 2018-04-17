5b7d1906408c40f6c0292ea1072f5874a4300454
Matt Brown <github@muglug.com>
2018-04-17 15:39:09 -0400

    Emit RedundantCondition when asserting two variables can never be equal

--- src/Psalm/Checker/Statements/Block/TryChecker.php
+++ src/Psalm/Checker/Statements/Block/TryChecker.php
@@ -275,9 +275,9 @@ class TryChecker
 
             if ($catch_actions[$i] !== [ScopeChecker::ACTION_END]) {
                 foreach ($catch_context->vars_in_scope as $var_id => $type) {
-                    if ($catch->var !== $var_id &&
-                        $context->hasVariable($var_id) &&
-                        $context->vars_in_scope[$var_id]->getId() !== $type->getId()
+                    if ((!is_string($catch->var->name) || ('$' . $catch->var->name) !== $var_id)
+                        && $context->hasVariable($var_id)
+                        && $context->vars_in_scope[$var_id]->getId() !== $type->getId()
                     ) {
                         $context->vars_in_scope[$var_id] = Type::combineUnionTypes(
                             $context->vars_in_scope[$var_id],
