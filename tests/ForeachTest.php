392f3194c005ae09095ae97c5e07c6abf97982ee
Matthew Brown <github@muglug.com>
2018-04-18 23:47:21 -0400

    Fix #684 - use getIterator return type for IteratorAggregate functions

--- tests/ForeachTest.php
+++ tests/ForeachTest.php
@@ -49,6 +49,41 @@ class ForeachTest extends TestCase
                         foreach ($object as $foo) {}
                     }'
             ],
+            'arrayIteratorIteration' => [
+                '<?php
+                    class Item {
+                      /**
+                       * @var string
+                       */
+                      public $prop = "var";
+                    }
+
+                    class Collection implements IteratorAggregate {
+                      /**
+                       * @var Item[]
+                       */
+                      private $items = [];
+
+                      public function add(Item $item): void
+                      {
+                          $this->items[] = $item;
+                      }
+
+                      /**
+                        * @return \ArrayIterator<mixed, Item>
+                        */
+                      public function getIterator(): \ArrayIterator
+                      {
+                          return new \ArrayIterator($this->items);
+                      }
+                    }
+
+                    $collection = new Collection();
+                    $collection->add(new Item());
+                    foreach ($collection as $item) {
+                      echo $item->prop;
+                    }'
+            ],
         ];
     }
 
