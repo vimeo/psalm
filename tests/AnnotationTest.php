12cb78bf18f5f220709e68dd3e00f0782ddffe33
Matt Brown <github@muglug.com>
2018-04-20 15:22:48 -0400

    Fix parsing of nested callable without args

--- tests/AnnotationTest.php
+++ tests/AnnotationTest.php
@@ -752,6 +752,52 @@ class AnnotationTest extends TestCase
                      */
                     function example(string $x) : void {}',
             ],
+            'megaClosureAnnotationWithoutSpacing' => [
+                '<?php
+                    /** @var array{a:Closure():(array<mixed, mixed>|null), b?:Closure():array<mixed, mixed>, c?:Closure():array<mixed, mixed>, d?:Closure():array<mixed, mixed>, e?:Closure():(array{f:null|string, g:null|string, h:null|string, i:string, j:mixed, k:mixed, l:mixed, m:mixed, n:bool, o?:array{0:string}}|null), p?:Closure():(array{f:null|string, g:null|string, h:null|string, q:string, i:string, j:mixed, k:mixed, l:mixed, m:mixed, n:bool, o?:array{0:string}}|null), r?:Closure():(array<mixed, mixed>|null), s:array<mixed, mixed>} */
+                    $arr = [];
+
+                    $arr["a"]()',
+            ],
+            'megaClosureAnnotationWithSpacing' => [
+                '<?php
+                    /** @var array{
+                      a: Closure() : (array<mixed, mixed>|null),
+                      b?: Closure() : array<mixed, mixed>,
+                      c?: Closure() : array<mixed, mixed>,
+                      d?: Closure() : array<mixed, mixed>,
+                      e?: Closure() : (array{
+                        f: null|string,
+                        g: null|string,
+                        h: null|string,
+                        i: string,
+                        j: mixed,
+                        k: mixed,
+                        l: mixed,
+                        m: mixed,
+                        n: bool,
+                        o?: array{0:string}
+                      }|null),
+                      p?: Closure() : (array{
+                        f: null|string,
+                        g: null|string,
+                        h: null|string,
+                        q: string,
+                        i: string,
+                        j: mixed,
+                        k: mixed,
+                        l: mixed,
+                        m: mixed,
+                        n: bool,
+                        o?: array{0:string}
+                      }|null),
+                      r?: Closure() : (array<mixed, mixed>|null),
+                      s: array<mixed, mixed>
+                    } */
+                    $arr = [];
+
+                    $arr["a"]()',
+            ],
         ];
     }
 
