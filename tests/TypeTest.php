bb7f908b04d905d6a2800bade3a7f24158f445b4
Matt Brown <github@muglug.com>
2018-05-03 13:15:16 -0400

    Fix #706 - don’t namespace &

--- tests/TypeTest.php
+++ tests/TypeTest.php
@@ -984,6 +984,26 @@ class TypeTest extends TestCase
                     function takesI(I $i): void {}
                     function takesA(A $i): void {}',
             ],
+            'intersectionInNamespace' => [
+                '<?php
+                    namespace NS;
+                    use Countable;
+
+                    class Item {}
+                    /**
+                     * @var iterable<Item>&Countable $collection
+                     */
+                    $collection = [];
+                    count($collection);
+
+                    /**
+                     * @param iterable<Item>&Countable $collection
+                     */
+                    function mycount($collection): int {
+                        return count($collection);
+                    }
+                    mycount($collection);',
+            ],
         ];
     }
 
