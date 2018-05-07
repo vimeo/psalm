1a6fd49f9f119f70989e015fa047fdef3cfeb33d
Matt Brown <github@muglug.com>
2018-05-07 18:09:40 -0400

    Fix strict reconciliation of bools

--- tests/TypeAlgebraTest.php
+++ tests/TypeAlgebraTest.php
@@ -819,6 +819,18 @@ class TypeAlgebraTest extends TestCase
 
                     $a = (($s === "a" && rand(0, 1)) || ($s === "b" && rand(0, 1))) ? 1 : 0;',
             ],
+            'boolComparison' => [
+                '<?php
+                    $a = (bool) rand(0, 1);
+
+                    if (rand(0, 1)) {
+                        $a = null;
+                    }
+
+                    if ($a !== (bool) rand(0, 1)) {
+                        echo $a === false ? "a" : "b";
+                    }',
+            ],
         ];
     }
 
