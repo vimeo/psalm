bd40356cbe868f279830266b4b8094f492834fe9
Matthew Brown <github@muglug.com>
2018-05-08 22:13:26 -0400

    Fix #721 by allowing @property annotations to be inheritable

--- tests/MagicPropertyTest.php
+++ tests/MagicPropertyTest.php
@@ -223,6 +223,26 @@ class MagicPropertyTest extends TestCase
                         }
                     }',
             ],
+            'inheritedGetPropertyExplicitCall' => [
+                '<?php
+                    /**
+                     * @property string $test
+                     */
+                    class A {
+                        public function __get(string $name) {}
+
+                        /**
+                         * @param mixed $value
+                         */
+                        public function __set(string $name, $value) {}
+                    }
+
+                    class B extends A {
+                        public function test(): string {
+                            return $this->__get("test");
+                        }
+                    }',
+            ],
         ];
     }
 
