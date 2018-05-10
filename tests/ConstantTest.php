444dd33118dab83593b2c4209d018aeaae018483
Matt Brown <github@muglug.com>
2018-05-10 13:01:55 -0400

    Fix issue with undefined class constant keys

--- tests/ConstantTest.php
+++ tests/ConstantTest.php
@@ -108,6 +108,19 @@ class ConstantTest extends TestCase
                 'assertions' => [],
                 'error_levels' => ['MixedAssignment'],
             ],
+            'hardToDefineClassConstant' => [
+                '<?php
+                    class A {
+                      const C = [
+                        self::B => 4,
+                        "name" => 3
+                      ];
+
+                      const B = 4;
+                    }
+
+                    echo A::C[4];',
+            ],
         ];
     }
 
