496018f84a1eb4c9d677f5525632a8e34a959b5b
Brown <github@muglug.com>
2018-10-23 14:38:36 -0400

    Fix #1024 - add support for filter_var

--- tests/FunctionCallTest.php
+++ tests/FunctionCallTest.php
@@ -982,6 +982,41 @@ class FunctionCallTest extends TestCase
                     '$d' => 'string',
                 ],
             ],
+            'filterVar' => [
+                '<?php
+                    function filterInt(string $s) : int {
+                        $filtered = filter_var($s, FILTER_VALIDATE_INT);
+                        if ($filtered === false) {
+                            return 0;
+                        }
+                        return $filtered;
+                    }
+                    function filterNullableInt(string $s) : ?int {
+                        return filter_var($s, FILTER_VALIDATE_INT, ["default" => null]);
+                    }
+                    function filterIntWithDefault(string $s) : int {
+                        return filter_var($s, FILTER_VALIDATE_INT, ["default" => 5]);
+                    }
+                    function filterBool(string $s) : bool {
+                        return filter_var($s, FILTER_VALIDATE_BOOLEAN);
+                    }
+                    function filterNullableBool(string $s) : ?bool {
+                        return filter_var($s, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                    }
+                    function filterNullableBoolWithFlagsArray(string $s) : ?bool {
+                        return filter_var($s, FILTER_VALIDATE_BOOLEAN, ["flags" => FILTER_NULL_ON_FAILURE]);
+                    }
+                    function filterFloat(string $s) : float {
+                        $filtered = filter_var($s, FILTER_VALIDATE_FLOAT);
+                        if ($filtered === false) {
+                            return 0.0;
+                        }
+                        return $filtered;
+                    }
+                    function filterFloatWithDefault(string $s) : float {
+                        return filter_var($s, FILTER_VALIDATE_FLOAT, ["default" => 5.0]);
+                    }',
+            ],
         ];
     }
 
