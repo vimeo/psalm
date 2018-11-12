7cc472c2f17e9aa1e49d49b35398ffc5ae385327
Bruce Weirdan <weirdan@gmail.com>
2018-11-12 23:17:26 +0200

    Don't choke on non-existing composer.lock
    It's useful to see enabled plugins and be able to disable them even if
    no composer files are found.

--- src/Psalm/PluginManager/PluginListFactory.php
+++ src/Psalm/PluginManager/PluginListFactory.php
@@ -5,8 +5,17 @@ class PluginListFactory
 {
     public function __invoke(string $current_dir, string $config_file_path = null): PluginList
     {
+        $stub_composer_lock = (object)[
+            "packages" => [],
+            "packages-dev" => [],
+        ];
+
         $config_file = new ConfigFile($current_dir, $config_file_path);
-        $composer_lock = new ComposerLock('composer.lock');
+        $lock_file = is_readable('composer.lock') ?
+            'composer.lock' :
+            'data:application/json,' . urlencode(json_encode($stub_composer_lock));
+
+        $composer_lock = new ComposerLock($lock_file);
         return new PluginList($config_file, $composer_lock);
     }
 }
