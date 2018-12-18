0655e1c19c96ca8958931b58cbf3a287f8ea3f2a
Dave Liddament <daveliddament@gmail.com>
2018-12-18 18:13:21 +0000

    UPDATE add issue type to text output
    Adds issue type to text output. Leaves .emacs output as before.

---
+++ src/Psalm/Output/Text.php
@@ -0,0 +1,29 @@
+<?php
+namespace Psalm\Output;
+
+use Psalm\Config;
+use Psalm\Output;
+
+class Text extends Output
+{
+    /**
+     * {{@inheritdoc}}
+     */
+    public function create(): string
+    {
+        $output = null;
+        foreach ($this->issues_data as $issue_data) {
+            $output .= sprintf(
+                '%s:%s:%s:%s - %s: %s',
+                $issue_data['file_path'],
+                $issue_data['line_from'],
+                $issue_data['column_from'],
+                ($issue_data['severity'] === Config::REPORT_ERROR ? 'error' : 'warning'),
+                $issue_data['type'],
+                $issue_data['message']
+            ) . "\n";
+        }
+
+        return $output;
+    }
+}
