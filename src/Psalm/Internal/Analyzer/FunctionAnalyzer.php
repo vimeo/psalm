93e3d66ce557c0ef792ddf3139c96c3bf3dde9d1
Matthew Brown <github@muglug.com>
2019-01-02 12:53:02 -0500

    Move filter_var handler function into its own method

--- src/Psalm/Internal/Analyzer/FunctionAnalyzer.php
+++ src/Psalm/Internal/Analyzer/FunctionAnalyzer.php
@@ -587,82 +587,7 @@ class FunctionAnalyzer extends FunctionLikeAnalyzer
                     break;
 
                 case 'filter_var':
-                    if (isset($call_args[1]->value->inferredType)
-                        && $call_args[1]->value->inferredType->isSingleIntLiteral()
-                    ) {
-                        $filter_type_type = $call_args[1]->value->inferredType->getSingleIntLiteral();
-
-                        $filter_type = null;
-
-                        switch ($filter_type_type->value) {
-                            case \FILTER_VALIDATE_INT:
-                                $filter_type = Type::getInt();
-                                break;
-
-                            case \FILTER_VALIDATE_FLOAT:
-                                $filter_type = Type::getFloat();
-                                break;
-
-                            case \FILTER_VALIDATE_BOOLEAN:
-                                $filter_type = Type::getBool();
-
-                                break;
-
-                            case \FILTER_VALIDATE_IP:
-                            case \FILTER_VALIDATE_MAC:
-                            case \FILTER_VALIDATE_REGEXP:
-                            case \FILTER_VALIDATE_URL:
-                            case \FILTER_VALIDATE_EMAIL:
-                            case \FILTER_VALIDATE_DOMAIN:
-                                $filter_type = Type::getString();
-                                break;
-                        }
-
-                        $has_object_like = false;
-
-                        if (isset($call_args[2]->value->inferredType) && $filter_type) {
-                            foreach ($call_args[2]->value->inferredType->getTypes() as $atomic_type) {
-                                if ($atomic_type instanceof Type\Atomic\ObjectLike) {
-                                    $has_object_like = true;
-
-                                    if (isset($atomic_type->properties['options'])
-                                        && $atomic_type->properties['options']->hasArray()
-                                        && isset($atomic_type->properties['options']->getTypes()['array']->properties['default'])
-                                    ) {
-                                        $filter_type = Type::combineUnionTypes(
-                                            $filter_type,
-                                            $atomic_type->properties['options']->getTypes()['array']->properties['default']
-                                        );
-                                    } else {
-                                        $filter_type->addType(new Type\Atomic\TFalse);
-                                    }
-
-                                    if (isset($atomic_type->properties['flags'])
-                                        && $atomic_type->properties['flags']->isSingleIntLiteral()
-                                    ) {
-                                        $filter_flag_type =
-                                            $atomic_type->properties['flags']->getSingleIntLiteral();
-
-                                        if ($filter_type->hasBool()
-                                            && $filter_flag_type->value === \FILTER_NULL_ON_FAILURE
-                                        ) {
-                                            $filter_type->addType(new Type\Atomic\TNull);
-                                        }
-                                    }
-                                } elseif ($atomic_type instanceof Type\Atomic\TLiteralInt) {
-                                    if ($filter_type->hasBool() && $atomic_type->value === \FILTER_NULL_ON_FAILURE) {
-                                        $filter_type->addType(new Type\Atomic\TNull);
-                                    }
-                                }
-                            }
-                        }
-
-                        if (!$has_object_like && $filter_type) {
-                            $filter_type->addType(new Type\Atomic\TFalse);
-                        }
-
-                        return $filter_type ?: Type::getMixed();
-                    }
+                    return self::getFilterVar($call_args);
             }
         }
 
@@ -699,6 +624,93 @@ class FunctionAnalyzer extends FunctionLikeAnalyzer
         }
 
         return $call_map_return_type;
+    }
+
+    /**
+     * @param  array<PhpParser\Node\Arg>    $call_args
+     *
+     * @return Type\Union
+     */
+    private static function getFilterVar(array $call_args)
+    {
+        if (isset($call_args[1]->value->inferredType)
+            && $call_args[1]->value->inferredType->isSingleIntLiteral()
+        ) {
+            $filter_type_type = $call_args[1]->value->inferredType->getSingleIntLiteral();
+
+            $filter_type = null;
+
+            switch ($filter_type_type->value) {
+                case \FILTER_VALIDATE_INT:
+                    $filter_type = Type::getInt();
+                    break;
+
+                case \FILTER_VALIDATE_FLOAT:
+                    $filter_type = Type::getFloat();
+                    break;
+
+                case \FILTER_VALIDATE_BOOLEAN:
+                    $filter_type = Type::getBool();
+
+                    break;
+
+                case \FILTER_VALIDATE_IP:
+                case \FILTER_VALIDATE_MAC:
+                case \FILTER_VALIDATE_REGEXP:
+                case \FILTER_VALIDATE_URL:
+                case \FILTER_VALIDATE_EMAIL:
+                case \FILTER_VALIDATE_DOMAIN:
+                    $filter_type = Type::getString();
+                    break;
+            }
+
+            $has_object_like = false;
+
+            if (isset($call_args[2]->value->inferredType) && $filter_type) {
+                foreach ($call_args[2]->value->inferredType->getTypes() as $atomic_type) {
+                    if ($atomic_type instanceof Type\Atomic\ObjectLike) {
+                        $has_object_like = true;
+
+                        if (isset($atomic_type->properties['options'])
+                            && $atomic_type->properties['options']->hasArray()
+                            && isset($atomic_type->properties['options']->getTypes()['array']->properties['default'])
+                        ) {
+                            $filter_type = Type::combineUnionTypes(
+                                $filter_type,
+                                $atomic_type->properties['options']->getTypes()['array']->properties['default']
+                            );
+                        } else {
+                            $filter_type->addType(new Type\Atomic\TFalse);
+                        }
+
+                        if (isset($atomic_type->properties['flags'])
+                            && $atomic_type->properties['flags']->isSingleIntLiteral()
+                        ) {
+                            $filter_flag_type =
+                                $atomic_type->properties['flags']->getSingleIntLiteral();
+
+                            if ($filter_type->hasBool()
+                                && $filter_flag_type->value === \FILTER_NULL_ON_FAILURE
+                            ) {
+                                $filter_type->addType(new Type\Atomic\TNull);
+                            }
+                        }
+                    } elseif ($atomic_type instanceof Type\Atomic\TLiteralInt) {
+                        if ($filter_type->hasBool() && $atomic_type->value === \FILTER_NULL_ON_FAILURE) {
+                            $filter_type->addType(new Type\Atomic\TNull);
+                        }
+                    }
+                }
+            }
+
+            if (!$has_object_like && $filter_type) {
+                $filter_type->addType(new Type\Atomic\TFalse);
+            }
+
+            return $filter_type ?: Type::getMixed();
+        }
+        
+        return Type::getMixed();
     }
 
     /**
