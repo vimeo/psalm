d8309a726ab50745586c865f64d1f198b9e4a693
Aaron Piotrowski <aaron@trowski.com>
2019-02-05 19:00:13 -0600

    Update Amp usage
    Fixed a few errors and used byte-stream for reading and writing.

--- src/Psalm/Internal/LanguageServer/ProtocolStreamWriter.php
+++ src/Psalm/Internal/LanguageServer/ProtocolStreamWriter.php
@@ -3,13 +3,8 @@ declare(strict_types = 1);
 
 namespace Psalm\Internal\LanguageServer;
 
-use Psalm\Internal\LanguageServer\Message;
-use Amp\{
-    Deferred,
-    Loop,
-    Promise
-};
-use RuntimeException;
+use Amp\ByteStream\ResourceOutputStream;
+use Amp\Promise;
 
 /**
  * @internal
@@ -17,26 +12,16 @@ use RuntimeException;
 class ProtocolStreamWriter implements ProtocolWriter
 {
     /**
-     * @var resource $output
+     * @var \Amp\ByteStream\ResourceOutputStream
      */
     private $output;
-
-    /**
-     * @var ?string
-     */
-    private $output_watcher;
-
-    /**
-     * @var array<int, array{message: string, deferred: Deferred}> $messages
-     */
-    private $messages = [];
 
     /**
      * @param resource $output
      */
     public function __construct($output)
     {
-        $this->output = $output;
+        $this->output = new ResourceOutputStream($output);
     }
 
     /**
@@ -44,58 +29,6 @@ class ProtocolStreamWriter implements ProtocolWriter
      */
     public function write(Message $msg): Promise
     {
-        // if the message queue is currently empty, register a write handler.
-        if (empty($this->messages)) {
-            $this->output_watcher = Loop::onWritable(
-                $this->output,
-                /** @return void */
-                function () {
-                    $this->flush();
-                }
-            );
-        }
-
-        $deferred = new \Amp\Deferred();
-        $this->messages[] = [
-            'message' => (string)$msg,
-            'deferred' => $deferred
-        ];
-        return $deferred->promise();
-    }
-
-    /**
-     * Writes pending messages to the output stream.
-     *
-     * @return void
-     */
-    private function flush()
-    {
-        $keepWriting = true;
-        while ($keepWriting) {
-            $message = $this->messages[0]['message'];
-            $deferred = $this->messages[0]['deferred'];
-
-            $bytesWritten = @fwrite($this->output, $message);
-
-            if ($bytesWritten > 0) {
-                $message = substr($message, $bytesWritten);
-            }
-
-            // Determine if this message was completely sent
-            if (strlen($message) === 0) {
-                array_shift($this->messages);
-
-                // This was the last message in the queue, remove the write handler.
-                if (count($this->messages) === 0 && $this->output_watcher) {
-                    Loop::cancel($this->output_watcher);
-                    $keepWriting = false;
-                }
-
-                $deferred->resolve();
-            } else {
-                $this->messages[0]['message'] = $message;
-                $keepWriting = false;
-            }
-        }
+        return $this->output->write((string)$msg);
     }
 }
