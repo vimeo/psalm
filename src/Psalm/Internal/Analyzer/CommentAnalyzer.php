6c7819b4381b3f9b0d328d4ce83414ad13f0893d
bugreportuser <37939393+bugreportuser@users.noreply.github.com>
2019-02-09 10:02:24 -0600

    Allow @psalm-template for classes

--- src/Psalm/Internal/Analyzer/CommentAnalyzer.php
+++ src/Psalm/Internal/Analyzer/CommentAnalyzer.php
@@ -535,8 +535,11 @@ class CommentAnalyzer
 
         $info = new ClassLikeDocblockComment();
 
-        if (isset($comments['specials']['template'])) {
-            foreach ($comments['specials']['template'] as $template_line) {
+        if (isset($comments['specials']['template']) || isset($comments['specials']['psalm-template'])) {
+            $all_templates = (isset($comments['specials']['template']) ? $comments['specials']['template'] : [])
+                + (isset($comments['specials']['psalm-template']) ? $comments['specials']['psalm-template'] : []);
+
+            foreach ($all_templates as $template_line) {
                 $template_type = preg_split('/[\s]+/', $template_line);
 
                 if (count($template_type) > 2
