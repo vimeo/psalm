61e086ac7ba7fc43cb81226a7b230a489e6bc7cd
bugreportuser <37939393+bugreportuser@users.noreply.github.com>
2019-02-18 20:31:08 -0600

    Ignore more redundant issues

--- tests/PropertyTypeTest.php
+++ tests/PropertyTypeTest.php
@@ -1385,6 +1385,42 @@ class PropertyTypeTest extends TestCase
                         }
                     }'
             ],
+            'noRedundantConditionWhenCheckingInitializationsEdgeCases' => [
+                '<?php
+                    final class Clazz {
+                        /**
+                         * @var bool
+                         */
+                        public $x;
+
+                        /**
+                         * @var int
+                         */
+                        public $y = 0;
+
+                        public function func1 (): bool {
+                            if ($this->y !== 0) {
+                                return true;
+                            }
+                            return false;
+                        }
+
+                        public function func2 (): int {
+                            if ($this->y !== 0) {
+                                return $this->y;
+                            }
+                            return 2;
+                        }
+
+                        public function __construct () {
+                            $this->x = false;
+                            if ($this->func1()) {
+                                $this->y = $this->func2();
+                            }
+                            $this->func2();
+                        }
+                    }'
+            ],
         ];
     }
 
