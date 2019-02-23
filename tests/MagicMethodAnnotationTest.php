18b5e87f41468c982bdd465b7d4ef05a87663cbe
bugreportuser <37939393+bugreportuser@users.noreply.github.com>
2019-02-22 19:42:36 -0600

    Allow @psalm-property and @psalm-method

--- tests/MagicMethodAnnotationTest.php
+++ tests/MagicMethodAnnotationTest.php
@@ -386,6 +386,26 @@ class MagicMethodAnnotationTest extends TestCase
                     '$d' => 'D',
                 ]
             ],
+            'validSimplePsalmAnnotations' => [
+                '<?php
+                    class ParentClass {
+                        public function __call(string $name, array $args) {}
+                    }
+
+                    /**
+                     * @psalm-method string getString() dsa sada
+                     * @psalm-method  void setInteger(int $integer) dsa sada
+                     */
+                    class Child extends ParentClass {}
+
+                    $child = new Child();
+
+                    $a = $child->getString();
+                    $child->setInteger(4);',
+                'assertions' => [
+                    '$a' => 'string',
+                ],
+            ],
         ];
     }
 
