51493b552a58bb4f25beda00a27d4f144c2dfa9f
bugreportuser <37939393+bugreportuser@users.noreply.github.com>
2019-02-23 08:37:37 -0600

    Add @psalm-property and @psalm-method override tests

--- tests/MagicMethodAnnotationTest.php
+++ tests/MagicMethodAnnotationTest.php
@@ -406,6 +406,28 @@ class MagicMethodAnnotationTest extends TestCase
                     '$a' => 'string',
                 ],
             ],
+            'overrideMethodAnnotations' => [
+                '<?php
+                    class ParentClass {
+                        public function __call(string $name, array $args) {}
+                    }
+
+                    /**
+                     * @method int getString() dsa sada
+                     * @method  void setInteger(string $integer) dsa sada
+                     * @psalm-method string getString() dsa sada
+                     * @psalm-method  void setInteger(int $integer) dsa sada
+                     */
+                    class Child extends ParentClass {}
+
+                    $child = new Child();
+
+                    $a = $child->getString();
+                    $child->setInteger(4);',
+                'assertions' => [
+                    '$a' => 'string',
+                ],
+            ],
         ];
     }
 
