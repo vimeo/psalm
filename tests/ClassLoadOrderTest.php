758c7e22ff5d88dad1944184882d9e5c6c708a11
Brown <github@muglug.com>
2019-05-23 12:54:23 -0400

    Fix bad signature in ClassLoadOrderTest

--- tests/ClassLoadOrderTest.php
+++ tests/ClassLoadOrderTest.php
@@ -99,7 +99,7 @@ class ClassLoadOrderTest extends TestCase
                         protected $foo = C::DOPE;
 
                         /** @return string */
-                        public function __get() {
+                        public function __get(string $s) {
                             return "foo";
                         }
                     }
