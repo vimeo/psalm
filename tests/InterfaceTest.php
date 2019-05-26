97f4cdb7f51f671e7a09c3a5e2a002e405b021a4
Matthew Brown <github@muglug.com>
2019-05-26 13:11:43 -0400

    Fix #1686 - intersect object and named class properly

--- tests/InterfaceTest.php
+++ tests/InterfaceTest.php
@@ -581,6 +581,29 @@ class InterfaceTest extends TestCase
                         return $i->foo();
                     }',
             ],
+            'intersectionObjectTypes' => [
+                '<?php
+
+                    class C {}
+
+                    interface IFoo {
+                        function foo() : object;
+                    }
+
+                    interface IBar {
+                        function foo() : C;
+                    }
+
+                    /** @param IFoo&IBar $i */
+                    function iFooFirst($i) : C {
+                        return $i->foo();
+                    }
+
+                    /** @param IBar&IFoo $i */
+                    function iBarFirst($i) : C {
+                        return $i->foo();
+                    }',
+            ],
         ];
     }
 
