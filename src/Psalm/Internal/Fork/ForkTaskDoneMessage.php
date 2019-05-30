cec69bd7e99f77951d6673c9b7322c75a55f5edb
Ilija Tovilo <ilija.tovilo@me.com>
2019-05-30 14:15:56 +0200

    Revert "Revert "Pass success flag to progress""
    This reverts commit 62a690ee4eb192065bac0934202923aeb472b89e.

--- src/Psalm/Internal/Fork/ForkTaskDoneMessage.php
+++ src/Psalm/Internal/Fork/ForkTaskDoneMessage.php
@@ -3,4 +3,14 @@ namespace Psalm\Internal\Fork;
 
 class ForkTaskDoneMessage implements ForkMessage
 {
+    /** @var mixed */
+    public $data;
+
+    /**
+     * @param mixed $data
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
 }
