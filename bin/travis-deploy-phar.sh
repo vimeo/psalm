e6f4f27498fef5ee0d8499eb88f18434639c4912
Bruce Weirdan <weirdan@gmail.com>
2019-06-01 19:51:33 +0300

    Simplify build config by using repo filter
    Also deploy signature to psalm/phar repo (because why not)

--- bin/travis-deploy-phar.sh
+++ bin/travis-deploy-phar.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 git clone https://${GITHUB_TOKEN}@github.com/psalm/phar.git > /dev/null 2>&1
-cp build/psalm.phar phar/psalm.phar
+cp build/psalm.phar build/psalm.phar.asc phar/
 cd phar
 git config user.email "travis@travis-ci.org"
 git config user.name "Travis CI"
-git add psalm.phar
+git add psalm.phar psalm.phar.asc
 git commit -m "Updated Psalm phar to commit ${TRAVIS_COMMIT}"
 git push --quiet origin master
 
