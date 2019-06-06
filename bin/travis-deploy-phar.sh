360cc10b5ac8a661334c61d676c599b66e2c5d9f
Barney Laurance <barney@redmagic.org.uk>
2019-06-06 23:12:53 +0100

    Add executable PHP files to PHAR distribution
    Previously a user of the phar distribution would have to invoke psalm as
    `vendor/bin/psalm.phar`. This is different to the command given in
    the psalm documentation, `vendor/bin/psalm`
    
    I also copied all files from the psalm/phar repo into
    assets/psalm-phar, so that development can be concentrated in this repo.
    
    The travis-deploy-phar.sh should copy any changes made back into the
    psalm/phar.git repo.

--- bin/travis-deploy-phar.sh
+++ bin/travis-deploy-phar.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 git clone https://${GITHUB_TOKEN}@github.com/psalm/phar.git > /dev/null 2>&1
-cp build/psalm.phar build/psalm.phar.asc phar/
 cd phar
+rm -rf !(".git")
+cp ../build/psalm.phar ../build/psalm.phar.asc ../assets/psalm-phar/* .
+mv dot-gitignore .gitignore
 git config user.email "travis@travis-ci.org"
 git config user.name "Travis CI"
-git add psalm.phar psalm.phar.asc
+git add --all .
 git commit -m "Updated Psalm phar to commit ${TRAVIS_COMMIT}"
 git push --quiet origin master
 
