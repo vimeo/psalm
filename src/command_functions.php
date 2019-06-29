b180948b9d5cae9d2ac4cab5a7685442afa9a51b
Tyson Andre <tysonandre775@hotmail.com>
2019-06-29 12:08:58 -0400

    Fix redundant casts/conditions in psalm
    This includes a check for array_map that was accidentally an assignment

--- src/command_functions.php
+++ src/command_functions.php
@@ -100,8 +100,8 @@ function requireAutoloaders($current_dir, $has_explicit_root, $vendor_dir)
         exit(1);
     }
 
-    define('PSALM_VERSION', (string) \PackageVersions\Versions::getVersion('vimeo/psalm'));
-    define('PHP_PARSER_VERSION', (string) \PackageVersions\Versions::getVersion('nikic/php-parser'));
+    define('PSALM_VERSION', \PackageVersions\Versions::getVersion('vimeo/psalm'));
+    define('PHP_PARSER_VERSION', \PackageVersions\Versions::getVersion('nikic/php-parser'));
 
     return $first_autoloader;
 }
