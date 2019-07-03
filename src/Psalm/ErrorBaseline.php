4cfd01b27e27323de63fdeedd35319c4c9da58fd
SignpostMarv <me@signpostmarv.name>
2019-07-03 20:33:59 +0100

    typehint parameter $matches

--- src/Psalm/ErrorBaseline.php
+++ src/Psalm/ErrorBaseline.php
@@ -266,6 +266,9 @@ class ErrorBaseline
 
         $xml = preg_replace_callback(
             '/<files (psalm-version="[^"]+") (?:php-version="(.+)">\n)/',
+            /**
+            * @param array{0:string, 1:string, 2:string} $matches
+            */
             function (array $matches) : string {
                 return
                     '<files' .
