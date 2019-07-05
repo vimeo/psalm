aa3df3f7a511ae4e12dedaa8f21552c47814fc5b
Matthew Brown <github@muglug.com>
2019-07-05 10:40:05 -0400

    Add better support for preg_replace_callback

--- tests/FunctionCallTest.php
+++ tests/FunctionCallTest.php
@@ -1768,6 +1768,19 @@ class FunctionCallTest extends TestCase
                         }
                     }'
             ],
+            'pregReplaceCallback' => [
+                '<?php
+                    function foo(string $s) : string {
+                        return preg_replace_callback(
+                            \'/<files (psalm-version="[^"]+") (?:php-version="(.+)">\n)/\',
+                            /** @param array<int, string> $matches */
+                            function (array $matches) : string {
+                                return $matches[1];
+                            },
+                            $s
+                        );
+                    }',
+            ],
         ];
     }
 
