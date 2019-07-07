820eba1151f9b57741bf59ee490cd561649b5cb3
Matthew Brown <github@muglug.com>
2019-07-07 14:01:33 -0400

    Add test for null init

--- tests/TryCatchTest.php
+++ tests/TryCatchTest.php
@@ -232,6 +232,22 @@ class TryCatchTest extends TestCase
 
                     echo $lastException->getMessage();'
             ],
+            'varSetInOnlyCatchWithNull' => [
+                '<?php
+                    $lastException = null;
+
+                    try {
+                        if (rand(0, 1)) {
+                            throw new \Exception("Gotcha!");
+                        }
+
+                        exit;
+                    } catch (\Exception $e) {
+                        $lastException = $e;
+                    }
+
+                    echo $lastException->getMessage();'
+            ],
         ];
     }
 
