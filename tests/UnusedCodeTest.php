f79e880c53c3a04c0d8ffcb2129b0939eecc427a
Demi <2e3s19@gmail.com>
2019-07-21 22:28:45 -0400

    Add a missing condition into the test

--- tests/UnusedCodeTest.php
+++ tests/UnusedCodeTest.php
@@ -585,15 +585,19 @@ class UnusedCodeTest extends TestCase
                     class A {
                         /** @var int */
                         private $used;
-                    
+
                         /** @var int */
                         private $unused;
-                    
+
+                        /** @var int */
+                        private static $staticUnused;
+
                         public function __construct() {
                             $this->used = 4;
                             $this->unused = 4;
+                            self::$staticUnused = 4;
                         }
-                    
+
                         public function handle(): void
                         {
                             $this->used++;
