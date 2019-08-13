8f29db240834d52806f82e926da0f06a2873576b
Matthew Brown <github@muglug.com>
2019-08-13 00:01:35 -0400

    Reduce output of PHPUnit test

--- bin/test-with-real-projects.sh
+++ bin/test-with-real-projects.sh
@@ -11,5 +11,5 @@ git clone git@github.com:sebastianbergmann/phpunit.git
 cd phpunit
 git checkout 24b6cfcec34c1167 # release 8.2.2
 composer install
-~/project/build/psalm.phar --config=.psalm/config.xml --monochrome
+~/project/build/psalm.phar --config=.psalm/config.xml --monochrome --show-info=false
 ~/project/build/psalm.phar --config=.psalm/static-analysis.xml --monochrome
