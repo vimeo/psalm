0972debfddda0ea4b089f27ff86a053a60e2daac
Brown <github@muglug.com>
2019-09-26 15:57:51 -0400

    Add test for array assertion type

--- tests/Template/FunctionTemplateAssertTest.php
+++ tests/Template/FunctionTemplateAssertTest.php
@@ -134,6 +134,29 @@ class FunctionTemplateAssertTest extends TestCase
                 [],
                 ['MixedArgument', 'MixedMethodCall', 'ArgumentTypeCoercion'],
             ],
+            'assertTypedArray' => [
+                '<?php
+                    namespace Bar;
+
+                    class A {
+                        public function foo() : void {}
+                    }
+
+                    /**
+                     * @template T
+                     * @param class-string<T> $expected
+                     * @param mixed  $actual
+                     * @psalm-assert T[] $actual
+                     */
+                    function assertArrayOf($expected, $actual) : void {}
+
+                    function bar(array $arr) : void {
+                        assertArrayOf(A::class, $arr);
+                        foreach ($arr as $a) {
+                            $a->foo();
+                        }
+                    }',
+            ],
             'assertTemplatedTypeString' => [
                 '<?php
                     interface Foo {
