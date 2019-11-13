4be3180512f813dd9cb88d141976191162e53097
Brown <github@muglug.com>
2019-11-13 13:22:04 -0500

    Add more impure functions

--- src/Psalm/Internal/Codebase/Functions.php
+++ src/Psalm/Internal/Codebase/Functions.php
@@ -282,7 +282,7 @@ class Functions
             'chdir', 'chgrp', 'chmod', 'chown', 'chroot', 'closedir', 'copy', 'file_put_contents',
             'fopen', 'fread', 'fwrite', 'fclose', 'touch', 'fpassthru', 'fputs', 'fscanf', 'fseek',
             'ftruncate', 'fprintf', 'symlink', 'mkdir', 'unlink', 'rename', 'rmdir', 'popen', 'pclose',
-            'fputcsv', 'umask', 'finfo_close',
+            'fputcsv', 'umask', 'finfo_close', 'readline_add_history', 'stream_set_timeout',
 
             // stream/socket io
             'stream_context_set_option', 'socket_write', 'stream_set_blocking', 'socket_close',
@@ -328,9 +328,10 @@ class Functions
             'shell_exec', 'exec', 'system', 'passthru', 'pcntl_exec',
 
             // well-known functions
-            'libxml_use_internal_errors', 'curl_exec',
+            'libxml_use_internal_errors', 'libxml_disable_entity_loader', 'curl_exec',
             'mt_srand', 'openssl_pkcs7_sign',
-            'mt_rand', 'rand',
+            'mt_rand', 'rand', 'wincache_ucache_delete', 'wincache_ucache_set', 'wincache_ucache_inc',
+            'class_alias',
 
             // php environment
             'ini_set', 'sleep', 'usleep', 'register_shutdown_function',
@@ -344,7 +345,8 @@ class Functions
             'openlog', 'syslog', 'error_log', 'define_syslog_variables',
 
             // session
-            'session_id', 'session_name', 'session_set_cookie_params',
+            'session_id', 'session_name', 'session_set_cookie_params', 'session_set_save_handler',
+            'session_regenerate_id', 'mb_internal_encoding',
 
             // ldap
             'ldap_set_option',
