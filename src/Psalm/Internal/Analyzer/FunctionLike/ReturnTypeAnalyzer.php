b3a15e80166395a73ebbfd7c7a76fcbbe893e03e
Brown <github@muglug.com>
2019-11-14 14:33:07 -0500

    Fix #2349 - allow suppression of InvalidReturnType for no-return

--- src/Psalm/Internal/Analyzer/FunctionLike/ReturnTypeAnalyzer.php
+++ src/Psalm/Internal/Analyzer/FunctionLike/ReturnTypeAnalyzer.php
@@ -192,7 +192,8 @@ class ReturnTypeAnalyzer
                     $cased_method_id . ' is not expected to return any values but it does, '
                         . 'either implicitly or explicitly',
                     $return_type_location
-                )
+                ),
+                $source->getSuppressedIssues()
             )) {
                 return false;
             }
