e0bc203d1611d00d0a2fb71175ea4ec1a2ff7c81
Tyson Andre <tysonandre775@hotmail.com>
2019-11-21 11:56:38 -0500

    Use the long progress bar for CI logs

--- src/command_functions.php
+++ src/command_functions.php
@@ -376,6 +376,9 @@ Options:
     --no-progress
         Disable the progress indicator
 
+    --long-progress
+        Use a progress indicator suitable for Continuous Integration logs
+
     --alter
         Run Psalter
 
