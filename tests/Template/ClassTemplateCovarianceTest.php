45bf8eee002beda0da100919cc05ca1d81c6c7c7
Matthew Brown <github@muglug.com>
2019-11-29 11:25:29 -0500

    Add test that should always work

--- tests/Template/ClassTemplateCovarianceTest.php
+++ tests/Template/ClassTemplateCovarianceTest.php
@@ -329,6 +329,48 @@ class ClassTemplateCovarianceTest extends TestCase
                     covariant($cats);
                     covariant($misc);',
             ],
+            'allowCovariantReferenceToMapToCovariant' => [
+                '<?php
+                    /** @template-covariant T */
+                    class CovariantReference
+                    {
+                        /** @var T */
+                        private $value;
+
+                        /** @param T $value */
+                        public function __construct($value)
+                        {
+                            $this->value = $value;
+                        }
+
+                        /** @return T */
+                        public function get()
+                        {
+                            return $this->value;
+                        }
+                    }
+
+                    /**
+                     * @template-covariant T
+                     */
+                    class C
+                    {
+                        /** @var CovariantReference<T> */
+                        private $reference;
+
+                        /** @param CovariantReference<T> $reference */
+                        public function __construct($reference)
+                        {
+                            $this->reference = $reference;
+                        }
+
+                        /** @return CovariantReference<T> */
+                        function getReference()
+                        {
+                            return $this->reference;
+                        }
+                    }'
+            ],
         ];
     }
 
