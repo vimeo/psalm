f48773e93111df197b14731436086f177e9d7653
Matthew Brown <github@muglug.com>
2019-12-01 14:22:56 -0500

    bump phpunit commit

--- bin/test-with-real-projects.sh
+++ bin/test-with-real-projects.sh
@@ -9,7 +9,7 @@ cd testing-with-real-projects
 git clone git@github.com:sebastianbergmann/phpunit.git
 
 cd phpunit
-git checkout 302faed # release 8.3.2
+git checkout 4db048f # small template fix
 composer install
 ~/project/build/psalm.phar --config=.psalm/config.xml --monochrome --show-info=false
 ~/project/build/psalm.phar --config=.psalm/static-analysis.xml --monochrome
