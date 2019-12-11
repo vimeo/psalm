3a7dd61889b72cb7ac386ad9b2224a4dcd20c8c1
Brown <github@muglug.com>
2019-12-11 10:30:40 -0500

    Fix #2457 - allow psalm-type aliases to be used above return type

--- src/Psalm/Internal/Analyzer/Statements/ReturnAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/ReturnAnalyzer.php
@@ -55,13 +55,18 @@ class ReturnAnalyzer
         $codebase = $statements_analyzer->getCodebase();
 
         if ($doc_comment && ($parsed_docblock = $statements_analyzer->getParsedDocblock())) {
+            $file_storage_provider = $codebase->file_storage_provider;
+
+            $file_storage = $file_storage_provider->get($statements_analyzer->getFilePath());
+
             try {
                 $var_comments = CommentAnalyzer::arrayToDocblocks(
                     $doc_comment,
                     $parsed_docblock,
                     $statements_analyzer->getSource(),
                     $statements_analyzer->getAliases(),
-                    $statements_analyzer->getTemplateTypeMap()
+                    $statements_analyzer->getTemplateTypeMap(),
+                    $file_storage->type_aliases
                 );
             } catch (DocblockParseException $e) {
                 if (IssueBuffer::accepts(
--- tests/AnnotationTest.php
+++ tests/AnnotationTest.php
@@ -1031,6 +1031,21 @@ class AnnotationTest extends TestCase
                         public static function barBar();
                     }',
             ],
+            'psalmTypeAnnotationAboveReturn' => [
+                '<?php
+                    /**
+                     * @psalm-type Person = array{name: string, age: int}
+                     */
+
+                    /**
+                     * @psalm-return Person
+                     */
+                    function getPerson_error(): array {
+                        $json = \'{"name": "John", "age": 44}\';
+                        /** @psalm-var Person */
+                        return json_decode($json, true);
+                    }'
+            ],
         ];
     }
 
