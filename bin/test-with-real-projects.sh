e3e3296dd9da3808e03705494a41105e01b9bac1
Bruce Weirdan <weirdan@gmail.com>
2019-12-29 03:51:39 +0200

    Test doctrine collections (#2524)
    * Added doctrine/collections to the list of real projects to test with
    
    * Don't use phar version to check doctrine/collections

--- bin/test-with-real-projects.sh
+++ bin/test-with-real-projects.sh
@@ -13,3 +13,10 @@ git checkout 69a81ac # bugfix
 composer install
 ~/project/build/psalm.phar --config=.psalm/config.xml --monochrome --show-info=false
 ~/project/build/psalm.phar --config=.psalm/static-analysis.xml --monochrome
+
+cd /tmp/testing-with-real-projects
+
+git clone git@github.com:doctrine/collections.git
+cd collections
+composer install
+~/project/psalm --monochrome --show-info=false
