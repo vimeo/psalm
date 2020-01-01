b26c57237f7f0f7def838193402318eb49e39022
Marco Pivetta <ocramius@gmail.com>
2020-01-01 19:55:33 +0100

    Ensure BC of API, compat with `roave/you-are-using-it-wrong`, completeness of `composer.json` (#2140)
    * Test against `roave/you-are-using-it-wrong`
    
    * Added CI step to ensure BC of declared API
    
    * Added step to ensure `composer.json` has all used deps
    
    * Including CI check tools as dev dependencies
    
    * Typo fix: s/backwards/backward
    
    * Run `roave/backward-compatibility-check` off an isolated location with no other dependencies
    
    * Run `test-with-real-projects` task with PHP 7.4 as base runtime
    
    * Run `testing-with-real-projects` also against `ocramius/proxy-manager`
    
    `ocramius/proxy-manager` is an extremely heavy `vimeo/psalm` consumer,
    and relies on a lot of the templated types system to generate real
    types for proxies produced by runtime evaluation.

--- bin/test-with-real-projects.sh
+++ bin/test-with-real-projects.sh
@@ -20,3 +20,18 @@ git clone git@github.com:muglug/collections.git
 cd collections
 composer install
 ~/project/psalm --monochrome --show-info=false
+
+cd /tmp/testing-with-real-projects
+
+git clone git@github.com:Ocramius/ProxyManager.git
+cd ProxyManager
+composer install
+~/project/build/psalm.phar --monochrome
+
+cd /tmp/testing-with-real-projects
+
+git clone git@github.com:roave/you-are-using-it-wrong.git
+cd you-are-using-it-wrong
+composer install
+~/project/build/psalm.phar --monochrome
+./vendor/bin/phpunit
