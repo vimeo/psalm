3aa6362d0b254aab34adf23778f9c5cd6bcdea1b
Brown <github@muglug.com>
2020-01-02 11:09:38 -0500

    Fix test-with-real-projects

--- bin/test-with-real-projects.sh
+++ bin/test-with-real-projects.sh
@@ -9,7 +9,6 @@ cd testing-with-real-projects
 
 git clone git@github.com:sebastianbergmann/phpunit.git
 cd phpunit
-git checkout 1c2bc44 # bugfix
 composer install
 ~/project/build/psalm.phar --config=.psalm/config.xml --monochrome --show-info=false
 ~/project/build/psalm.phar --config=.psalm/static-analysis.xml --monochrome
@@ -23,16 +22,16 @@ composer install
 
 cd /tmp/testing-with-real-projects
 
-git clone git@github.com:muglug/ProxyManager.git
+git clone git@github.com:vimeo/ProxyManager.git
 cd ProxyManager
-git checkout c61365d3a856d5a88f53b7c1fc8dc775f06fa25c
 composer install
 ~/project/psalm --monochrome
 
 cd /tmp/testing-with-real-projects
 
-git clone git@github.com:roave/you-are-using-it-wrong.git
+git clone git@github.com:muglug/you-are-using-it-wrong.git
 cd you-are-using-it-wrong
+git checkout 5d29951fd0c49570e566c3dd5118eb18dc9c67bb
 composer install
 ~/project/psalm --monochrome
 ./vendor/bin/phpunit
