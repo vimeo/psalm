df8f2085f0adcc907ad2dc26a4231310444fb1f1
Brown <github@muglug.com>
2020-01-14 18:39:15 -0500

    Change constant type to be more forgiving

--- src/Psalm/Internal/CallMap.php
+++ src/Psalm/Internal/CallMap.php
@@ -1191,7 +1191,7 @@ return [
 'connection_aborted' => ['int'],
 'connection_status' => ['int'],
 'connection_timeout' => ['int'],
-'constant' => ['scalar|array<scalar>|null', 'const_name'=>'string'],
+'constant' => ['mixed', 'const_name'=>'string'],
 'convert_cyr_string' => ['string', 'str'=>'string', 'from'=>'string', 'to'=>'string'],
 'convert_uudecode' => ['string', 'data'=>'string'],
 'convert_uuencode' => ['string', 'data'=>'string'],
