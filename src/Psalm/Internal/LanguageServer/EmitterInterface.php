c61033237a15d3161ceaf5ea3f8d42be62a0a47d
Orklah <ronan_morin@hotmail.com>
2020-01-22 19:43:06 +0100

    change interface phpdoc to be compatible with expected list<>

--- src/Psalm/Internal/LanguageServer/EmitterInterface.php
+++ src/Psalm/Internal/LanguageServer/EmitterInterface.php
@@ -42,7 +42,7 @@ interface EmitterInterface
      * Lastly, if there are 5 event handlers for an event. The continueCallback
      * will be called at most 4 times.
      *
-     * @param array<int, mixed> $arguments
+     * @param list<mixed> $arguments
      */
     public function emit(
         string $eventName,
