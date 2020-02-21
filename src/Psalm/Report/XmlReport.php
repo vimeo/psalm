4fd72ed7250a733449e0df6deebc8935e6069dad
Matthew Brown <github@muglug.com>
2020-02-20 22:45:00 -0500

    Use simple (array) cast

--- src/Psalm/Report/XmlReport.php
+++ src/Psalm/Report/XmlReport.php
@@ -13,34 +13,17 @@ class XmlReport extends Report
      */
     public function create(): string
     {
-        $items = array_map(
-            /**
-             * @return array{severity: string, line_from: int, line_to: int, type: string, message: string,
-             * file_name: string, file_path: string, snippet: string, from: int, to: int,
-             * snippet_from: int, snippet_to: int, column_from: int, column_to: int, selected_text: string}
-             */
-            function (IssueData $issue_data) {
-                return [
-                    'severity'      => $issue_data->severity,
-                    'line_from'     => $issue_data->line_from,
-                    'line_to'       => $issue_data->line_to,
-                    'type'          => $issue_data->type,
-                    'message'       => $issue_data->message,
-                    'file_name'     => $issue_data->file_name,
-                    'file_path'     => $issue_data->file_path,
-                    'snippet'       => $issue_data->snippet,
-                    'from'          => $issue_data->from,
-                    'to'            => $issue_data->to,
-                    'snippet_from'  => $issue_data->snippet_from,
-                    'snippet_to'    => $issue_data->snippet_to,
-                    'column_from'   => $issue_data->column_from,
-                    'column_to'     => $issue_data->column_to,
-                    'selected_text' => $issue_data->selected_text,
-                ];
-            },
-            $this->issues_data
+        $items = Array2XML::createXML(
+            'report',
+            [
+                'item' => 
+                    function (IssueData $issue_data) {
+                        return (array) $issue_data;
+                    },
+                    $this->issues_data
+                )
+            ]
         );
-        $xml = Array2XML::createXML('report', ['item' => $items]);
 
         return $xml->saveXML();
     }
