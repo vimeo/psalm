9f45e6e9841e473f291f7a69e4d2e126f23bfe38
kolja <koljaz@web.de>
2020-02-24 21:24:42 +0100

    Added function to read memory_limit ini setting always in bytes.

---
+++ tests/functions/GetMemoryLimitInBytesTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Psalm\Tests\Functions;
+
+use function Psalm\getMemoryLimitInBytes;
+
+class GetMemoryLimitInBytesTest extends \Psalm\Tests\TestCase
+{
+    public function memoryLimitSettingProvider()
+    {
+        return [
+            // unlimited
+            [-1, -1],
+            // byte values
+            [1, 1],
+            [512, 512],
+            [2048, 2048],
+            // uppercase units
+            ['1K', 1024],
+            ['24K', 24576],
+            ['1M', 1048576],
+            ['24M', 25165824],
+            ['1G', 1073741824],
+            ['24G', 25769803776],
+            // lowercase units
+            ['1k', 1024],
+            ['24k', 24576],
+            ['1m', 1048576],
+            ['24m', 25165824],
+            ['1g', 1073741824],
+            ['24g', 25769803776],
+        ];
+    }
+
+    /**
+     * @dataProvider memoryLimitSettingProvider
+     *
+     * @param $setting
+     * @param $expectedBytes
+     *
+     * @return void
+     */
+    public function testGetMemoryLimitInBytes(
+        $setting,
+        $expectedBytes
+    ) {
+        ini_set('memory_limit', $setting);
+        $this->assertSame($expectedBytes, getMemoryLimitInBytes(), 'Memory limit in bytes does not fit setting');
+    }
+}
