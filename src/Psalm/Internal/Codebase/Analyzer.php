0e60a1a223755140ff52a621c9cf61211289d980
Matthew Brown <github@muglug.com>
2020-03-06 19:31:39 -0500

    Revert erroneous line

--- src/Psalm/Internal/Codebase/Analyzer.php
+++ src/Psalm/Internal/Codebase/Analyzer.php
@@ -570,7 +570,9 @@ class Analyzer
     public function loadCachedResults(ProjectAnalyzer $project_analyzer)
     {
         $codebase = $project_analyzer->getCodebase();
-        if ($codebase->diff_methods) {
+        if ($codebase->diff_methods
+            && (!$codebase->collect_references || $codebase->server_mode)
+        ) {
             $this->analyzed_methods = $codebase->file_reference_provider->getAnalyzedMethods();
             $this->existing_issues = $codebase->file_reference_provider->getExistingIssues();
             $file_maps = $codebase->file_reference_provider->getFileMaps();
