15c6778223f3d09c15c9e45fad4ba2e4f30cb1e4
Matthew Brown <github@muglug.com>
2020-03-29 18:59:48 -0400

    Add test that passes

--- tests/TypeReconciliation/ConditionalTest.php
+++ tests/TypeReconciliation/ConditionalTest.php
@@ -2586,6 +2586,35 @@ class ConditionalTest extends \Psalm\Tests\TestCase
                         use T;
                     }'
             ],
+            'smallConditional' => [
+                '<?php
+                    class A {
+                        public array $parts = [];
+                    }
+
+                    class FuncCall {
+                        /** @var ?A */
+                        public $name;
+                        /** @var array<string> */
+                        public $args = [];
+                    }
+
+                    function barr(FuncCall $function) : void {
+                        if (!$function->name instanceof A) {
+                            return;
+                        }
+
+                        if ($function->name->parts === ["function_exists"]
+                            && isset($function->args[0])
+                        ) {
+                            // do something
+                        } elseif ($function->name->parts === ["class_exists"]
+                            && isset($function->args[0])
+                        ) {
+                            // do something else
+                        }
+                    }'
+            ],
         ];
     }
 
