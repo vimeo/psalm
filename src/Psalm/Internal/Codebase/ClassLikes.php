81534bbe1803e3bf55f58dadc939b53c5341e9f7
Brown <github@muglug.com>
2020-04-04 18:57:33 -0400

    Fix possibly null issue

--- src/Psalm/Internal/Codebase/ClassLikes.php
+++ src/Psalm/Internal/Codebase/ClassLikes.php
@@ -336,7 +336,7 @@ class ClassLikes
                     $calling_method_id,
                     $fq_class_name_lc
                 );
-            } elseif (strtolower($calling_fq_class_name) !== $fq_class_name_lc) {
+            } elseif (!$calling_fq_class_name || strtolower($calling_fq_class_name) !== $fq_class_name_lc) {
                 $this->file_reference_provider->addNonMethodReferenceToClass(
                     $code_location->file_path,
                     $fq_class_name_lc
