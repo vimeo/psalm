6308ba782b80d57094ee36af5f8e058c2dde2b21
Bruce Weirdan <weirdan@gmail.com>
2020-04-06 02:57:36 +0300

    Run every project as separate step
    It's easier to see which project is failing this way

--- bin/test-with-real-projects.sh
+++ bin/test-with-real-projects.sh
@@ -4,28 +4,32 @@ set -e
 set -x
 
 cd /tmp/
-mkdir testing-with-real-projects
+mkdir -p testing-with-real-projects
 cd testing-with-real-projects
 
-git clone git@github.com:muglug/phpunit.git
-cd phpunit
-composer install
-~/project/build/psalm.phar --config=.psalm/config.xml --monochrome --show-info=false
-~/project/build/psalm.phar --config=.psalm/static-analysis.xml --monochrome
+case $1 in
+phpunit)
+	git clone git@github.com:muglug/phpunit.git
+	cd phpunit
+	composer install
+	~/project/build/psalm.phar --config=.psalm/config.xml --monochrome --show-info=false
+	~/project/build/psalm.phar --config=.psalm/static-analysis.xml --monochrome
+	;;
 
-cd /tmp/testing-with-real-projects
+collections)
+	git clone git@github.com:muglug/collections.git
+	cd collections
+	composer install
+	~/project/psalm --monochrome --show-info=false
+	;;
 
-git clone git@github.com:muglug/collections.git
-cd collections
-composer install
-~/project/psalm --monochrome --show-info=false
-
-cd /tmp/testing-with-real-projects
-
-git clone git@github.com:muglug/ProxyManager.git
-cd ProxyManager
-composer install
-~/project/psalm --monochrome
-
-cd /tmp/testing-with-real-projects
-
+proxymanager)
+	git clone git@github.com:muglug/ProxyManager.git
+	cd ProxyManager
+	composer install
+	~/project/psalm --monochrome
+	;;
+*)
+	echo "Usage: test-with-real-projects.sh {phpunit|collections|proxymanager}"
+	exit 1
+esac
