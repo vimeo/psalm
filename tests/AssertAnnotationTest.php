e42d099d6f087776ac274ec4c8256877f439d345
m0003r <m0003r@gmail.com>
2020-04-09 23:31:40 +0300

    Fix $assertion_var_id calculation in method calls

--- tests/AssertAnnotationTest.php
+++ tests/AssertAnnotationTest.php
@@ -494,6 +494,44 @@ class AssertAnnotationTest extends TestCase
                         $t->bar();
                     }'
             ],
+            'assertThisTypeCombinedInsideMethod' => [
+                '<?php
+                    class Type {
+                        /**
+                         * @psalm-assert FooType $this
+                         */
+                        public function assertFoo() : void {
+                            if (!$this instanceof FooType) {
+                                throw new \Exception();
+                            }
+                        }
+                        
+                        /**
+                         * @psalm-assert BarType $this
+                         */
+                        public function assertBar() : void {
+                            if (!$this instanceof BarType) {
+                                throw new \Exception();
+                            }
+                        }
+                        
+                        function takesType(Type $t) : void {
+                            $t->assertFoo();
+                            $t->assertBar();
+                            $t->foo();
+                            $t->bar();
+                        }
+                    }
+
+                    interface FooType {
+                        public function foo(): void;
+                    }
+
+                    interface BarType {
+                        public function bar(): void;
+                    }
+'
+            ],
             'assertThisTypeSimpleCombined' => [
                 '<?php
                     class Type {
