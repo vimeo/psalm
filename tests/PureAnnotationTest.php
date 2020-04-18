53cd878694d680fb32be4a8d5be35c8eb21c2c64
Grégoire Paris <postmaster@greg0ire.fr>
2020-04-18 10:50:06 +0200

    Account for PDOException
    Probably for legacy reasons, PDOException::getCode() can return string (it's an
    SQLSTATE code). Note that if you instantiate it yourself, you will get
    an integer though.
    
    I changed the signature of the Exception stub accordingly, and then
    there were errors about incompatible signatures, so I had to change the
    signature of Throwable too, which makes sense since a Throwable could be
    a PDOException. The signature at
    https://www.php.net/manual/en/throwable.getcode.php seems plain wrong,
    and that same page of the docs warns about PDOException. Not sure what
    are the plans for future versions of PHP.

--- tests/PureAnnotationTest.php
+++ tests/PureAnnotationTest.php
@@ -214,8 +214,10 @@ class PureAnnotationTest extends TestCase
                 '<?php
                     /**
                      * @psalm-pure
+                     *
+                     * @return int|string https://www.php.net/manual/en/throwable.getcode.php
                      */
-                    function getCode(Throwable $e): int {
+                    function getCode(Throwable $e) {
                         return $e->getCode();
                     }
 
