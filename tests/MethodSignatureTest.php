e7b8983b7ea811ae95131134e0c4c40d6777e856
Grégoire Paris <postmaster@greg0ire.fr>
2020-04-20 23:17:47 +0200

    Make Throwable methods overridable (#3203)
    Methods from \Exception cannot be overriden, but that does not mean
    methods from \Throwable cannot be.
    
    Closes #3202

--- tests/MethodSignatureTest.php
+++ tests/MethodSignatureTest.php
@@ -773,6 +773,25 @@ class MethodSignatureTest extends TestCase
                 [],
                 '7.4'
             ],
+            'allowOverridingThrowable' => [
+                '<?php
+                    /**
+                     * @psalm-immutable
+                     */
+                    interface MyException extends \Throwable
+                    {
+                        /**
+                         * Informative comment
+                         */
+                        public function getMessage(): string;
+                        public function getCode();
+                        public function getFile(): string;
+                        public function getLine(): int;
+                        public function getTrace(): array;
+                        public function getPrevious(): ?\Throwable;
+                        public function getTraceAsString(): string;
+                    }'
+            ]
         ];
     }
 
