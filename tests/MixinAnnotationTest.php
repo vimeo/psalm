ebcb0b8cc48da269b19a470219e4d28ff9521c49
Brown <github@muglug.com>
2020-04-26 16:59:03 -0400

    Fix #3228 - support IteratorIterator wrapping

--- tests/MixinAnnotationTest.php
+++ tests/MixinAnnotationTest.php
@@ -109,6 +109,42 @@ class MixinAnnotationTest extends TestCase
                         }
                     }'
             ],
+            'wrapCustomIterator' => [
+                '<?php
+                    class Subject implements Iterator {
+                        /**
+                         * the index method exists
+                         *
+                         * @param int $index
+                         * @return bool
+                         */
+                        public function index($index) {
+                            return true;
+                        }
+
+                        public function current() {
+                            return 2;
+                        }
+
+                        public function next() {}
+
+                        public function key() {
+                            return 1;
+                        }
+
+                        public function valid() {
+                            return false;
+                        }
+
+                        public function rewind() {}
+                    }
+
+                    $iter = new IteratorIterator(new Subject());
+                    $b = $iter->index(0);',
+                [
+                    '$b' => 'bool',
+                ]
+            ],
         ];
     }
 }
