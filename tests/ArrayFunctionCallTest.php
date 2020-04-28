1fb1c2195d09d918842fff562694d4ddc78537a6
Grégoire Paris <postmaster@greg0ire.fr>
2020-04-28 04:49:07 +0200

    Allow non-string keys (#3221)
    Although it would be stupid to provide an array with exclusively
    non-string keys, it's possible to have an array with a bit of both.
    
    See for instance
    https://github.com/doctrine/dbal/blob/155d028be084ef69110d09938c57c351a2126e5d/tests/Doctrine/Tests/DBAL/Functional/DataAccessTest.php#L263-L276

--- tests/ArrayFunctionCallTest.php
+++ tests/ArrayFunctionCallTest.php
@@ -1342,6 +1342,12 @@ class ArrayFunctionCallTest extends TestCase
                         return "";
                     }'
             ],
+            'arrayChangeKeyCaseWithNonStringKeys' => [
+                '<?php
+
+                $a = [42, "A" => 42];
+                echo array_change_key_case($a, CASE_LOWER)[0];'
+            ],
         ];
     }
 
