ae48c1895f36d60fa977e13c017930e4be3d8e6e
Brown <github@muglug.com>
2020-05-14 22:09:37 -0400

    Fix #3365 - add support for negated empty string checks

--- tests/TypeReconciliation/ConditionalTest.php
+++ tests/TypeReconciliation/ConditionalTest.php
@@ -2755,6 +2755,24 @@ class ConditionalTest extends \Psalm\Tests\TestCase
                         return [$type];
                     }'
             ],
+            'nonEmptyStringAfterLiteralCheck' => [
+                '<?php
+                    /**
+                     * @param non-empty-string $greeting
+                     */
+                    function sayHi(string $greeting): void {
+                        echo $greeting;
+                    }
+
+                    /** @var string */
+                    $hello = "foo";
+
+                    if ($hello === "") {
+                        throw new \Exception("an empty string is not a greeting");
+                    }
+
+                    sayHi($hello);',
+            ],
         ];
     }
 
