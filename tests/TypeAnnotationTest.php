24b2dbc716a5a693185d222e5cef078a0b48eb59
Jefersson Nathan <malukenho.dev@gmail.com>
2020-05-16 19:08:45 +0200

    Add test for import ocross namespaces
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- tests/TypeAnnotationTest.php
+++ tests/TypeAnnotationTest.php
@@ -229,6 +229,30 @@ class TypeAnnotationTest extends TestCase
                         }
                     }'
             ],
+            'classTypeAliasFromExternalNamespace' => [
+                '<?php
+                namespace Foo {
+                    /** @psalm-type PhoneType = array{phone: string} */
+                    class Phone {
+                        /** @psalm-return PhoneType */
+                        public function toArray(): array {
+                            return ["phone" => "Nokia"];
+                        }
+                    }
+                }
+
+                namespace Bar {
+                    /**
+                     * @psalm-import-type PhoneType from \Foo\Phone
+                     */
+                    class User {
+                        /** @psalm-return PhoneType */
+                        function toArray(): array {
+                            return (new \Foo\Phone)->toArray();
+                        }
+                    }
+                }'
+            ],
         ];
     }
 
