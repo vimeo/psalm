9d9a6722747ff8d580fa12964e9d5e6d4e60a868
Jefersson Nathan <malukenho.dev@gmail.com>
2020-05-16 15:44:23 +0200

    Add test case scenario for @psalm-import-type
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- tests/TypeAnnotationTest.php
+++ tests/TypeAnnotationTest.php
@@ -189,6 +189,46 @@ class TypeAnnotationTest extends TestCase
                         }
                     }'
             ],
+            'classTypeAliasImportWithAlias' => [
+                '<?php
+                    /** @psalm-type PhoneType = array{phone: string} */
+                    class Phone {
+                        /** @psalm-return PhoneType */
+                        public function toArray(): array {
+                            return ["phone" => "Nokia"];
+                        }
+                    }
+
+                    /**
+                     * @psalm-import-type PhoneType from Phone as TPhone
+                     */
+                    class User {
+                        /** @psalm-return TPhone */
+                        function toArray(): array {
+                            return array_merge([], (new Phone)->toArray());
+                        }
+                    }'
+            ],
+            'classTypeAliasDirectUsage' => [
+                '<?php
+                    /** @psalm-type PhoneType = array{phone: string} */
+                    class Phone {
+                        /** @psalm-return PhoneType */
+                        public function toArray(): array {
+                            return ["phone" => "Nokia"];
+                        }
+                    }
+
+                    /**
+                     * @psalm-import-type PhoneType from Phone
+                     */
+                    class User {
+                        /** @psalm-return PhoneType */
+                        function toArray(): array {
+                            return array_merge([], (new Phone)->toArray());
+                        }
+                    }'
+            ],
         ];
     }
 
