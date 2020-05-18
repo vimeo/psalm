04a576708cc14c4541d050b054c606a6145199ac
Evgeniy <evgromr1@gmail.com>
2020-05-18 13:22:50 -0700

    Correct analyze clone expression (#3382)
    * Correct analyze clone, add PossibleInvalidClone issue type
    
    * Infer mixed type when possible incorrect clone
    
    * Remove unused variable

---
+++ src/Psalm/Issue/PossiblyInvalidClone.php
@@ -0,0 +1,8 @@
+<?php
+namespace Psalm\Issue;
+
+class PossiblyInvalidClone extends CodeIssue
+{
+    const ERROR_LEVEL = 3;
+    const SHORTCODE = 226;
+}
---
+++ tests/CloneTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psalm\Tests;
+
+class CloneTest extends TestCase
+{
+    use Traits\InvalidCodeAnalysisTestTrait;
+    use Traits\ValidCodeAnalysisTestTrait;
+
+    /**
+     * @return iterable<string,array{string,assertions?:array<string,string>,error_levels?:string[]}>
+     */
+    public function providerValidCodeParse()
+    {
+        return [
+            'cloneCorrect' => [
+                '<?php
+                    class A {}
+                    function foo(A $a) : A {
+                        return clone $a;
+                    }
+                    $a = foo(new A());',
+            ],
+            'cloneCorrectWithPublicMethod' => [
+                '<?php
+                    class A {
+                        public function __clone() {}
+                    }
+                    function foo(A $a) : A {
+                        return clone $a;
+                    }
+                    foo(new A());',
+            ],
+            'clonePrivateInternally' => [
+                '<?php
+                    class A {
+                        private function __clone() {}
+                        public function foo(): self {
+                            return clone $this;
+                        }
+                    }',
+            ],
+        ];
+    }
+
+    /**
+     * @return iterable<string,array{string,error_message:string,2?:string[],3?:bool,4?:string}>
+     */
+    public function providerInvalidCodeParse()
+    {
+        return [
+            'invalidIntClone' => [
+                '<?php
+                    $a = 5;
+                    clone $a;',
+                'error_message' => 'InvalidClone',
+            ],
+            'invalidMixedClone' => [
+                '<?php
+                    /** @var mixed $a */
+                    $a = 5;
+                    clone $a;',
+                'error_message' => 'PossiblyInvalidClone',
+            ],
+            'notVisibleCloneMethod' => [
+                '<?php
+                    class A {
+                        private function __clone() {}
+                    }
+                    $a = new A();
+                    clone $a;',
+                'error_message' => 'InvalidClone',
+            ],
+            'invalidGenericClone' => [
+                '<?php
+                    /**
+                     * @template T as int|string
+                     * @param T $a
+                     */
+                    function foo($a): void {
+                        clone $a;
+                    }',
+                'error_message' => 'InvalidClone',
+            ],
+            'possiblyInvalidGenericClone' => [
+                '<?php
+                    /**
+                     * @template T
+                     * @param T $a
+                     */
+                    function foo($a): void {
+                        clone $a;
+                    }',
+                'error_message' => 'PossiblyInvalidClone',
+            ],
+            'mixedTypeInferredIfErrors' => [
+                '<?php
+                    class A {}
+                    /**
+                     * @param A|string $a
+                     */
+                    function foo($a): void {
+                        /**
+                         * @psalm-suppress PossiblyInvalidClone
+                         */
+                        $cloned = clone $a;
+                    }',
+                'error_message' => 'MixedAssignment',
+            ]
+        ];
+    }
+}
