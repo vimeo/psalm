05962fd51c696879184635d577c8adab01acc387
Matthew Brown <github@muglug.com>
2020-06-06 12:11:33 -0400

    Update FakeFileReferenceCacheProvider.php

--- tests/Internal/Provider/FakeFileReferenceCacheProvider.php
+++ tests/Internal/Provider/FakeFileReferenceCacheProvider.php
@@ -55,6 +55,9 @@ class FakeFileReferenceCacheProvider extends \Psalm\Internal\Provider\FileRefere
      */
     private $cached_file_maps = [];
 
+    /** @var \Psalm\Config */
+    private $config;
+
     public function __construct() {
         $this->config = \Psalm\Config::getInstance();
     }
