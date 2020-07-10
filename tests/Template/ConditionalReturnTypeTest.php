8349564cc47dcfee7fb2745aef4f4eee403f66ad
Brown <github@muglug.com>
2020-07-10 13:19:23 -0400

    Fix #3790 - removing false/null from template not redundant

--- tests/Template/ConditionalReturnTypeTest.php
+++ tests/Template/ConditionalReturnTypeTest.php
@@ -588,12 +588,13 @@ class ConditionalReturnTypeTest extends TestCase
                         }
                     }'
             ],
-            'SKIPPED-checkNullOrFalse' => [
+            'checkNullOrFalse' => [
                 '<?php
                     /**
                      * @template T of mixed|false|null
                      * @param T $i
                      * @return (T is false ? no-return : T is null ? no-return : T)
+                     * @psalm-suppress LessSpecificReturnType
                      */
                     function orThrow($i) {
                         if ($i === false || $i === null) {
