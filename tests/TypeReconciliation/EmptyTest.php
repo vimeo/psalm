73a9831e202019de0f288be4b103b619971a1407
Yannick Gottschalk <ygottschalk@techfak.uni-bielefeld.de>
2020-07-18 15:03:54 +0200

    fixed test again

--- tests/TypeReconciliation/EmptyTest.php
+++ tests/TypeReconciliation/EmptyTest.php
@@ -254,6 +254,7 @@ class EmptyTest extends \Psalm\Tests\TestCase
 
                         while (!empty($needle)) {
                             $key = key($needle);
+                            if ($key === null) continue;
                             $val = $needle[$key];
                             unset($needle[$key]);
 
