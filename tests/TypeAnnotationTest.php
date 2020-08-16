38af5db8f04cacf45a356fcf32234ef43d6f7852
Matthew Brown <github@muglug.com>
2020-08-16 16:12:29 -0400

    Fix #3929 - merge expanded types where necessary

--- tests/TypeAnnotationTest.php
+++ tests/TypeAnnotationTest.php
@@ -397,6 +397,42 @@ class TypeAnnotationTest extends TestCase
                     }',
                 'error_message' => 'InvalidDocblock',
             ],
+            'mergeImportedTypes' => [
+                '<?php
+                    namespace A\B;
+
+                    /**
+                     * @psalm-type _A=array{
+                     *      id:int
+                     * }
+                     *
+                     * @psalm-type _B=array{
+                     *      id:int,
+                     *      something:int
+                     * }
+                     */
+                    class Types
+                    {
+                    }
+
+                    namespace A;
+
+                    /**
+                     * @psalm-import-type _A from \A\B\Types as _AA
+                     * @psalm-import-type _B from \A\B\Types as _BB
+                     */
+                    class Id
+                    {
+                        /**
+                         * @psalm-param _AA|_BB $_item
+                         */
+                        public function ff(array $_item): int
+                        {
+                            return $_item["something"];
+                        }
+                    }',
+                'error_message' => 'PossiblyUndefinedArrayOffset'
+            ],
         ];
     }
 }
