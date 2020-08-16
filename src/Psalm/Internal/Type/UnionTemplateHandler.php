a356a43a510b57b18cb5c9161bc87e2c148c851b
Matthew Brown <github@muglug.com>
2020-08-16 12:26:07 -0400

    Fix #3964 - prevent incorrect intersection of unions

--- src/Psalm/Internal/Type/UnionTemplateHandler.php
+++ src/Psalm/Internal/Type/UnionTemplateHandler.php
@@ -704,11 +704,23 @@ class UnionTemplateHandler
                 }
 
                 if (isset($template_result->lower_bounds[$param_name_key][$atomic_type->defining_class][0])) {
-                    $intersection_type = \Psalm\Type::intersectUnionTypes(
+                    $intersection_type = null;
+
+                    if (!UnionTypeComparator::isContainedBy(
+                        $codebase,
                         $template_result->lower_bounds[$param_name_key][$atomic_type->defining_class][0],
+                        $generic_param
+                    ) || !UnionTypeComparator::isContainedBy(
+                        $codebase,
                         $generic_param,
-                        $codebase
-                    );
+                        $template_result->lower_bounds[$param_name_key][$atomic_type->defining_class][0]
+                    )) {
+                        $intersection_type = \Psalm\Type::intersectUnionTypes(
+                            $template_result->lower_bounds[$param_name_key][$atomic_type->defining_class][0],
+                            $generic_param,
+                            $codebase
+                        );
+                    }
 
                     if ($intersection_type) {
                         $template_result->lower_bounds[$param_name_key][$atomic_type->defining_class][0]
--- tests/Template/FunctionTemplateTest.php
+++ tests/Template/FunctionTemplateTest.php
@@ -1337,6 +1337,25 @@ class FunctionTemplateTest extends TestCase
                 [],
                 '7.4'
             ],
+            'closureParamConstraintsMet' => [
+                '<?php
+                    class A {}
+                    class B {}
+
+                    $test = [new A(), new B()];
+
+                    usort(
+                        $test,
+                        /**
+                         * @param A|B $a
+                         * @param A|B $b
+                         */
+                        function($a, $b): int
+                        {
+                            return $a === $b ? 1 : -1;
+                        }
+                    );'
+            ],
         ];
     }
 
