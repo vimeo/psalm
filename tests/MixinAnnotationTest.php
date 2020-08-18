ed8a9c5a99471ed02b5fa08a2103f76f3d7b3ee6
Daniel Melchior <daniel.melchior@interweber.de>
2020-08-18 13:31:08 +0200

    fix #4013: prevent exception when two mixins declare methods with same name
    fixes #4013

--- tests/MixinAnnotationTest.php
+++ tests/MixinAnnotationTest.php
@@ -533,6 +533,38 @@ class MixinAnnotationTest extends TestCase
                         return (new FooGrandChild)->other();
                     }'
             ],
+            'multipleMixinsWithSameMethod' => [
+                '<?php
+
+                    class Mix1
+                    {
+                        public function foo(): string
+                        {
+                            return "";
+                        }
+                    }
+
+                    class Mix2
+                    {
+                        public function foo(): string
+                        {
+                            return "";
+                        }
+                    }
+
+                    /**
+                     * @mixin Mix1
+                     * @mixin Mix2
+                     */
+                    class Bar
+                    {
+                    
+                    }
+
+                    $bar = new Bar();
+
+                    $bar->foo();'
+            ],
         ];
     }
 
