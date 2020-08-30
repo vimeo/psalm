f34e54ec4115c4b364b2a2a0c076bdfe5146509b
Brown <github@muglug.com>
2020-08-30 13:16:37 -0400

    Support PHP 8 union types

--- tests/PropertyTypeTest.php
+++ tests/PropertyTypeTest.php
@@ -2059,6 +2059,22 @@ class PropertyTypeTest extends TestCase
                         }
                     }'
             ],
+            'unionPropertyType' => [
+                '<?php
+                    class A {
+                        public string|int $i;
+
+                        public function __construct() {
+                            $this->i = 5;
+                            $this->i = "hello";
+                        }
+                    }
+
+                    $a = new A();
+
+                    if ($a->i === 3) {}
+                    if ($a->i === "foo") {}'
+            ],
         ];
     }
 
