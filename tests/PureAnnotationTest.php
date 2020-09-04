f4c2edf40b294dbc3754b04322b9784247fff5bd
Brown <github@muglug.com>
2020-09-04 16:46:20 -0400

    Fix #4132 - ignore purity of $this when checking for initialisation

--- tests/PureAnnotationTest.php
+++ tests/PureAnnotationTest.php
@@ -387,6 +387,27 @@ class PureAnnotationTest extends TestCase
                         return false;
                     }',
             ],
+            'allowPureInConstrucctorThis' => [
+                '<?php
+                    class Port {
+                       private int $portNumber;
+
+                       public function __construct(int $portNumber) {
+                          if (!$this->isValidPort($portNumber)) {
+                             throw new Exception();
+                          }
+
+                          $this->portNumber = $portNumber;
+                       }
+
+                       /**
+                        * @psalm-pure
+                        */
+                       private function isValidPort(int $portNumber): bool {
+                          return $portNumber >= 1 && $portNumber <= 1000;
+                       }
+                    }'
+            ],
         ];
     }
 
