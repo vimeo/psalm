e472600513c54d048a9d3dc61d2cb9b25d19501d
Brown <github@muglug.com>
2020-09-13 23:28:31 -0400

    Fix a few more things

--- tests/ReturnTypeTest.php
+++ tests/ReturnTypeTest.php
@@ -1267,6 +1267,22 @@ class ReturnTypeTest extends TestCase
                     }',
                 'error_message' => 'LessSpecificReturnStatement',
             ],
+            'docblockishTypeMustReturn' => [
+                '<?php
+                    /**
+                     * @return "a"|"b"|null
+                     */
+                    function foo() : ?string {
+                        if (rand(0, 1)) {
+                            return "a";
+                        }
+
+                        if (rand(0, 1)) {
+                            return "b";
+                        }
+                    }',
+                'error_message' => 'InvalidReturnType',
+            ],
         ];
     }
 }
