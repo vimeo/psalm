a8a2a4cb66feabe4d0cc433dbb2f3f5979c4cc12
Brown <github@muglug.com>
2020-09-19 14:18:52 -0400

    Fix #4210 - fix __invoke declaration tracing

--- tests/CallableTest.php
+++ tests/CallableTest.php
@@ -848,6 +848,22 @@ class CallableTest extends TestCase
 
                     foo(["a", "b"]);'
             ],
+            'abstractInvokeInTrait' => [
+                '<?php
+                    function testFunc(callable $func) : void {}
+
+                    trait TestTrait {
+                        abstract public function __invoke() : void;
+
+                        public function apply() : void {
+                            testFunc($this);
+                        }
+                    }
+
+                    abstract class TestClass {
+                        use TestTrait;
+                    }'
+            ],
         ];
     }
 
