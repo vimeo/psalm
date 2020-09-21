6334c50c07334def8a5019d079ee95b0d9bc7689
Brown <github@muglug.com>
2020-09-20 21:25:35 -0400

    Add back necessary suppression

--- tests/UnusedVariableTest.php
+++ tests/UnusedVariableTest.php
@@ -1293,6 +1293,7 @@ class UnusedVariableTest extends TestCase
                      * @psalm-suppress MixedMethodCall
                      * @psalm-suppress MissingParamType
                      * @psalm-suppress MixedArgument
+                     * @psalm-suppress PossiblyNullArgument
                      */
                     function foo($a): void {
                         $m = $_GET["m"] ?? null;
