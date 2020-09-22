8da6feb7b72bd8ee8862d5d9aefedea9670bed81
aheart <aheart@users.noreply.github.com>
2020-09-22 20:44:14 +0300

    Filter selected text to prevent invalid XML (#4234)

--- src/Psalm/Report/JunitReport.php
+++ src/Psalm/Report/JunitReport.php
@@ -168,7 +168,7 @@ class JunitReport extends Report
         $ret = 'message: ' . htmlspecialchars(trim($data->message), ENT_XML1 | ENT_QUOTES) . "\n";
         $ret .= 'type: ' . trim($data->type) . "\n";
         $ret .= 'snippet: ' . htmlspecialchars(trim($data->snippet), ENT_XML1 | ENT_QUOTES) . "\n";
-        $ret .= 'selected_text: ' . trim($data->selected_text) . "\n";
+        $ret .= 'selected_text: ' . htmlspecialchars(trim($data->selected_text)) . "\n";
         $ret .= 'line: ' . $data->line_from . "\n";
         $ret .= 'column_from: ' . $data->column_from . "\n";
         $ret .= 'column_to: ' . $data->column_to . "\n";
