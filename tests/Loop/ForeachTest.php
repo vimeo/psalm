7f975164614fbc0ad952d5a914ea6e08aaa7add1
Brown <github@muglug.com>
2020-09-27 17:50:48 -0400

    Ensure loop marks vars as possibly assigned

--- tests/Loop/ForeachTest.php
+++ tests/Loop/ForeachTest.php
@@ -1057,6 +1057,21 @@ class ForeachTest extends \Psalm\Tests\TestCase
                         }
                     }'
             ],
+            'loopCanUpdateOuter' => [
+                '<?php
+                    /**
+                     * @param array<int> $mappings
+                     */
+                    function foo(string $id, array $mappings) : void {
+                        if ($id === "a") {
+                            foreach ($mappings as $value) {
+                                $id = $value;
+                            }
+                        }
+
+                        if (is_int($id)) {}
+                    }'
+            ],
         ];
     }
 
