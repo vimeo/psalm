dfa885435474d41498e211a78b69132a3bf0d367
Matt Brown <github@muglug.com>
2020-10-12 14:45:11 -0400

    Fix #4310 - prevent literal class check on union

--- tests/Template/ConditionalReturnTypeTest.php
+++ tests/Template/ConditionalReturnTypeTest.php
@@ -621,6 +621,39 @@ class ConditionalReturnTypeTest extends TestCase
                         }
                     }'
             ],
+            'stringOrClassStringT' => [
+                '<?php
+                    class A {}
+
+                    /**
+                     * @template T
+                     * @param string|class-string<T> $name
+                     * @return ($name is class-string ? T : mixed)
+                     */
+                    function get(string $name) {
+                        return;
+                    }
+
+                    $lowercase_a = "a";
+
+                    /** @var class-string $class_string */
+                    $class_string = "b";
+
+                    /** @psalm-suppress MixedAssignment */
+                    $expect_mixed = get($lowercase_a);
+                    $expect_object = get($class_string);
+
+                    $expect_a_object = get(A::class);
+
+                    /** @psalm-suppress MixedAssignment */
+                    $expect_mixed_from_literal = get("LiteralDirect");',
+                [
+                    '$expect_mixed' => 'mixed',
+                    '$expect_object' => 'object',
+                    '$expect_a_object' => 'A',
+                    '$expect_mixed_from_literal' => 'mixed',
+                ]
+            ],
         ];
     }
 }
