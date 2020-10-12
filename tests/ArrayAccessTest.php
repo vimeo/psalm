5743471a197638868da82220faef4b4f087dadaf
Matt Brown <github@muglug.com>
2020-10-12 12:45:31 -0400

    Don’t merge array and Traversable to iterable when there are intersections

--- tests/ArrayAccessTest.php
+++ tests/ArrayAccessTest.php
@@ -964,6 +964,18 @@ class ArrayAccessTest extends TestCase
                         }
                     }'
             ],
+            'arrayAccessOnArraylikeObjectOrArray' => [
+                '<?php
+                    /**
+                     * @param arraylike-object<int, string>|array<int, string> $arr
+                     */
+                    function test($arr): string {
+                        return $arr[0] ?? "";
+                    }
+
+                    test(["a", "b"]);
+                    test(new ArrayObject(["a", "b"]));'
+            ],
         ];
     }
 
