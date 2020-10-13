bb7d7132f790a24d34aa8420620b4b53b0db423e
Matt Brown <github@muglug.com>
2020-10-12 20:25:46 -0400

    Do arithmetic for preg_split arguments

--- tests/FunctionCallTest.php
+++ tests/FunctionCallTest.php
@@ -1362,6 +1362,13 @@ class FunctionCallTest extends TestCase
                         return preg_split("/ /", $s);
                     }'
             ],
+            'pregSplitWithFlags' => [
+                '<?php
+                    /** @return list<string> */
+                    function foo(string $s) {
+                        return preg_split("/ /", $s, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+                    }'
+            ],
             'mbConvertEncodingWithArray' => [
                 '<?php
                     /**
