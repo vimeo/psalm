76986eb0036ee9e1873bc5203a144adc9303d214
Matt Brown <github@muglug.com>
2020-10-16 17:11:37 -0400

    Fix #4350 - numeric-string should not get replaced

--- src/Psalm/Type/Atomic/TFloat.php
+++ src/Psalm/Type/Atomic/TFloat.php
@@ -12,4 +12,17 @@ class TFloat extends Scalar
     {
         return 'float';
     }
+
+    /**
+     * @param  array<string> $aliased_classes
+     */
+    public function toPhpString(
+        ?string $namespace,
+        array $aliased_classes,
+        ?string $this_class,
+        int $php_major_version,
+        int $php_minor_version
+    ): ?string {
+        return $php_major_version >= 7 ? 'float' : null;
+    }
 }
