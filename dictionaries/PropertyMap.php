d313f570610d55f2eafbdef5a4f0aac2890763a4
Benjamin Morel <benjamin.morel@gmail.com>
2020-11-17 03:48:04 +0100

    mysqli::$insert_id can be a string (#4577)

--- dictionaries/PropertyMap.php
+++ dictionaries/PropertyMap.php
@@ -260,7 +260,7 @@ return [
         'field_count' => 'int',
         'host_info' => 'string',
         'info' => 'string',
-        'insert_id' => 'int',
+        'insert_id' => 'int|string',
         'protocol_version' => 'string',
         'server_info' => 'string',
         'server_version' => 'int',
