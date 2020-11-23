28b17cb30251fd851753d2937321b37327d77641
Matthew Brown <github@muglug.com>
2020-11-23 09:53:27 -0500

    newline

--- src/Psalm/Internal/Codebase/Functions.php
+++ src/Psalm/Internal/Codebase/Functions.php
@@ -286,7 +286,8 @@ class Functions
             'fopen', 'fread', 'fwrite', 'fclose', 'touch', 'fpassthru', 'fputs', 'fscanf', 'fseek',
             'ftruncate', 'fprintf', 'symlink', 'mkdir', 'unlink', 'rename', 'rmdir', 'popen', 'pclose',
             'fgetcsv', 'fputcsv', 'umask', 'finfo_close', 'readline_add_history', 'stream_set_timeout',
-            'fgets', 'fflush', 'move_uploaded_file', 'file_exists', 'realpath', 'glob', 'is_readable', 'is_dir', 'is_file',
+            'fgets', 'fflush', 'move_uploaded_file', 'file_exists', 'realpath', 'glob', 'is_readable',
+            'is_dir', 'is_file',
 
             // stream/socket io
             'stream_context_set_option', 'socket_write', 'stream_set_blocking', 'socket_close',
