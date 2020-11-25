b9e9411363ab5246c2b0f550fff72ee68b50399b
Markus Staab <m.staab@complex-it.de>
2020-11-25 08:59:42 +0100

    fix stub param

--- stubs/pdo.php
+++ stubs/pdo.php
@@ -3,8 +3,8 @@
 class PdoStatement {
     /**
      * @template T
-     * @param T $class
-     * @return T
+     * @param class-string<T> $class
+     * @return false|T
      */
     public function fetchObject($class = "stdclass") {}
 }
