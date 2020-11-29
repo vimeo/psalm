621c4dd8d18010a36703a84bca50876fb958f810
Benjamin Morel <benjamin.morel@gmail.com>
2020-11-29 21:58:04 +0100

    Enforce class-string in is_subclass_of()

--- tests/ClassLikeStringTest.php
+++ tests/ClassLikeStringTest.php
@@ -387,25 +387,6 @@ class ClassLikeStringTest extends TestCase
                         return $s;
                     }',
             ],
-            'createClassOfTypeFromStringUsingIsSubclassOfString' => [
-                '<?php
-                    class A {}
-
-                    /**
-                     * @return class-string<A> $s
-                     */
-                    function foo(string $s) : string {
-                        if (!class_exists($s)) {
-                            throw new \UnexpectedValueException("bad");
-                        }
-
-                        if (!is_subclass_of($s, "\A")) {
-                            throw new \UnexpectedValueException("bad");
-                        }
-
-                        return $s;
-                    }',
-            ],
             'checkSubclassOfAbstract' => [
                 '<?php
                     interface Foo {
