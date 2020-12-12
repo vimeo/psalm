80adb84bf697127dc83bd47c2f9cfa03083a392b
orklah <ronan_morin@hotmail.com>
2020-12-12 19:50:23 +0100

    remove unused variable

--- examples/TemplateChecker.php
+++ examples/TemplateChecker.php
@@ -18,7 +18,7 @@ class TemplateAnalyzer extends Psalm\Internal\Analyzer\FileAnalyzer
 {
     const VIEW_CLASS = 'Your\\View\\Class';
 
-    public function analyze(?Context $file_context = null, bool $preserve_analyzers = false, ?Context $global_context = null): void
+    public function analyze(?Context $file_context = null, ?Context $global_context = null): void
     {
         $codebase = $this->project_analyzer->getCodebase();
         $stmts = $codebase->getStatementsForFile($this->file_path);
