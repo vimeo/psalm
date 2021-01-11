7f9712dc92445997b056f0759efee961a57b58da
Bruce Weirdan <weirdan@gmail.com>
2021-01-11 20:16:44 +0200

    Added GHA workflow to build phars (#4974)

--- bin/build-phar.sh
+++ bin/build-phar.sh
@@ -10,8 +10,10 @@ php $DIR/improve_class_alias.php
 
 vendor/bin/box compile
 
+SIGNING_KEY_ID=${SIGNING_KEY_ID:-8A03EA3B385DBAA1}
+
 if [[ "$GPG_ENCRYPTION" != '' ]] ; then
-    echo $GPG_ENCRYPTION | gpg --passphrase-fd 0 keys.asc.gpg
+    echo "$GPG_ENCRYPTION" | gpg --batch --passphrase-fd 0 keys.asc.gpg
     gpg --batch --yes --import keys.asc
-    echo $SIGNING_KEY | gpg --passphrase-fd 0 -u 8A03EA3B385DBAA1 --armor --detach-sig build/psalm.phar
+    echo "$SIGNING_KEY" | gpg --batch --passphrase-fd 0 --pinentry-mode loopback -u "$SIGNING_KEY_ID" --armor --detach-sig build/psalm.phar
 fi
