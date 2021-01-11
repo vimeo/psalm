00f3ccbb3aeb78340703a66cfc99a234038a2a2f
Matt Brown <github@muglug.com>
2021-01-11 14:37:17 -0500

    Maybe fix signing

--- bin/build-phar.sh
+++ bin/build-phar.sh
@@ -10,10 +10,7 @@ php $DIR/improve_class_alias.php
 
 vendor/bin/box compile
 
-SIGNING_KEY_ID=${SIGNING_KEY_ID:-8A03EA3B385DBAA1}
-
-if [[ "$GPG_ENCRYPTION" != '' ]] ; then
-    echo "$GPG_ENCRYPTION" | gpg --batch --passphrase-fd 0 keys.asc.gpg
-#    gpg --batch --yes --import keys.asc
-    echo "$SIGNING_KEY" | gpg --batch --passphrase-fd 0 --pinentry-mode loopback -u "$SIGNING_KEY_ID" --armor --detach-sig build/psalm.phar
+if [[ "$GPG_SIGNING" != '' ]] ; then
+    echo "$SIGNING_KEY" | gpg --import --no-tty --batch --yes
+    gpg --command-fd 0 --pinentry-mode loopback -u "$SIGNING_KEY_ID" --batch --detach-sign --armor --output build/psalm.phar.asc build/psalm.phar
 fi
