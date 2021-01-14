92481f851d237a9e5f7ebd22fa199ed69960cc8c
Matthew Brown <github@muglug.com>
2021-01-14 16:33:38 -0500

    Improve error message

--- src/Psalm/Internal/ExecutionEnvironment/SystemCommandExecutor.php
+++ src/Psalm/Internal/ExecutionEnvironment/SystemCommandExecutor.php
@@ -22,8 +22,8 @@ final class SystemCommandExecutor
      */
     public function execute(string $command) : array
     {
-        if (!function_exists('exec')) {
-            throw new \RuntimeException(sprintf('Failed to execute command: %s', $command));
+        if (!\function_exists('exec')) {
+            throw new \RuntimeException(sprintf('exec does not exist, failed to execute command: %s', $command));
         };
 
         exec($command, $result, $returnValue);
