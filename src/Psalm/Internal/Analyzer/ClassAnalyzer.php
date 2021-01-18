5b354c8905c4e297b7df7d13d924758eef581e43
nicoorfi <nico_orfi@yahoo.com>
2021-01-18 15:39:56 +0100

    WIP

--- src/Psalm/Internal/Analyzer/ClassAnalyzer.php
+++ src/Psalm/Internal/Analyzer/ClassAnalyzer.php
@@ -1556,22 +1556,22 @@ class ClassAnalyzer extends ClassLikeAnalyzer
             if ($codebase->alter_code && $this->class->name && $codebase->classes_to_move) {
                 if (isset($codebase->classes_to_move[strtolower($fq_trait_name)])) {
 
-                    // $destination_trait = $codebase->classes_to_move[strtolower($fq_trait_name)];
+                    $destination_trait = $codebase->classes_to_move[strtolower($fq_trait_name)];
 
-                    // $bounds = $trait_location->getSelectionBounds();
+                    $bounds = $trait_location->getSelectionBounds();
 
-                    //         $file_manipulations = [
-                    //             new \Psalm\FileManipulation(
-                    //                 $bounds[0],
-                    //                 $bounds[1],
-                    //                 Type::getStringFromFQCLN($destination_trait, null, [], null)
-                    //             )
-                    //         ];
+                            $file_manipulations = [
+                                new \Psalm\FileManipulation(
+                                    $bounds[0],
+                                    $bounds[1],
+                                    Type::getStringFromFQCLN($destination_trait, null, [], null)
+                                )
+                            ];
 
-                    //         \Psalm\Internal\FileManipulation\FileManipulationBuffer::add(
-                    //             $this->getFilePath(),
-                    //             $file_manipulations
-                    //         );
+                            \Psalm\Internal\FileManipulation\FileManipulationBuffer::add(
+                                $this->getFilePath(),
+                                $file_manipulations
+                            );
                 }
             }
 
