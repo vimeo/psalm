2b2d1c10600955517638ca9789b350411fe79770
Matt Brown <github@muglug.com>
2021-02-08 09:44:46 -0500

    Add null check

--- src/Psalm/Internal/Provider/ReturnTypeProvider/ArrayMapReturnTypeProvider.php
+++ src/Psalm/Internal/Provider/ReturnTypeProvider/ArrayMapReturnTypeProvider.php
@@ -373,12 +373,14 @@ class ArrayMapReturnTypeProvider implements \Psalm\Plugin\EventHandler\FunctionR
 
                         $callable_type = $statements_source->node_data->getType($function_call_arg->value);
 
-                        foreach ($callable_type->getAtomicTypes() as $atomic_type) {
-                            if ($atomic_type instanceof Type\Atomic\TKeyedArray
-                                && \count($atomic_type->properties) === 2
-                                && isset($atomic_type->properties[0])
-                            ) {
-                                $lhs_instance_type = clone $atomic_type->properties[0];
+                        if ($callable_type) {
+                            foreach ($callable_type->getAtomicTypes() as $atomic_type) {
+                                if ($atomic_type instanceof Type\Atomic\TKeyedArray
+                                    && \count($atomic_type->properties) === 2
+                                    && isset($atomic_type->properties[0])
+                                ) {
+                                    $lhs_instance_type = clone $atomic_type->properties[0];
+                                }
                             }
                         }
 
