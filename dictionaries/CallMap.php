5205ed402ed7e45b15be187100907080bd0cd67d
Alies Lapatsin <web.lapateen@gmail.com>
2021-02-12 14:41:37 +0300

    Remove ReflectionProperty::getType from common CallMap
    It added in PHP7.4 only
    @see https://www.php.net/manual/en/reflectionproperty.gettype.php

--- dictionaries/CallMap.php
+++ dictionaries/CallMap.php
@@ -11563,7 +11563,6 @@ return [
 'ReflectionProperty::getDocComment' => ['string|false'],
 'ReflectionProperty::getModifiers' => ['int'],
 'ReflectionProperty::getName' => ['string'],
-'ReflectionProperty::getType' => ['?ReflectionType'],
 'ReflectionProperty::getValue' => ['mixed', 'object='=>'object'],
 'ReflectionProperty::isDefault' => ['bool'],
 'ReflectionProperty::isPrivate' => ['bool'],
