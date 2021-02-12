144bb37f768d39d51c07eede3a7198300d123db7
Alberto Piai <apiai@sensational.ch>
2021-02-12 23:00:38 +0100

    Fix array_key_exists() with all int literal keys (#5197)
    When checking code like the following:
    
    ```
    <?php
    
    function checkNegated(string $key): void {
        $arr = [
            0 => "foo",
            1 => "bar",
        ];
    
        if (!array_key_exists($key, $arr)) {
            printf("not found\n");
        }
    }
    
    function check(string $key): void {
        $arr = [
            0 => "foo",
            1 => "bar",
        ];
    
        if (array_key_exists($key, $arr)) {
            printf("found\n");
        }
    }
    ```
    
    the `if` in `checkNegated` would cause:
    
    ```
    ERROR: RedundantCondition - 9:10 - Type string for $key is never =int(0)
    ```
    
    This happens when the array keys are all int literals, but the "needle"
    is a string.
    
    `array_key_exists()` uses a loose equality comparison, but the generated
    assertions for this specific case
    (`AssertionFinder::getArrayKeyExistsAssertions`) was generating strict
    equality clauses. This commit fixes it by changing the generated clause
    from `=` to `~`.

--- tests/TypeReconciliation/ArrayKeyExistsTest.php
+++ tests/TypeReconciliation/ArrayKeyExistsTest.php
@@ -327,6 +327,34 @@ class ArrayKeyExistsTest extends \Psalm\Tests\TestCase
                         return 1;
                     }'
             ],
+            'comparesStringAndAllIntKeysCorrectly' => [
+                '<?php
+                    /**
+                     * @param array<1|2|3, string> $arr
+                     * @return bool
+                     */
+                    function checkArrayKeyExistsComparison(array $arr, string $key): bool
+                    {
+                        if (array_key_exists($key, $arr)) {
+                            return true;
+                        }
+                        return false;
+                    }'
+            ],
+            'comparesStringAndAllIntKeysCorrectlyNegated' => [
+                '<?php
+                    /**
+                     * @param array<1|2|3, string> $arr
+                     * @return bool
+                     */
+                    function checkArrayKeyExistsComparisonNegated(array $arr, string $key): bool
+                    {
+                        if (!array_key_exists($key, $arr)) {
+                            return false;
+                        }
+                        return true;
+                    }'
+            ],
         ];
     }
 
