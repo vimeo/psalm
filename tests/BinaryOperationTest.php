2f2ee0008c7430263ea3e211919267f886480859
orklah <ronan_morin@hotmail.com>
2021-02-15 22:26:46 +0100

    add test

--- tests/BinaryOperationTest.php
+++ tests/BinaryOperationTest.php
@@ -357,6 +357,37 @@ class BinaryOperationTest extends TestCase
                      function foo($a, $b): void {
                          onlyZeroOrPlusMinusOne($a <=> $b);
                      }'
+            ],
+            'notAlwaysPositiveBitOperations' => [
+                '<?php
+
+                    $a = 1;
+                    $b = 1;
+                    $c = 32;
+                    $d = 64;
+                    $e = 2;
+
+                    if (0 === ($a ^ $b)) {
+                        echo "Actually, zero\n";
+                    }
+
+                    if (0 === ($a & $e)) {
+                        echo "Actually, zero\n";
+                    }
+
+                    if (0 === ($a >> $b)) {
+                        echo "Actually, zero\n";
+                    }
+
+                    if (8 === PHP_INT_SIZE) {
+                        if (0 === ($a << $d)) {
+                            echo "Actually, zero\n";
+                        }
+                    } else {
+                        if (0 === ($a << $c)) {
+                            echo "Actually, zero\n";
+                        }
+                    }'
             ]
         ];
     }
