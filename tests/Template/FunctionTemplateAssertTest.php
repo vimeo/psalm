686f9fcaaa2bd08a3f5d2ec6b5f97dbc3c51386d
Matt Brown <github@muglug.com>
2021-02-16 20:24:16 -0500

    Don’t throw an exceptin when parsing invalid type

--- tests/Template/FunctionTemplateAssertTest.php
+++ tests/Template/FunctionTemplateAssertTest.php
@@ -986,6 +986,26 @@ class FunctionTemplateAssertTest extends TestCase
                     }',
                 'error_message' => 'string, string',
             ],
+            'noCrashWhenOnUnparseableTemplatedAssertion' => [
+                '<?php
+                    /**
+                     * @template TCandidateKey as array-key
+                     * @param array $arr
+                     * @param TCandidateKey $key
+                     * @psalm-assert has-array-key<TCandidateKey> $arr
+                     */
+                    function keyExists(array $arr, $key) : void {
+                        if (!array_key_exists($key, $arr)) {
+                            throw new \Exception("bad");
+                        }
+                    }
+
+                    function fromArray(array $data) : void {
+                        keyExists($data, "id");
+                        if (is_string($data["id"])) {}
+                    }',
+                'error_message' => 'InvalidDocblock',
+            ],
         ];
     }
 }
