c9a95187708ae15e0c947750de98b8ce29fc95c7
Matthew Brown <github@muglug.com>
2021-02-24 10:12:25 -0500

    Break up long array docblocks

--- src/Psalm/Internal/PhpVisitor/Reflector/FunctionLikeDocblockScanner.php
+++ src/Psalm/Internal/PhpVisitor/Reflector/FunctionLikeDocblockScanner.php
@@ -363,7 +363,10 @@ class FunctionLikeDocblockScanner
      * @param  array<string, TypeAlias>|null   $type_aliases
      * @param  array<string, array<string, Type\Union>> $function_template_types
      *
-     * @return array{array<int, array{0: string, 1: int, 2?: string}>, array<string, array<string, Type\Union>>}
+     * @return array{
+     *     array<int, array{0: string, 1: int, 2?: string}>,
+     *     array<string, array<string, Type\Union>>
+     * }
      */
     private static function getConditionalSanitizedTypeTokens(
         string $docblock_return_type,
@@ -568,7 +571,17 @@ class FunctionLikeDocblockScanner
      * @param array<string, array<string, Type\Union>> $class_template_types
      * @param array<string, non-empty-array<string, Type\Union>> $function_template_types
      * @param array<string, TypeAlias> $type_aliases
-     * @param array<int, array{type:string,name:string,line_number:int,start:int,end:int,description?:string}> $docblock_params
+     * @param array<
+     *     int,
+     *     array{
+     *         type:string,
+     *         name:string,
+     *         line_number:int,
+     *         start:int,
+     *         end:int,
+     *         description?:string
+     *     }
+     * > $docblock_params
      */
     private static function improveParamsFromDocblock(
         Codebase $codebase,
