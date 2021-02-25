fff32d0443b6e99e6fbdec43bd85a5bbe00e9c6b
elnoro <karimov.work@gmail.com>
2021-02-26 03:42:20 +0500

    added test for multiline templates (#5282)

--- tests/Template/ClassTemplateTest.php
+++ tests/Template/ClassTemplateTest.php
@@ -3322,6 +3322,27 @@ class ClassTemplateTest extends TestCase
                         takesA($a);
                     }',
             ],
+            'templateIsAComplexMultilineType' => [
+                '<?php
+                /**
+                 * @template T of array{
+                 *    a: string,
+                 *    b: int
+                 * }
+                 */
+                class MyContainer {
+                    /** @var T */
+                    private $value;
+                    /** @param T $value */
+                    public function __construct($value) {
+                        $this->value = $value;
+                    }
+                    /** @return T */
+                    public function getValue() {
+                      return $this->value;
+                    }
+                }'
+            ],
         ];
     }
 
