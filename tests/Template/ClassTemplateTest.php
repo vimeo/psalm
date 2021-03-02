ba8779ee305e8f8610db8bbf08109a6811cc1c74
Bruce Weirdan <weirdan@gmail.com>
2021-03-03 01:27:47 +0200

    Make WeakMap generic

--- tests/Template/ClassTemplateTest.php
+++ tests/Template/ClassTemplateTest.php
@@ -3355,6 +3355,52 @@ class ClassTemplateTest extends TestCase
                     '$f' => 'Foo<object>'
                 ]
             ],
+            'PHP80-weakmapIsGeneric' => [
+                '<?php
+                    /** @param WeakMap<Throwable,int> $wm */
+                    function isCountable(WeakMap $wm): int {
+                        return count($wm);
+                    }
+
+                    /**
+                     * @param WeakMap<Throwable,int> $wm
+                     * @return array{Throwable,int}
+                     */
+                    function isTraverable(WeakMap $wm): array {
+                        foreach ($wm as $k => $v) {
+                            return [$k, $v];
+                        }
+                        throw new RuntimeException;
+                    }
+
+                    /**
+                     * @param WeakMap<Throwable,int> $wm
+                     * @return Traversable<Throwable,int>
+                     */
+                    function hasAggregateIterator(WeakMap $wm): Traversable {
+                        return $wm->getIterator();
+                    }
+
+                    /**
+                     * @param WeakMap<Throwable,int> $wm
+                     */
+                    function readsLikeAnArray(WeakMap $wm): int {
+                        $ex = new Exception;
+                        if (isset($wm[$ex])) {
+                            return $wm[$ex];
+                        }
+                        throw new RuntimeException;
+                    }
+
+                    /**
+                     * @param WeakMap<Throwable,int> $wm
+                     */
+                    function writesLikeAnArray(WeakMap $wm): void {
+                        $ex = new Exception;
+                        $wm[$ex] = 42;
+                    }
+                ',
+            ]
         ];
     }
 
