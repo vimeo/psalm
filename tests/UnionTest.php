a46c69ec719003e780b161ed4ab819ba2baf2e20
Bruce Weirdan <weirdan@gmail.com>
2021-03-08 18:08:21 +0200

    Added test to keep Union commutative
    Union should be a commutative operation, such that `A|B = B|A`. This
    seems to be the case with types parsed from strings, but not for unions
    created directly in the code.
    
    E.g.
    ```
    new Union([Atomic::create('int'), Atomic::create('positive-int')])
    ```
    currently results in `positive-int`, even though int should subsume it.
    
    Refs vimeo/psalm#5342

---
+++ tests/UnionTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Psalm\Tests;
+
+use Psalm\Type\Atomic;
+use Psalm\Type\Union;
+
+class UnionTest extends TestCase
+{
+    private const KNOWN_ATOMICS = [
+        'int',
+        'float',
+        'string',
+        'bool',
+        'void',
+        'array-key',
+        'iterable',
+        'never-return',
+        'object',
+        'callable',
+        'pure-callable',
+        'array',
+        'non-empty-array',
+        'callable-array',
+        'list',
+        'non-empty-list',
+        'non-empty-string',
+        'non-falsy-string',
+        'lowercase-string',
+        'non-empty-lowercase-string',
+        'resource',
+        'closed-resource',
+        'positive-int',
+        'numeric',
+        'true',
+        'false',
+        'empty',
+        'scalar',
+        'null',
+        'mixed',
+        'callable-object',
+        'class-string',
+        'trait-string',
+        'callable-string',
+        'numeric-string',
+        'html-escaped-string',
+        'false-y',
+        '$this',
+
+    ];
+
+    public function testUnionsAreCommutative(): void
+    {
+        foreach (self::KNOWN_ATOMICS as $a) {
+            foreach (self::KNOWN_ATOMICS as $b) {
+                $this->assertSame(
+                    (new Union([
+                        Atomic::create($a),
+                        Atomic::create($b)
+                    ]))->getId(),
+                    (new Union([
+                        Atomic::create($b),
+                        Atomic::create($a)
+                    ]))->getId(),
+                    "$a|$b != $b|$a",
+                );
+            }
+        }
+    }
+}
