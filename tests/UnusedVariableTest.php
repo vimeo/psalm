8be77aaa2eddcd4a7740df9664bef458af585a04
Bruce Weirdan <weirdan@gmail.com>
2021-03-11 07:08:32 +0200

    Track variable usage in bool to int casts (#5349)
    Fixes vimeo/psalm#4956

--- tests/UnusedVariableTest.php
+++ tests/UnusedVariableTest.php
@@ -2316,6 +2316,18 @@ class UnusedVariableTest extends TestCase
                         };
                     }
                 ',
+            ],
+            'usedInIntCastInAssignment' => [
+                '<?php
+                    /** @return mixed */
+                    function f() {
+                        $a = random_int(0, 10) >= 5 ? true : false;
+
+                        $b = (int) $a;
+
+                        return $b;
+                    }
+                '
             ]
         ];
     }
