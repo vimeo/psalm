20efadda7a0ef024040f3525dc83ae58b10f5c9e
Bruce Weirdan <weirdan@gmail.com>
2021-03-16 23:24:44 +0200

    Fix BC break introduced in 4.6.3
    Fixes vimeo/psalm#5405
    
    Requires new patch release.
    
    This PR reverts changes to the signature of `IssueBuffer::finish()` and
    introduces separate method to be used to capture `$_SERVER`

--- src/psalm-refactor.php
+++ src/psalm-refactor.php
@@ -42,7 +42,7 @@ require_once __DIR__ . '/Psalm/Internal/ErrorHandler.php';
 require_once __DIR__ . '/command_functions.php';
 require_once __DIR__ . '/Psalm/Internal/Composer.php';
 require_once __DIR__ . '/Psalm/Internal/IncludeCollector.php';
-require_once __DIR__ . '/' . 'Psalm/Internal/ExecutionEnvironment/BuildInfoCollector.php';
+require_once __DIR__ . '/' . 'Psalm/IssueBuffer.php';
 
 (
     /** @param array<int,string> $argv */
@@ -162,7 +162,7 @@ HELP;
         $vendor_dir = \Psalm\getVendorDir($current_dir);
 
         // capture environment before registering autoloader (it may destroy it)
-        $build_info_collector = new BuildInfoCollector($_SERVER);
+        IssueBuffer::captureServer($_SERVER);
 
         $include_collector = new IncludeCollector();
         $first_autoloader = $include_collector->runAndCollect(
@@ -309,6 +309,6 @@ HELP;
 
         $project_analyzer->check($current_dir);
 
-        IssueBuffer::finish($project_analyzer, $build_info_collector, false, $start_time);
+        IssueBuffer::finish($project_analyzer, false, $start_time);
     }
 )($argv);
