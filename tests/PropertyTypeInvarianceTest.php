fb82d6796418963f9eb5ece6b6ce8083cd61f8ac
AndrolGenhald <AndrolGenhald@gmail.com>
2021-03-17 08:33:03 -0500

    Add template covariant test.

--- tests/PropertyTypeInvarianceTest.php
+++ tests/PropertyTypeInvarianceTest.php
@@ -148,6 +148,32 @@ class PropertyTypeInvarianceTest extends TestCase
                         protected $items = [];
                     }',
             ],
+            'allowTemplateCovariant' => [
+                '<?php
+                    class Foo {}
+                    class Bar extends Foo {}
+                    class Baz extends Bar {}
+
+                    /** @template-covariant T */
+                    class Pair
+                    {
+                        /** @var T|null */
+                        public $a;
+
+                        /** @var T|null */
+                        public $b;
+                    }
+
+                    /** @extends Pair<Foo> */
+                    class FooPair extends Pair
+                    {
+                        /** @var Bar|null */
+                        public $a;
+
+                        /** @var Baz|null */
+                        public $b;
+                    }',
+            ],
         ];
     }
 
