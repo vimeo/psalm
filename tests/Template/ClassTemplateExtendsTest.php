04f3e29f94c81b3d861b9edddac483cab08b48f5
Matt Brown <github@muglug.com>
2021-03-18 11:06:54 -0400

    Expand documentation method return type to ensure we’re giving a fair shot

--- tests/Template/ClassTemplateExtendsTest.php
+++ tests/Template/ClassTemplateExtendsTest.php
@@ -4350,6 +4350,56 @@ class ClassTemplateExtendsTest extends TestCase
 
                     $foo = new ObjectStorage();'
             ],
+            'liskovTerminatedByFinalClass' => [
+                '<?php
+                    final class CustomEnum extends Enum
+                    {
+                        public static function all() : CustomEnumSet
+                        {
+                            return new CustomEnumSet();
+                        }
+                    }
+
+                    /**
+                     * @template T of Enum
+                     */
+                    class EnumSet
+                    {
+                        private $type;
+
+                        /**
+                         * @param class-string<T> $type
+                         */
+                        public function __construct(string $type)
+                        {
+                            $this->type = $type;
+                        }
+                    }
+
+                    abstract class Enum {
+                        /**
+                         * @return EnumSet<static>
+                         */
+                        public static function all() : EnumSet
+                        {
+                            return new EnumSet(static::class);
+                        }
+                    }
+
+                    /**
+                     * @extends EnumSet<CustomEnum>
+                     */
+                    final class CustomEnumSet extends EnumSet {
+
+                        public function __construct()
+                        {
+                            parent::__construct(CustomEnum::class);
+                        }
+                    }',
+                [],
+                [],
+                '7.4'
+            ],
         ];
     }
 
