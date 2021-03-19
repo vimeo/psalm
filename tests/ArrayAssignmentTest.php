be968603fa53099403daf8d3f60d5ad786cb9a85
AndrolGenhald <AndrolGenhald@gmail.com>
2021-03-19 18:41:06 -0500

    Fix false-positive with non-empty object like array (fixes #5436)

--- tests/ArrayAssignmentTest.php
+++ tests/ArrayAssignmentTest.php
@@ -436,6 +436,17 @@ class ArrayAssignmentTest extends TestCase
                     '$foo' => 'array{a: int, b: array{int, int}}',
                 ],
             ],
+            'objectLikeArrayIsNonEmpty' => [
+                '<?php
+                    /**
+                     * @param array{a?: string, b: string} $arg
+                     * @return non-empty-array<string, string>
+                     */
+                    function test(array $arg): array {
+                        return $arg;
+                    }
+                ',
+            ],
             'nestedTKeyedArrayAddition' => [
                 '<?php
                     $foo = [];
