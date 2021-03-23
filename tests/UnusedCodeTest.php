477ae33cd6cfdf91fbaca94bf57b0566f3d6370c
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2021-03-24 00:34:12 +0100

    allow dismissing return value of no-return pure functions (#5461)

--- tests/UnusedCodeTest.php
+++ tests/UnusedCodeTest.php
@@ -753,6 +753,32 @@ class UnusedCodeTest extends TestCase
                     echo $test->id;
                     echo $test->name;'
             ],
+            'unusedNoReturnFunctionCall' => [
+                '<?php
+                    /**
+                     * @return no-return
+                     *
+                     * @pure
+                     *
+                     * @throws RuntimeException
+                     */
+                    function invariant_violation(string $message): void
+                    {
+                        throw new RuntimeException($message);
+                    }
+
+                    /**
+                     * @pure
+                     */
+                    function reverse(string $string): string
+                    {
+                        if ("" === $string) {
+                            invariant_violation("i do not like empty strings.");
+                        }
+
+                        return strrev($string);
+                    }'
+            ],
         ];
     }
 
