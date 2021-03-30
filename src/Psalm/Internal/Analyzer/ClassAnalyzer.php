69275af0c538c8337b623eed4916e53f48545256
Bruce Weirdan <weirdan@gmail.com>
2021-03-30 09:16:10 +0300

    Do not consider isset checks on static properties as redundant
    Unlike normal properties, static properties do not have a prescribed
    initialization sequence, so they can always be uninitialized (or unset).
    Thus `isset()` checks on them are never redundant.
    
    Fixes vimeo/psalm#5489

--- src/Psalm/Internal/Analyzer/ClassAnalyzer.php
+++ src/Psalm/Internal/Analyzer/ClassAnalyzer.php
@@ -804,6 +804,7 @@ class ClassAnalyzer extends ClassLikeAnalyzer
                 ) {
                     $property_type->initialized = false;
                     $property_type->from_property = true;
+                    $property_type->from_static_property = $property_storage->is_static;
                 }
             } else {
                 $property_type = Type::getMixed();
@@ -811,6 +812,7 @@ class ClassAnalyzer extends ClassLikeAnalyzer
                 if (!$property_storage->has_default && !$property_storage->is_promoted) {
                     $property_type->initialized = false;
                     $property_type->from_property = true;
+                    $property_type->from_static_property = $property_storage->is_static;
                 }
             }
 
