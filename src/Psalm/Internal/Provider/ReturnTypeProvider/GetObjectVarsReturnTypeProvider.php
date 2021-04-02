106c7db5d15a4fd7a45aa6b494f8eed48b806061
Bruce Weirdan <weirdan@gmail.com>
2021-04-02 22:39:43 +0300

    Provide more detailed return type for get_object_vars() (#5557)
    Handle keyed objects and named objects, including support for property
    visibility.

--- src/Psalm/Internal/Provider/ReturnTypeProvider/GetObjectVarsReturnTypeProvider.php
+++ src/Psalm/Internal/Provider/ReturnTypeProvider/GetObjectVarsReturnTypeProvider.php
@@ -1,11 +1,16 @@
 <?php
 namespace Psalm\Internal\Provider\ReturnTypeProvider;
 
-use PhpParser;
+use Psalm\Internal\Analyzer\ClassAnalyzer;
 use Psalm\Plugin\EventHandler\Event\FunctionReturnTypeProviderEvent;
+use Psalm\Plugin\EventHandler\FunctionReturnTypeProviderInterface;
 use Psalm\Type;
+use stdClass;
 
-class GetObjectVarsReturnTypeProvider implements \Psalm\Plugin\EventHandler\FunctionReturnTypeProviderInterface
+use function reset;
+use function strtolower;
+
+class GetObjectVarsReturnTypeProvider implements FunctionReturnTypeProviderInterface
 {
     /**
      * @return array<lowercase-string>
@@ -30,6 +35,61 @@ class GetObjectVarsReturnTypeProvider implements \Psalm\Plugin\EventHandler\Func
         if (($first_arg_type = $statements_source->node_data->getType($call_args[0]->value))
              && $first_arg_type->isObjectType()
         ) {
+            if ($first_arg_type->isSingle()) {
+                $atomics = $first_arg_type->getAtomicTypes();
+                $object_type = reset($atomics);
+
+                if ($object_type instanceof Type\Atomic\TObjectWithProperties) {
+                    if ([] === $object_type->properties) {
+                        return Type::getEmptyArray();
+                    }
+                    return new Type\Union([
+                        new Type\Atomic\TKeyedArray($object_type->properties)
+                    ]);
+                } elseif ($object_type instanceof Type\Atomic\TNamedObject) {
+                    if (strtolower($object_type->value) === strtolower(stdClass::class)) {
+                        return Type::parseString('array<string, mixed>');
+                    }
+                    $codebase = $statements_source->getCodebase();
+                    $class_storage = $codebase->classlikes->getStorageFor($object_type->value);
+
+                    if (null === $class_storage) {
+                        return Type::parseString('array<string, mixed>');
+                    }
+
+                    if ([] === $class_storage->appearing_property_ids) {
+                        return Type::getEmptyArray();
+                    }
+
+                    $properties = [];
+                    foreach ($class_storage->appearing_property_ids as $name => $property_id) {
+                        if (ClassAnalyzer::checkPropertyVisibility(
+                            $property_id,
+                            $event->getContext(),
+                            $statements_source,
+                            $event->getCodeLocation(),
+                            $event->getStatementsSource()->getSuppressedIssues(),
+                            false
+                        ) === true) {
+                            $property_type = $codebase->properties->getPropertyType(
+                                $property_id,
+                                false,
+                                $statements_source,
+                                $event->getContext()
+                            );
+                            $properties[$name] = $property_type ?: Type::getMixed();
+                        }
+                    }
+
+                    if ([] === $properties) {
+                        return Type::getEmptyArray();
+                    }
+
+                    return new Type\Union([
+                        new Type\Atomic\TKeyedArray($properties)
+                    ]);
+                }
+            }
             return Type::parseString('array<string, mixed>');
         }
 
---
+++ tests/ReturnTypeProvider/GetObjectVarsTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Psalm\Tests\ReturnTypeProvider;
+
+use Psalm\Tests\TestCase;
+use Psalm\Tests\Traits\ValidCodeAnalysisTestTrait;
+
+class GetObjectVarsTest extends TestCase
+{
+    use ValidCodeAnalysisTestTrait;
+
+    public function providerValidCodeParse(): iterable
+    {
+        yield 'returnsPublicProperties' => [
+            '<?php
+                class C {
+                    /** @var string */
+                    public $prop = "val";
+                }
+                $ret = get_object_vars(new C);
+            ',
+            ['$ret' => 'array{prop: string}'],
+        ];
+
+        yield 'omitsPrivateAndProtectedPropertiesWhenCalledOutsideOfClassScope' => [
+            '<?php
+                class C {
+                    /** @var string */
+                    private $priv = "val";
+
+                    /** @var string */
+                    protected $prot = "val";
+                }
+                $ret = get_object_vars(new C);
+            ',
+            ['$ret' => 'array<empty, empty>'],
+        ];
+
+        yield 'includesPrivateAndProtectedPropertiesWhenCalledInsideClassScope' => [
+            '<?php
+                class C {
+                    /** @var string */
+                    private $priv = "val";
+
+                    /** @var string */
+                    protected $prot = "val";
+
+                    /** @return array{priv: string, prot: string} */
+                    public function method(): array {
+                        return get_object_vars($this);
+                    }
+                }
+            ',
+            [],
+        ];
+
+        yield 'includesProtectedAndOmitsPrivateFromParentWhenCalledInDescendant' => [
+            '<?php
+                class C {
+                    /** @var string */
+                    private $priv = "val";
+
+                    /** @var string */
+                    protected $prot = "val";
+
+                    /** @var string */
+                    public $pub = "val";
+                }
+
+                class D extends C {
+                    /** @return array{prot: string} */
+                    public function method(): array {
+                        return get_object_vars($this);
+                    }
+                }
+            ',
+            [],
+        ];
+
+        yield 'propertiesOfObjectWithKeys' => [
+            '<?php
+                /**
+                 * @param object{a:int, b:string, c:bool} $p
+                 * @return array{a:int, b:string, c:bool}
+                 */
+                function f(object $p): array {
+                    return get_object_vars($p);
+                }
+            ',
+            [],
+        ];
+
+        yield 'propertiesOfPOPO' => [
+            // todo: fix object cast so that it results in `object{a:1}` instead
+            '<?php $ret = get_object_vars((object)["a" => 1]);',
+            ['$ret' => 'array<string, mixed>'],
+        ];
+    }
+}
