dcaf26f49774ad41f58de0eab12ed1fe56ecfa5b
Bruce Weirdan <weirdan@gmail.com>
2021-04-03 04:26:20 +0300

    Support array of objects in array_column()
    Fixes vimeo/psalm#5460

--- src/Psalm/Internal/Provider/ReturnTypeProvider/GetObjectVarsReturnTypeProvider.php
+++ src/Psalm/Internal/Provider/ReturnTypeProvider/GetObjectVarsReturnTypeProvider.php
@@ -1,7 +1,10 @@
 <?php
 namespace Psalm\Internal\Provider\ReturnTypeProvider;
 
+use Psalm\CodeLocation;
+use Psalm\Context;
 use Psalm\Internal\Analyzer\ClassAnalyzer;
+use Psalm\Internal\Analyzer\SourceAnalyzer;
 use Psalm\Plugin\EventHandler\Event\FunctionReturnTypeProviderEvent;
 use Psalm\Plugin\EventHandler\FunctionReturnTypeProviderInterface;
 use Psalm\Type;
@@ -22,6 +25,70 @@ class GetObjectVarsReturnTypeProvider implements FunctionReturnTypeProviderInter
         ];
     }
 
+    public static function getGetObjectVarsReturnType(
+        Type\Union $first_arg_type,
+        SourceAnalyzer $statements_source,
+        Context $context,
+        CodeLocation $location
+    ): Type\Union {
+        if ($first_arg_type->isSingle()) {
+            $atomics = $first_arg_type->getAtomicTypes();
+            $object_type = reset($atomics);
+
+            if ($object_type instanceof Type\Atomic\TObjectWithProperties) {
+                if ([] === $object_type->properties) {
+                    return Type::getEmptyArray();
+                }
+                return new Type\Union([
+                    new Type\Atomic\TKeyedArray($object_type->properties)
+                ]);
+            } elseif ($object_type instanceof Type\Atomic\TNamedObject) {
+                if (strtolower($object_type->value) === strtolower(stdClass::class)) {
+                    return Type::parseString('array<string, mixed>');
+                }
+                $codebase = $statements_source->getCodebase();
+                $class_storage = $codebase->classlikes->getStorageFor($object_type->value);
+
+                if (null === $class_storage) {
+                    return Type::parseString('array<string, mixed>');
+                }
+
+                if ([] === $class_storage->appearing_property_ids) {
+                    return Type::getEmptyArray();
+                }
+
+                $properties = [];
+                foreach ($class_storage->appearing_property_ids as $name => $property_id) {
+                    if (ClassAnalyzer::checkPropertyVisibility(
+                        $property_id,
+                        $context,
+                        $statements_source,
+                        $location,
+                        $statements_source->getSuppressedIssues(),
+                        false
+                    ) === true) {
+                        $property_type = $codebase->properties->getPropertyType(
+                            $property_id,
+                            false,
+                            $statements_source,
+                            $context
+                        );
+                        $properties[$name] = $property_type ?: Type::getMixed();
+                    }
+                }
+
+                if ([] === $properties) {
+                    return Type::getEmptyArray();
+                }
+
+                return new Type\Union([
+                    new Type\Atomic\TKeyedArray($properties)
+                ]);
+            }
+        }
+        return Type::parseString('array<string, mixed>');
+    }
+
     public static function getFunctionReturnType(FunctionReturnTypeProviderEvent $event): ?Type\Union
     {
         $statements_source = $event->getStatementsSource();
@@ -35,62 +102,12 @@ class GetObjectVarsReturnTypeProvider implements FunctionReturnTypeProviderInter
         if (($first_arg_type = $statements_source->node_data->getType($call_args[0]->value))
              && $first_arg_type->isObjectType()
         ) {
-            if ($first_arg_type->isSingle()) {
-                $atomics = $first_arg_type->getAtomicTypes();
-                $object_type = reset($atomics);
-
-                if ($object_type instanceof Type\Atomic\TObjectWithProperties) {
-                    if ([] === $object_type->properties) {
-                        return Type::getEmptyArray();
-                    }
-                    return new Type\Union([
-                        new Type\Atomic\TKeyedArray($object_type->properties)
-                    ]);
-                } elseif ($object_type instanceof Type\Atomic\TNamedObject) {
-                    if (strtolower($object_type->value) === strtolower(stdClass::class)) {
-                        return Type::parseString('array<string, mixed>');
-                    }
-                    $codebase = $statements_source->getCodebase();
-                    $class_storage = $codebase->classlikes->getStorageFor($object_type->value);
-
-                    if (null === $class_storage) {
-                        return Type::parseString('array<string, mixed>');
-                    }
-
-                    if ([] === $class_storage->appearing_property_ids) {
-                        return Type::getEmptyArray();
-                    }
-
-                    $properties = [];
-                    foreach ($class_storage->appearing_property_ids as $name => $property_id) {
-                        if (ClassAnalyzer::checkPropertyVisibility(
-                            $property_id,
-                            $event->getContext(),
-                            $statements_source,
-                            $event->getCodeLocation(),
-                            $event->getStatementsSource()->getSuppressedIssues(),
-                            false
-                        ) === true) {
-                            $property_type = $codebase->properties->getPropertyType(
-                                $property_id,
-                                false,
-                                $statements_source,
-                                $event->getContext()
-                            );
-                            $properties[$name] = $property_type ?: Type::getMixed();
-                        }
-                    }
-
-                    if ([] === $properties) {
-                        return Type::getEmptyArray();
-                    }
-
-                    return new Type\Union([
-                        new Type\Atomic\TKeyedArray($properties)
-                    ]);
-                }
-            }
-            return Type::parseString('array<string, mixed>');
+            return self::getGetObjectVarsReturnType(
+                $first_arg_type,
+                $statements_source,
+                $event->getContext(),
+                $event->getCodeLocation()
+            );
         }
 
         return null;
