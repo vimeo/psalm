3b3065c881c052d17967da6370f0c7c96193be63
Daniil Gentili <daniil@daniil.it>
2021-04-09 17:29:45 +0200

    Allow variable keys with array_key_exists (#5606)
    * Allow variable keys with array_key_exists
    
    * Switch to elseif

--- tests/TypeReconciliation/ArrayKeyExistsTest.php
+++ tests/TypeReconciliation/ArrayKeyExistsTest.php
@@ -267,6 +267,24 @@ class ArrayKeyExistsTest extends \Psalm\Tests\TestCase
                         }
                     }',
             ],
+            'arrayKeyExistsVariable' => [
+                '<?php
+                    class pony
+                    {
+                    }
+                    /**
+                     * @param array{0?: string, test?: string, pony?: string} $params
+                     * @return string|null
+                     */
+                    function a(array $params = [])
+                    {
+                        foreach ([0, "test", pony::class] as $key) {
+                            if (\array_key_exists($key, $params)) {
+                                return $params[$key];
+                            }
+                        }
+                    }'
+            ],
             'noCrashOnArrayKeyExistsBracket' => [
                 '<?php
                     class MyCollection {
