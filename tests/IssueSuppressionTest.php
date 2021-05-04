dd8f84a49c1acb93a03eb62756164f1b6cb99baf
Bruce Weirdan <weirdan@gmail.com>
2021-05-04 10:39:19 +0300

    Suppress issues emitted by MethodComparator at class level
    Fixes vimeo/psalm#5698

--- tests/IssueSuppressionTest.php
+++ tests/IssueSuppressionTest.php
@@ -287,8 +287,29 @@ class IssueSuppressionTest extends TestCase
                             return $this->mightBeNull->format("");
                         }
                     }
-                '
+                ',
             ],
+            'methodSignatureMismatchSuppressedAtClassLevel' => [
+                '<?php
+                    class ParentClass {
+                        /**
+                         * @psalm-suppress MissingParamType
+                         * @return mixed
+                         */
+                        public function func($var) {
+                            return $var;
+                        }
+                    }
+
+                    /** @psalm-suppress MethodSignatureMismatch */
+                    class MismatchMethod extends ParentClass {
+                        /** @return mixed */
+                        public function func(string $var) {
+                            return $var;
+                        }
+                    }
+                ',
+            ]
         ];
     }
 
