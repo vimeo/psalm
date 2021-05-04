aa3b3376daed95bde7b55169607bc8db813354f1
Bruce Weirdan <weirdan@gmail.com>
2021-05-04 11:45:59 +0300

    Prefer `@psalm-template` over `@phpstan-template`
    Fixes vimeo/psalm#5590

--- tests/ClassLikeDocblockParserTest.php
+++ tests/ClassLikeDocblockParserTest.php
@@ -20,4 +20,17 @@ class ClassLikeDocblockParserTest extends \Psalm\Tests\TestCase
 
         $this->assertSame('Some Description', $class_docblock->description);
     }
+
+    public function testPreferPsalmPrefixedAnnotationsOverPhpstanOnes(): void
+    {
+        $doc = '/**
+ * @psalm-template-covariant T of string
+ * @phpstan-template T of int
+ */
+';
+        $node = new Class_(null);
+        $php_parser_doc = new \PhpParser\Comment\Doc($doc);
+        $class_docblock = ClassLikeDocblockParser::parse($node, $php_parser_doc, new Aliases());
+        $this->assertSame([['T', 'of', 'string', true, 33]], $class_docblock->templates);
+    }
 }
