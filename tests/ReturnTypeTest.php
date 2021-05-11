11cd6331edb836b6ea1847345a6ea98fbd228c6a
orklah <ronan_morin@hotmail.com>
2021-05-11 21:35:32 +0200

    allow scalar values in return to be accepted, even when the branch has no inferred types

--- tests/ReturnTypeTest.php
+++ tests/ReturnTypeTest.php
@@ -919,6 +919,20 @@ class ReturnTypeTest extends TestCase
                         throw new RuntimeException;
                     }
                 '
+            ],
+            'scalarLiteralsInferredAfterUndefinedClass' => [
+                '<?php
+                    /** @param object $arg */
+                    function test($arg): ?string
+                    {
+                        /** @psalm-suppress UndefinedClass */
+                        if ($arg instanceof SomeClassThatDoesNotExist) {
+                            return null;
+                        }
+
+                        return "b";
+                    }
+                '
             ]
         ];
     }
