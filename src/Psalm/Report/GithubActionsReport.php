5c7aad0e5dbb022e3117837998f3a4c31823d5eb
Samuel Parkinson <samuel.parkinson@hey.com>
2021-05-13 10:00:33 +0000

    Add issue link to GitHub Actions report
    If an issue link is available, include it in the annotations on GitHub.

--- src/Psalm/Report/GithubActionsReport.php
+++ src/Psalm/Report/GithubActionsReport.php
@@ -11,13 +11,14 @@ class GithubActionsReport extends Report
     {
         $output = '';
         foreach ($this->issues_data as $issue_data) {
+            $issue_reference = $issue_data->link ? ' (see ' . $issue_data->link . ')' : '';
             $output .= sprintf(
                 '::%s file=%s,line=%s,col=%s::%s',
                 ($issue_data->severity === Config::REPORT_ERROR ? 'error' : 'warning'),
                 $issue_data->file_name,
                 $issue_data->line_from,
                 $issue_data->column_from,
-                $issue_data->message
+                $issue_data->message . $issue_reference
             ) . "\n";
         }
 
