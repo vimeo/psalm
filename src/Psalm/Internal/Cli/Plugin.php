4df76568555caa01e1e60bbdf31b43119e720456
Bruce Weirdan <weirdan@gmail.com>
2021-05-30 02:02:54 +0300

    Moved psalm_plugin to the CLI command class

---
+++ src/Psalm/Internal/Cli/Plugin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Psalm\Internal\Cli;
+
+use PackageVersions\Versions;
+use Psalm\Internal\PluginManager\Command\DisableCommand;
+use Psalm\Internal\PluginManager\Command\EnableCommand;
+use Psalm\Internal\PluginManager\Command\ShowCommand;
+use Psalm\Internal\PluginManager\PluginListFactory;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputOption;
+
+use function dirname;
+use function getcwd;
+use function Psalm\requireAutoloaders;
+
+use const DIRECTORY_SEPARATOR;
+
+require_once __DIR__ . '/../../../command_functions.php';
+require_once __DIR__ . '/../Composer.php';
+
+final class Plugin
+{
+    public static function run(): void
+    {
+        $current_dir = (string)getcwd() . DIRECTORY_SEPARATOR;
+        $vendor_dir = \Psalm\getVendorDir($current_dir);
+        requireAutoloaders($current_dir, false, $vendor_dir);
+
+        $app = new Application('psalm-plugin', Versions::getVersion('vimeo/psalm'));
+
+        $psalm_root = dirname(__DIR__, 4) . DIRECTORY_SEPARATOR;
+
+        $plugin_list_factory = new PluginListFactory($current_dir, $psalm_root);
+
+        $app->addCommands([
+            new ShowCommand($plugin_list_factory),
+            new EnableCommand($plugin_list_factory),
+            new DisableCommand($plugin_list_factory),
+        ]);
+
+        $app->getDefinition()->addOption(
+            new InputOption('config', 'c', InputOption::VALUE_REQUIRED, 'Path to Psalm config file')
+        );
+
+        $app->setDefaultCommand('show');
+        $app->run();
+    }
+}
