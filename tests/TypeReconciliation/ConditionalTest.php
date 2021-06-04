1c9f1a2d7f2435c5813a33ffdb0358a9ddb0ed59
andrew <klimichkartorgnusov@gmail.com>
2021-06-04 18:02:21 +0300

    Fix generic destruction in assertion

--- tests/TypeReconciliation/ConditionalTest.php
+++ tests/TypeReconciliation/ConditionalTest.php
@@ -2626,6 +2626,46 @@ class ConditionalTest extends \Psalm\Tests\TestCase
                      isValid($val) ? $takesValid($val) : $takesInvalid($val);
                  }'
             ],
+            'reconcileMoreThanOneGenericObject' => [
+                '<?php
+
+                 final class Invalid {}
+
+                 /**
+                  * @template T
+                  */
+                 final class Valid {}
+
+                 /**
+                  * @template T
+                  *
+                  * @param Invalid|Valid<T> $val
+                  * @psalm-assert-if-true Valid<T> $val
+                  */
+                 function isValid($val): bool
+                 {
+                     return $val instanceof Valid;
+                 }
+
+                 /**
+                  * @template T
+                  * @param Valid<T>|Invalid $val1
+                  * @param Valid<T>|Invalid $val2
+                  * @param Valid<T>|Invalid $val3
+                  */
+                 function inGenericContext($val1, $val2, $val3): void
+                 {
+                     $takesValid =
+                          /** @param Valid<T> $_valid */
+                          function ($_valid): void {};
+
+                     if (isValid($val1) && isValid($val2) && isValid($val3)) {
+                         $takesValid($val1);
+                         $takesValid($val2);
+                         $takesValid($val3);
+                     }
+                 }'
+            ]
         ];
     }
 
