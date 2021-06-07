a5080f2cf622d0254c21f0440f27eef50e633954
Bruce Weirdan <weirdan@gmail.com>
2021-06-07 17:45:43 +0300

    Do not provide mixed return type on unrelated methods (#5895)
    Fixes vimeo/psalm#5894

--- src/Psalm/Internal/Provider/ReturnTypeProvider/DomNodeAppendChild.php
+++ src/Psalm/Internal/Provider/ReturnTypeProvider/DomNodeAppendChild.php
@@ -16,14 +16,18 @@ class DomNodeAppendChild implements \Psalm\Plugin\EventHandler\MethodReturnTypeP
         $source = $event->getSource();
         $call_args = $event->getCallArgs();
         $method_name_lowercase = $event->getMethodNameLowercase();
+
+        if ($method_name_lowercase !== 'appendchild') {
+            return null;
+        }
+
         if (!$source instanceof \Psalm\Internal\Analyzer\StatementsAnalyzer
             || !$call_args
         ) {
             return Type::getMixed();
         }
 
-        if ($method_name_lowercase === 'appendchild'
-            && ($first_arg_type = $source->node_data->getType($call_args[0]->value))
+        if (($first_arg_type = $source->node_data->getType($call_args[0]->value))
             && $first_arg_type->hasObjectType()
         ) {
             return clone $first_arg_type;
