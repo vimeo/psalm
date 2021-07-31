a205a23ccb855aa7def01b9051d7c6a592df6fd3
Matthew Brown <github@muglug.com>
2021-07-31 19:50:56 -0400

    Fix #6212 - remove untested code, add test for reasonable behaviour

--- tests/Template/ClassTemplateTest.php
+++ tests/Template/ClassTemplateTest.php
@@ -3554,6 +3554,29 @@ class ClassTemplateTest extends TestCase
                         return rand(0, 1) ? $c : $d->get();
                     }',
             ],
+            'allowCompatibleGenerics' => [
+                '<?php
+                    /** @template T of object */
+                    interface A {}
+
+                    /** @template T of object */
+                    interface B {}
+
+                    /**
+                     * @template T of object
+                     * @param A<T> $a
+                     * @param B<T> $b
+                     */
+                    function foo(A $a, B $b): void {}
+
+                    /**
+                     * @param A<stdClass> $a
+                     * @param B<stdClass> $b
+                     */
+                    function bar(A $a, B $b): void {
+                        foo($a, $b);
+                    }'
+            ],
         ];
     }
 
