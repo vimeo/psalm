603f85bfcc6b27059a9529d050af6fb6bf0deb51
Dgame <rswhite4@googlemail.com>
2021-10-07 20:59:18 +0200

    Set ERROR_LEVEL = 1

--- src/Psalm/Issue/NonStrictBoolCondition.php
+++ src/Psalm/Issue/NonStrictBoolCondition.php
@@ -4,6 +4,6 @@ namespace Psalm\Issue;
 
 class NonStrictBoolCondition extends CodeIssue
 {
-    public const ERROR_LEVEL = -1;
+    public const ERROR_LEVEL = 1;
     public const SHORTCODE = 280;
 }
