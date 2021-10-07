3aedb1231ee9cad5eba7bc93aed3f5dd5197cd33
Nils Werner <nils.werner@sonarsource.com>
2021-10-07 10:31:17 +0200

    Fix code quality issues

--- src/Psalm/Internal/CliUtils.php
+++ src/Psalm/Internal/CliUtils.php
@@ -396,8 +396,8 @@ Output:
 
     --output-format=console
         Changes the output format.
-        Available formats: compact, console, text, emacs, json, pylint, xml, checkstyle, junit, sonarqube, sonarcloud, github,
-                           phpstorm, codeclimate
+        Available formats: compact, console, text, emacs, json, pylint, xml, checkstyle, junit, sonarqube, sonarcloud,
+                           github, phpstorm, codeclimate
 
     --no-progress
         Disable the progress indicator
@@ -412,7 +412,8 @@ Reports:
     --report=PATH
         The path where to output report file. The output format is based on the file extension.
         (Currently supported formats: ".json", ".xml", ".txt", ".emacs", ".pylint", ".console",
-        ".sarif", "checkstyle.xml", "sonarqube.json", "sonarcloud.json", "codeclimate.json", "summary.json", "junit.xml")
+        ".sarif", "checkstyle.xml", "sonarqube.json", "sonarcloud.json", "codeclimate.json", "summary.json",
+        "junit.xml")
 
     --report-show-info[=BOOLEAN]
         Whether the report should include non-errors in its output (defaults to true)
