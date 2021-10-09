4866155b3ad5260554283683bcb6a6bcd9f8756b
orklah <ronan_morin@hotmail.com>
2021-10-09 20:03:52 +0200

    add test

--- tests/Template/FunctionTemplateTest.php
+++ tests/Template/FunctionTemplateTest.php
@@ -1549,6 +1549,45 @@ class FunctionTemplateTest extends TestCase
                      */
                     function deserialize_object(string $data, string $type) {}'
             ],
+            'arrayKeyInTemplateOfArrayKey' => [
+                '<?php
+
+                    /**
+                     * @template TKey of array-key
+                     * @template TValue
+                     * @template TNewKey of array-key
+                     * @template TNewValue
+                     * @psalm-param iterable<TKey, TValue> $iterable
+                     * @psalm-param callable(TKey, TValue): iterable<TNewKey, TNewValue> $mapper
+                     * @psalm-return \Generator<TNewKey, TNewValue>
+                     */
+                    function map(iterable $iterable, callable $mapper): Generator
+                    {
+                        foreach ($iterable as $key => $value) {
+                            yield from $mapper($key, $value);
+                        }
+                    }
+
+                    /**
+                     * @psalm-return iterable<array-key, \stdClass>
+                     */
+                    function iter(): iterable
+                    {
+                        return [];
+                    }
+
+                    /**
+                     * @template TKey of array-key
+                     * @psalm-param TKey $key
+                     * @psalm-return Generator<TKey, string>
+                     */
+                    function mapper($key): Generator
+                    {
+                        yield $key => "a";
+                    }
+
+                    map(iter(), "mapper");'
+            ],
         ];
     }
 
