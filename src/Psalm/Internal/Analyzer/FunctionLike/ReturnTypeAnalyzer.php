7a47ebfcd6108a119e68148177716f089c805145
orklah <ronan_morin@hotmail.com>
2021-10-16 00:26:00 +0200

    fix

--- src/Psalm/Internal/Analyzer/FunctionLike/ReturnTypeAnalyzer.php
+++ src/Psalm/Internal/Analyzer/FunctionLike/ReturnTypeAnalyzer.php
@@ -399,7 +399,8 @@ class ReturnTypeAnalyzer
                 || ($classlike_storage && $classlike_storage->final)
         );
 
-        if (!$inferred_return_type->isNever()
+        if (!$inferred_return_type_parts
+            && !$inferred_return_type->isNever()
             && !$inferred_yield_types
             && (!$function_like_storage || !$function_like_storage->has_yield)
         ) {
