1f02795e4282fd2e2eb8ab87e8300589b39c6b73
orklah <ronan_morin@hotmail.com>
2021-10-20 19:54:32 +0200

    fix missing implicit __toString

--- tests/ToStringTest.php
+++ tests/ToStringTest.php
@@ -487,6 +487,20 @@ class ToStringTest extends TestCase
                 ',
                 'error_message' => 'ImplicitToStringCast'
             ],
+            'implicitCastInToString' => [
+                '<?php
+                    declare(strict_types=1);
+
+                    final class A
+                    {
+                        public function __toString(): string
+                        {
+                            return new SplFileInfo("a");
+                        }
+                    }
+                ',
+                'error_message' => 'ImplicitToStringCast'
+            ],
         ];
     }
 }
