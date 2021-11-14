f5b71a3a5f790f2e2ec318540494ab22bc9e4c77
Bruce Weirdan <weirdan@gmail.com>
2021-11-14 03:57:10 +0200

    Infer mb_strtolower() result as string when encoding is specified
    `mb_strtolower()` may return characters we generally consider uppercase
    when it's given the encoding argument. This PR makes Psalm to err on the
    side of caution and treat the return type as `string` rather than
    `lowercase-string` in this case
    
    Refs vimeo/psalm#6908

--- tests/FunctionCallTest.php
+++ tests/FunctionCallTest.php
@@ -1676,6 +1676,26 @@ class FunctionCallTest extends TestCase
                         if (strpos(DateTime::class, $needle)) {}
                     }',
             ],
+            'mb_strtolowerProducesStringWithSecondArgument' => [
+                '<?php
+                    $r = mb_strtolower("École", "BASE64");
+                ',
+                'assertions' => [
+                    '$r===' => 'string',
+                ],
+            ],
+            'mb_strtolowerProducesLowercaseStringWithNullOrAbsentEncoding' => [
+                '<?php
+                    $a = mb_strtolower("AAA");
+                    $b = mb_strtolower("AAA", null);
+                ',
+                'assertions' => [
+                    '$a===' => 'lowercase-string',
+                    '$b===' => 'lowercase-string',
+                ],
+                [],
+                '8.1',
+            ],
         ];
     }
 
