41fd460db5f60c6a844f235a0aef0853f7277400
orklah <ronan_morin@hotmail.com>
2021-11-21 13:48:19 +0100

    check __callStatic purity instead of the pseudoMethod purity

--- src/Psalm/Internal/Analyzer/Statements/Expression/Call/StaticMethod/AtomicStaticCallAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/Expression/Call/StaticMethod/AtomicStaticCallAnalyzer.php
@@ -469,6 +469,7 @@ class AtomicStaticCallAnalyzer
                 true,
                 $context->insideUse()
             )) {
+                $callstatic_storage = $codebase->methods->getStorage($callstatic_id);
                 if ($codebase->methods->return_type_provider->has($fq_class_name)) {
                     $return_type_candidate = $codebase->methods->return_type_provider->getReturnType(
                         $statements_analyzer,
@@ -516,7 +517,7 @@ class AtomicStaticCallAnalyzer
                     }
 
                     if (!$context->inside_throw) {
-                        if ($context->pure && !$pseudo_method_storage->pure) {
+                        if ($context->pure && !$callstatic_storage->pure) {
                             if (IssueBuffer::accepts(
                                 new ImpureMethodCall(
                                     'Cannot call an impure method from a pure context',
@@ -526,7 +527,7 @@ class AtomicStaticCallAnalyzer
                             )) {
                                 // fall through
                             }
-                        } elseif ($context->mutation_free && !$pseudo_method_storage->mutation_free) {
+                        } elseif ($context->mutation_free&& !$callstatic_storage->mutation_free) {
                             if (IssueBuffer::accepts(
                                 new ImpureMethodCall(
                                     'Cannot call a possibly-mutating method from a mutation-free context',
@@ -539,9 +540,9 @@ class AtomicStaticCallAnalyzer
                         } elseif ($statements_analyzer->getSource()
                             instanceof \Psalm\Internal\Analyzer\FunctionLikeAnalyzer
                             && $statements_analyzer->getSource()->track_mutations
-                            && !$pseudo_method_storage->pure
+                            && !$callstatic_storage->pure
                         ) {
-                            if (!$pseudo_method_storage->mutation_free) {
+                            if (!$callstatic_storage->mutation_free) {
                                 $statements_analyzer->getSource()->inferred_has_mutation = true;
                             }
 
--- tests/PureAnnotationTest.php
+++ tests/PureAnnotationTest.php
@@ -413,6 +413,35 @@ class PureAnnotationTest extends TestCase
                        }
                     }'
             ],
+            'pureThroughCallStatic' => [
+                '<?php
+
+                    /**
+                     * @method static self FOO()
+                     * @method static static BAR()
+                     * @method static static BAZ()
+                     *
+                     * @psalm-immutable
+                     */
+                    class MyEnum
+                    {
+                        const FOO = "foo";
+                        const BAR = "bar";
+                        const BAZ = "baz";
+
+                        /** @psalm-pure */
+                        public static function __callStatic(string $name, array $params): static
+                        {
+                            throw new BadMethodCallException("not implemented");
+                        }
+                    }
+
+                    /** @psalm-pure */
+                    function gimmeFoo(): MyEnum
+                    {
+                        return MyEnum::FOO();
+                    }',
+            ],
         ];
     }
 
