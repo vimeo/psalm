0d477221950c802179fb775475f8ab3f47b9c827
orklah <ronan_morin@hotmail.com>
2021-11-22 21:13:25 +0100

    fix counting array properties when some are Never

--- tests/TypeReconciliation/RedundantConditionTest.php
+++ tests/TypeReconciliation/RedundantConditionTest.php
@@ -867,7 +867,19 @@ class RedundantConditionTest extends \Psalm\Tests\TestCase
 
                     }
                     '
-            ]
+            ],
+            'countWithNeverValuesInKeyedArray' => [
+                '<?php
+                    /** @var non-empty-array $report_data */
+                    $report_data = [];
+                    if ( array_key_exists( "A", $report_data ) ) {
+                    } elseif ( !empty( $report_data[0]["type"] ) && rand(0,1) ) {
+                        if ( rand(0,1) ) {}
+
+                        if ( count( $report_data ) === 1 ) {
+                        }
+                    }'
+            ],
         ];
     }
 
