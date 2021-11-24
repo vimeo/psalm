049285add246467132c200effb367892bf18603f
orklah <ronan_morin@hotmail.com>
2021-11-24 19:43:43 +0100

    allow using EnumCase in an Attribute

--- tests/EnumTest.php
+++ tests/EnumTest.php
@@ -174,6 +174,31 @@ class EnumTest extends TestCase
                 [],
                 '8.1',
             ],
+            'EnumCaseInAttribute' => [
+                '<?php
+                    class CreateController {
+                        #[Param(paramType: ParamType::FLAG)]
+                        public function actionGet(): void {}
+                    }
+
+                    use Attribute;
+
+                    #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+                    class Param {
+                        public function __construct(
+                            public ParamType $paramType = ParamType::PARAM
+                        ) {
+                        }
+                    }
+
+                    enum ParamType {
+                        case FLAG;
+                        case PARAM;
+                    }',
+                'assertions' => [],
+                [],
+                '8.1',
+            ],
         ];
     }
 
