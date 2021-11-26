f215ed28d05813a9648ebc89adda5ea76dfe8309
Bruce Weirdan <weirdan@gmail.com>
2021-11-26 05:59:06 +0200

    Drop special handling of constant references on enums
    Internally enum cases are constants, and they should be resolved as
    such.
    
    Fixes vimeo/psalm#6994

--- tests/EnumTest.php
+++ tests/EnumTest.php
@@ -174,6 +174,26 @@ class EnumTest extends TestCase
                 [],
                 '8.1',
             ],
+            'wildcardConstantsOnEnum' => [
+                '<?php
+                    enum A {
+                        const C_1 = 1;
+                        const C_2 = 2;
+                        const C_3 = 3;
+
+                        /**
+                         * @param self::C_* $i
+                         */
+                        public static function foo(int $i) : void {}
+                    }
+
+                    A::foo(A::C_1);
+                    A::foo(A::C_2);
+                    A::foo(A::C_3);',
+                'assertions' => [],
+                [],
+                '8.1',
+            ],
             'EnumCaseInAttribute' => [
                 '<?php
                     class CreateController {
