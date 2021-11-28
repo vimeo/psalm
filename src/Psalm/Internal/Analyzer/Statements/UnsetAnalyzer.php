925df22052a6716e353eb189b2c02639a05a0e58
Daniil Gentili <daniil@daniil.it>
2021-11-28 13:42:30 +0100

    Use simplified, proper logic

--- src/Psalm/Internal/Analyzer/Statements/UnsetAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/UnsetAnalyzer.php
@@ -7,7 +7,7 @@ use Psalm\Internal\Analyzer\Statements\Expression\ExpressionIdentifier;
 use Psalm\Internal\Analyzer\StatementsAnalyzer;
 use Psalm\Type;
 
-use function array_is_list;
+use function count;
 
 class UnsetAnalyzer
 {
@@ -52,8 +52,12 @@ class UnsetAnalyzer
                                 || $var->dim instanceof PhpParser\Node\Scalar\LNumber
                             ) {
                                 if (isset($atomic_root_type->properties[$var->dim->value])) {
+                                    if ($atomic_root_type->is_list
+                                        && $var->dim->value !== count($atomic_root_type->properties)-1
+                                    ) {
+                                        $atomic_root_type->is_list = false;
+                                    }
                                     unset($atomic_root_type->properties[$var->dim->value]);
-                                    $atomic_root_type->is_list = array_is_list($atomic_root_type->properties);
                                     $root_type->bustCache(); //remove id cache
                                 }
 
