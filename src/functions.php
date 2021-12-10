237ef63fb53d2b64a404d55d4c247db8ff017e85
ralila <>
2021-12-10 15:57:18 +0100

    Adjust php-doc

--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,8 @@ namespace Psalm;
 use Symfony\Component\Filesystem\Path;
 
 /**
- * @deprecated Use {@see Webmozart\PathUtil\Path::isAbsolute}. No longer used by Psalm, going to be removed in Psalm 5
+ * @deprecated Use {@see Symfony\Component\Filesystem\Path::isAbsolute}.
+ *             No longer used by Psalm, going to be removed in Psalm 5
  */
 function isAbsolutePath(string $path): bool
 {
