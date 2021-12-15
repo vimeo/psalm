03ccb9b548952aa107cd44973d62217a1710f54d
AndrolGenhald <AndrolGenhald@gmail.com>
2021-12-15 11:09:10 -0600

    Add test for nested `array_map` return type.

--- tests/ReturnTypeTest.php
+++ tests/ReturnTypeTest.php
@@ -1065,6 +1065,29 @@ class ReturnTypeTest extends TestCase
                         }
                     }'
             ],
+            'nestedArrayMapReturnTypeDoesntCrash' => [
+                '<?php
+                    function bar(array $a): array {
+                        return $a;
+                    }
+
+                    /**
+                     * @param array[] $x
+                     *
+                     * @return array[]
+                     */
+                    function foo(array $x): array {
+                        return array_map(
+                            "array_merge",
+                            array_map(
+                                "bar",
+                                $x
+                            ),
+                            $x
+                        );
+                    }
+                ',
+            ],
         ];
     }
 
