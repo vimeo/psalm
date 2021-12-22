c94364631b39e62ec5703f153e33a42570be4010
Tomasz Mlynski <devilshakerz@gmail.com>
2021-12-22 20:23:11 +0100

    Normalize variable name

--- src/Psalm/Internal/Analyzer/ProjectAnalyzer.php
+++ src/Psalm/Internal/Analyzer/ProjectAnalyzer.php
@@ -1053,12 +1053,12 @@ class ProjectAnalyzer
     private function checkDirWithConfig(string $dir_name, Config $config, bool $allow_non_project_files = false): void
     {
         $file_extensions = $config->getFileExtensions();
-        $directory_filter = $allow_non_project_files ? null : [$this->config, 'isInProjectDirs'];
+        $filter = $allow_non_project_files ? null : [$this->config, 'isInProjectDirs'];
 
         $file_paths = $this->file_provider->getFilesInDir(
             $dir_name,
             $file_extensions,
-            $directory_filter
+            $filter
         );
 
         $files_to_scan = [];
