3d212c642cff81dada3ab0619c55d5cdf77337fc
adrew <klimichkartorgnusov@gmail.com>
2021-12-22 16:34:25 +0300

    Add test for mismatch param type from docblock

--- tests/CallableTest.php
+++ tests/CallableTest.php
@@ -1318,6 +1318,42 @@ class CallableTest extends TestCase
                     c("hii");',
                 'error_message' => 'InvalidArgument',
             ],
+            'mismatchParamTypeFromDocblock' => [
+                '<?php
+                    /**
+                     * @template A
+                     */
+                    final class ArrayList
+                    {
+                        /**
+                         * @template B
+                         * @param Closure(A): B $effect
+                         * @return ArrayList<B>
+                         */
+                        public function map(Closure $effect): ArrayList
+                        {
+                            throw new RuntimeException("???");
+                        }
+                    }
+
+                    /**
+                     * @template T
+                     * @template B
+                     *
+                     * @param ArrayList<T> $list
+                     * @return ArrayList<array{T}>
+                     */
+                    function genericContext(ArrayList $list): ArrayList
+                    {
+                        return $list->map(
+                            /** @param B $_a */
+                            function ($_a) {
+                                return [$_a];
+                            }
+                        );
+                    }',
+                'error_message' => 'InvalidArgument',
+            ]
         ];
     }
 }
