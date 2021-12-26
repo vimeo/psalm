f7b6c4e1b692d12d3595a5044a7a7e64dfd5b15f
orklah <ronan_morin@hotmail.com>
2021-12-26 19:19:47 +0100

    escape more special chars in keyed array names that won't be read  by the parser

--- tests/AssertAnnotationTest.php
+++ tests/AssertAnnotationTest.php
@@ -1684,6 +1684,40 @@ class AssertAnnotationTest extends TestCase
                         }
                     }',
             ],
+            'assertOnKeyedArrayWithSpecialCharsInNames' => [
+                '<?php
+
+                    class Foo {
+                        /** @var array<string, int> */
+                        public array $bar;
+
+                        /**
+                         * @param array<string, int> $bar
+                         */
+                        public function __construct(array $bar) {
+                            $this->bar = $bar;
+                        }
+                    }
+
+                    $expected = [
+                        "#[]" => 21,
+                        "<<>>" => 6,
+                    ];
+
+                    $foo = new Foo($expected);
+                    assertSame($expected, $foo->bar);
+
+                    /**
+                     * @psalm-template ExpectedType
+                     * @psalm-param ExpectedType $expected
+                     * @psalm-assert =ExpectedType $actual
+                     */
+                    function assertSame($expected, $actual): void {
+                        if ($expected !== $actual) {
+                            throw new Exception("Expected doesn\'t match actual");
+                        }
+                    }',
+            ],
         ];
     }
 
