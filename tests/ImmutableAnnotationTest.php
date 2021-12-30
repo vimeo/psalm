392402cf6d244aa871c6de9cb7c02c1aaee710ed
orklah <ronan_morin@hotmail.com>
2021-12-30 22:29:36 +0100

    allow calling mutation_free function inside a mutation_free context

--- tests/ImmutableAnnotationTest.php
+++ tests/ImmutableAnnotationTest.php
@@ -526,6 +526,26 @@ class ImmutableAnnotationTest extends TestCase
                         }
                     }',
             ],
+            'allowMutationFreeCallInMutationFreeContext' => [
+                '<?php
+
+                    /**
+                     * @psalm-mutation-free
+                     */
+                    function getData(): array {
+                        /** @var mixed $arr */
+                        $arr = $GLOBALS["cachedData"] ?? [];
+
+                        return is_array($arr) ? $arr : [];
+                    }
+
+                    /**
+                     * @psalm-mutation-free
+                     */
+                    function getDataItem(string $key): mixed {
+                        return getData()[$key] ?? null;
+                    }',
+            ],
         ];
     }
 
