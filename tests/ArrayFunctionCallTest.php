65efab3a8de2a99732c377971c958eebb09e98dc
orklah <ronan_morin@hotmail.com>
2022-01-09 15:48:58 +0100

    keep class-strings through array_merge

--- tests/ArrayFunctionCallTest.php
+++ tests/ArrayFunctionCallTest.php
@@ -2121,6 +2121,24 @@ class ArrayFunctionCallTest extends TestCase
                         return $a;
                     }',
             ],
+            'arrayUnshiftOnEmptyArrayMeansNonEmptyList' => [
+                '<?php
+
+                    class A {
+                        /** @var array<class-string, string> */
+                        private array $a;
+
+                        public function __construct() {
+                            $this->a = [];
+                        }
+
+                        public function handle(): void {
+                            $b = [A::class => "d"];
+                            $this->a = array_merge($this->a, $b);
+                        }
+                    }
+                    ',
+            ],
         ];
     }
 
