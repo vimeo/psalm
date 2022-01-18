379d0d6c494e0e18f9e812c718ae692c2465a8a2
adrew <klimichkartorgnusov@gmail.com>
2022-01-18 18:54:47 +0300

    Add tests

--- tests/CallableTest.php
+++ tests/CallableTest.php
@@ -295,6 +295,77 @@ class CallableTest extends TestCase
                     '$result' => 'ArrayList<int>',
                 ],
             ],
+            'inferTemplateOfHighOrderFunctionFromMethodArgByPreviousArg' => [
+                '<?php
+                     final class Ops
+                     {
+                         /**
+                          * @template T
+                          * @return Closure(list<T>): T
+                          */
+                         public function flatten() { throw new RuntimeException("???"); }
+                     }
+                     /**
+                      * @return list<list<int>>
+                      */
+                     function getList() { throw new RuntimeException("???"); }
+                     /**
+                      * @template T
+                      * @return Closure(list<T>): T
+                      */
+                     function flatten() { throw new RuntimeException("???"); }
+                     /**
+                      * @template A
+                      * @template B
+                      *
+                      * @param list<A> $_a
+                      * @param callable(A): B $_ab
+                      * @return list<B>
+                      */
+                     function map(array $_a, callable $_ab) { throw new RuntimeException("???"); }
+
+                     $ops = new Ops;
+                     $result = map(getList(), $ops->flatten());
+                 ',
+                'assertions' => [
+                    '$result' => 'list<int>',
+                ],
+            ],
+            'inferTemplateOfHighOrderFunctionFromStaticMethodArgByPreviousArg' => [
+                '<?php
+                     final class StaticOps
+                     {
+                         /**
+                          * @template T
+                          * @return Closure(list<T>): T
+                          */
+                         public static function flatten() { throw new RuntimeException("???"); }
+                     }
+                     /**
+                      * @return list<list<int>>
+                      */
+                     function getList() { throw new RuntimeException("???"); }
+                     /**
+                      * @template T
+                      * @return Closure(list<T>): T
+                      */
+                     function flatten() { throw new RuntimeException("???"); }
+                     /**
+                      * @template A
+                      * @template B
+                      *
+                      * @param list<A> $_a
+                      * @param callable(A): B $_ab
+                      * @return list<B>
+                      */
+                     function map(array $_a, callable $_ab) { throw new RuntimeException("???"); }
+
+                     $result = map(getList(), StaticOps::flatten());
+                 ',
+                'assertions' => [
+                    '$result' => 'list<int>',
+                ],
+            ],
             'inferPipelineWithPartiallyAppliedFunctions' => [
                 '<?php
                     /**
