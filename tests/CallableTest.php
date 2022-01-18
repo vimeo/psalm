3886d5b4c6642342addf4190e1a2076b073ceb33
adrew <klimichkartorgnusov@gmail.com>
2022-01-18 13:12:01 +0300

    Add test for high order function arg inference in class context

--- tests/CallableTest.php
+++ tests/CallableTest.php
@@ -262,6 +262,39 @@ class CallableTest extends TestCase
                     '$result' => 'list<int>',
                 ],
             ],
+            'inferTemplateOfHighOrderFunctionArgByPreviousArgInClassContext' => [
+                '<?php
+                    /**
+                     * @template A
+                     */
+                    final class ArrayList
+                    {
+                        /**
+                         * @template B
+                         *
+                         * @param callable(A): B $ab
+                         * @return ArrayList<B>
+                         */
+                        public function map(callable $ab) { throw new RuntimeException("???"); }
+                    }
+
+                    /**
+                     * @return ArrayList<int>
+                     */
+                    function getList() { throw new RuntimeException("???"); }
+
+                    /**
+                     * @template T
+                     * @return Closure(T): T
+                     */
+                    function id() { throw new RuntimeException("???"); }
+
+                    $result = getList()->map(id());
+                ',
+                'assertions' => [
+                    '$result' => 'ArrayList<int>',
+                ],
+            ],
             'inferPipelineWithPartiallyAppliedFunctions' => [
                 '<?php
                     /**
