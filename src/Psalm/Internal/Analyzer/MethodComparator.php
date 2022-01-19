38945018f55339cae3a05f7e8d400c2ac049d592
Daniil Gentili <daniil@daniil.it>
2022-01-19 12:39:08 +0100

    Run analysis on all PHP versions

--- src/Psalm/Internal/Analyzer/MethodComparator.php
+++ src/Psalm/Internal/Analyzer/MethodComparator.php
@@ -118,7 +118,6 @@ class MethodComparator
 
         if (!$guide_classlike_storage->user_defined
             && $implementer_classlike_storage->user_defined
-            && $codebase->analysis_php_version_id >= 80100
             && ($guide_method_storage->return_type
                 || $guide_method_storage->signature_return_type
             )
