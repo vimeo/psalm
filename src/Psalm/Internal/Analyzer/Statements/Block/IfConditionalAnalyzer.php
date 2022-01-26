7aca2ba9e9368594f5c05197c2ba7c3d781b33a8
Matthew Brown <github@muglug.com>
2022-01-26 16:00:25 -0500

    Fix return type of method never returning null

--- src/Psalm/Internal/Analyzer/Statements/Block/IfConditionalAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/Block/IfConditionalAnalyzer.php
@@ -114,14 +114,12 @@ class IfConditionalAnalyzer
 
         $outer_context->inside_conditional = true;
 
-        if ($externally_applied_if_cond_expr) {
-            if (ExpressionAnalyzer::analyze(
-                $statements_analyzer,
-                $externally_applied_if_cond_expr,
-                $outer_context
-            ) === false) {
-                throw new ScopeAnalysisException();
-            }
+        if (ExpressionAnalyzer::analyze(
+            $statements_analyzer,
+            $externally_applied_if_cond_expr,
+            $outer_context
+        ) === false) {
+            throw new ScopeAnalysisException();
         }
 
         $first_cond_assigned_var_ids = $outer_context->assigned_var_ids;
@@ -236,7 +234,7 @@ class IfConditionalAnalyzer
      * Returns statements that are definitely evaluated before any statements after the end of the
      * if/elseif/else blocks
      */
-    private static function getDefinitelyEvaluatedExpressionAfterIf(PhpParser\Node\Expr $stmt): ?PhpParser\Node\Expr
+    private static function getDefinitelyEvaluatedExpressionAfterIf(PhpParser\Node\Expr $stmt): PhpParser\Node\Expr
     {
         if ($stmt instanceof PhpParser\Node\Expr\BinaryOp\Equal
             || $stmt instanceof PhpParser\Node\Expr\BinaryOp\Identical
@@ -280,7 +278,7 @@ class IfConditionalAnalyzer
      * Returns statements that are definitely evaluated before any statements inside
      * the if block
      */
-    private static function getDefinitelyEvaluatedExpressionInsideIf(PhpParser\Node\Expr $stmt): ?PhpParser\Node\Expr
+    private static function getDefinitelyEvaluatedExpressionInsideIf(PhpParser\Node\Expr $stmt): PhpParser\Node\Expr
     {
         if ($stmt instanceof PhpParser\Node\Expr\BinaryOp\Equal
             || $stmt instanceof PhpParser\Node\Expr\BinaryOp\Identical
