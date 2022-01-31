5a19d42daba4aac06c6abe77d21999c082976139
orklah <ronan_morin@hotmail.com>
2022-01-31 21:51:31 +0100

    fix test

--- tests/ClosureTest.php
+++ tests/ClosureTest.php
@@ -800,6 +800,7 @@ class ClosureTest extends TestCase
             ],
             'unknownFirstClassCallable' => [
                 '<?php
+                    /** @psalm-suppress UndefinedFunction */
                     unknown(...);',
             ],
         ];
