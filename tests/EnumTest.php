aaf34d75734d4a238a00eab92f5f7c53aadad781
orklah <ronan_morin@hotmail.com>
2022-02-13 19:05:51 +0100

    add test

--- tests/EnumTest.php
+++ tests/EnumTest.php
@@ -405,6 +405,35 @@ class EnumTest extends TestCase
                 'ignored_issues' => [],
                 'php_version' => '8.1',
             ],
+            'EnumCaseReconciliation' => [
+                'code' => '<?php
+                    enum Code: int
+                    {
+                        case Ok = 0;
+                        case Fatal = 1;
+                    }
+
+                    function foo(): Code|null
+                    {
+                        return null;
+                    }
+
+                    $code = foo();
+                    $code1 = null;
+                    $code2 = null;
+                    if($code instanceof Code){
+                        $code1 = $code;
+                    }
+                    if(!$code instanceof Code){
+                        $code2 = $code;
+                    }',
+                'assertions' => [
+                    '$code1' => 'Code|null',
+                    '$code2' => 'null',
+                ],
+                'ignored_issues' => [],
+                'php_version' => '8.1',
+            ],
         ];
     }
 
