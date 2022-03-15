b00d9e9514552d5fa2f359302c94d9d0b71745e7
Oliver Hader <oliver@typo3.org>
2022-03-15 03:12:14 +0100

    Add test case reproducing issue #7771
    Related: #7771

--- tests/TypeReconciliation/ConditionalTest.php
+++ tests/TypeReconciliation/ConditionalTest.php
@@ -2839,6 +2839,15 @@ class ConditionalTest extends TestCase
                         echo "false";
                     }',
             ],
+            'BinaryDataIsPassedSeeIssue7771' => [
+                'code' => '<?php
+                    function matches(string $value): bool {
+                        if ("\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1" !== $value) {
+                            return false;
+                        }
+                        return true;
+                    }'
+            ],
         ];
     }
 
