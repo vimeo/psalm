b72f0564bc9b05b8896d584b490c98fcb0729ccc
orklah <ronan_morin@hotmail.com>
2022-04-05 21:05:23 +0200

    fix refining lowercase string and non-empty-string together

--- tests/AssertAnnotationTest.php
+++ tests/AssertAnnotationTest.php
@@ -2038,6 +2038,23 @@ class AssertAnnotationTest extends TestCase
                     }
                 ',
             ],
+            'assertNonEmptyStringWithLowercaseString' => [
+                'code' => '<?php
+
+                    /** @psalm-assert non-empty-string $input */
+                    function assertLowerCase(string $input): void { throw new \Exception($input . " irrelevant"); }
+
+                    /**
+                     * @param lowercase-string $input
+                     * @return non-empty-lowercase-string
+                     */
+                    function makeLowerNonEmpty(string $input): string
+                    {
+                        assertLowerCase($input);
+
+                        return $input;
+                    }',
+            ],
         ];
     }
 
