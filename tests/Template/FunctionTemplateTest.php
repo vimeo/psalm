c4187c55a9346848223b19656b1f73c2278ee0ca
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2022-04-16 00:03:19 +0900

    add test case

--- tests/Template/FunctionTemplateTest.php
+++ tests/Template/FunctionTemplateTest.php
@@ -1606,6 +1606,22 @@ class FunctionTemplateTest extends TestCase
                         }
                     }'
             ],
+            'dontScreamForArithmeticsOnFloatTemplates' => [
+                '<?php
+
+                    /**
+                     * @template T of ?float
+                     * @param T $p
+                     * @return (T is null ? null : float)
+                     */
+                    function foo(?float $p): ?float
+                    {
+                        if ($p === null) {
+                            return null;
+                        }
+                        return $p - 1;
+                    }'
+            ],
         ];
     }
 
