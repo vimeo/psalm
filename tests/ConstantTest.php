80e94daefb1cc05651a16ec63ffff3ffc8d1295e
AndrolGenhald <AndrolGenhald@gmail.com>
2022-05-20 14:37:35 -0500

    Fix class const issue when using floats declared in future consts (fixes #7973).
     - Calculate literal type for float arithmetic instead of only for int arithmetic
     - Fix copy/paste fail causing InvalidConstantAssignmentValue to be marked as fixable

--- tests/ConstantTest.php
+++ tests/ConstantTest.php
@@ -1419,6 +1419,15 @@ class ConstantTest extends TestCase
                     interface Bar extends Foo {}
                 ',
             ],
+            'classConstsUsingFutureFloatDeclarationWithMultipleLevels' => [
+                'code' => '<?php
+                    class Foo {
+                        public const BAZ = self::BAR + 1.0;
+                        public const BAR = self::FOO + 1.0;
+                        public const FOO = 1.0;
+                    }
+                ',
+            ],
         ];
     }
 
