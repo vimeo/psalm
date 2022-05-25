6305c21b5b1984546e3a8fc5adc9383f54cc85f6
Christian Schiffler <c.schiffler@cyberspectrum.de>
2022-05-25 08:50:19 +0200

    Remove now obsolete loop
    The class names in property map are now always lowercase

--- src/Psalm/Internal/Codebase/PropertyMap.php
+++ src/Psalm/Internal/Codebase/PropertyMap.php
@@ -29,12 +29,7 @@ class PropertyMap
         /** @var array<string, array<string, string>> */
         $property_map = require(dirname(__DIR__, 4) . '/dictionaries/PropertyMap.php');
 
-        self::$property_map = [];
-
-        foreach ($property_map as $key => $value) {
-            $cased_key = strtolower($key);
-            self::$property_map[$cased_key] = $value;
-        }
+        self::$property_map = $property_map;
 
         return self::$property_map;
     }
