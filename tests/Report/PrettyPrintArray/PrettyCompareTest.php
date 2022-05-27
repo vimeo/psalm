9f46f65f40e5ec2dd3734b5b8281b4f9e134d3f1
BruceGitHub <roberto.diana@gmail.com>
2022-05-28 00:08:39 +0200

    adds test for PrettyCompareTest.php

---
+++ tests/Report/PrettyPrintArray/PrettyCompareTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Psalm\Tests\Report\PrettyPrintArray;
+
+use Generator;
+use Psalm\Report\PrettyPrintArray\PrettyCompare;
+use Psalm\Tests\TestCase;
+
+use function explode;
+
+use const PHP_EOL;
+
+class PrettyCompareTest extends TestCase
+{
+    /**
+     * @dataProvider providerValidPayload
+     */
+    public function testCompare(string $inferred, string $declared, string $expected): void
+    {
+        $sut = new PrettyCompare();
+        $actual = $sut->compare($inferred, $declared);
+
+        $this->assertOutputPrettyPrintEquals($expected, $actual);
+    }
+
+    private function assertOutputPrettyPrintEquals(string $expected_output, string $output): void
+    {
+        $linesOutput = explode(PHP_EOL, ($expected_output));
+
+        foreach ($linesOutput as $line) {
+            $this->assertStringContainsString(
+                $line,
+                $output
+            );
+        }
+    }
+
+
+    /**
+     * @return Generator<int, array{string, string, string}>
+     */
+    public function providerValidPayload(): Generator
+    {
+        $inferred = <<<"EOT"
+        field: value,
+        field2: value2,
+        arr1: array {
+         arr2: array {
+          arr3: array {
+           foo: bar
+          }
+         }
+        }
+        EOT;
+
+        $declared = <<<"EOT"
+        field: value,
+        field2: value2,
+        arr1: array {
+         arr2: array {
+          arr3: array {
+           foo: bar
+          }
+         }
+        }
+        EOT;
+
+        $expected = <<<"EOT"
+        | Expected                                           | Provided
+        | ---                                                | ---
+        | field: value,                                      | field: value,
+        | field2: value2,                                    | field2: value2,
+        | arr1: array {                                      | arr1: array {
+        |  arr2: array {                                     |  arr2: array {
+        |   arr3: array {                                    |   arr3: array {
+        |    foo: bar                                        |    foo: bar
+        |   }                                                |   }
+        |  }                                                 |  }
+        | }                                                  | }
+        |                                                    |
+        EOT;
+
+        yield [$inferred, $declared, $expected];
+    }
+}
