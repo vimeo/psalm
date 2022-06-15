ded566e79a19e37f55cfe6073778042b44801f31
Sam Mousa <sam@mousa.nl>
2022-06-15 10:56:15 +0200

    fix: callmap improvements and updated ignore list

--- tests/Internal/Codebase/InternalCallMapHandlerTest.php
+++ tests/Internal/Codebase/InternalCallMapHandlerTest.php
@@ -2,6 +2,7 @@
 
 namespace Psalm\Tests\Internal\Codebase;
 
+use Psalm\Codebase;
 use Psalm\Internal\Analyzer\ProjectAnalyzer;
 use Psalm\Internal\Codebase\InternalCallMapHandler;
 use Psalm\Internal\Codebase\Reflection;
@@ -16,20 +17,52 @@ use Psalm\Type;
 use ReflectionFunction;
 use ReflectionNamedType;
 use ReflectionParameter;
+use ReflectionType;
 
 class InternalCallMapHandlerTest extends TestCase
 {
-    private $ignoredFunctions = [
+    private static $ignoredFunctions = [
         'sprintf', 'printf', 'ctype_print', 'date_sunrise' /** deprecated in 8.1 */,
-        'ctype_digit', 'ctype_lower', 'ctype_alnum', 'ctype_alpha', 'ctype_cntrl',
-        'ctype_graph', 'ctype_lower', 'ctype_print', 'ctype_punct', 'ctype_space', 'ctype_upper',
-        'ctype_xdigit', 'file_put_contents', 'sodium_crypto_generichash', 'sodium_crypto_generichash_final',
-        'dom_import_simplexml', 'imagegd', 'imagegd2', 'pg_exec', 'mysqli_execute', 'array_multisort',
-        'memcache_add_server', 'memcache_cas', 'memcache_pconnect', 'memcache_connect', 'memcache_delete',
-        'memcache_replace', 'memcache_append', 'memcache_increment', 'memcache_set_failure_callback', 'memcache_decrement',
-        'memcache_set_server_params', 'memcache_get_server_status', 'memcache_prepend', 'memcache_flush'
+        'file_put_contents',
+        'dom_import_simplexml', 'imagegd', 'imagegd2', 'mysqli_execute', 'array_multisort',
+        'intlcal_from_date_time', 'simplexml_import_dom', 'imagefilledpolygon',
+        /** deprecated in 8.0 */
+        'zip_entry_close'
 
     ];
+
+    private static $ignoredPrefixes = [
+        'memcache_',
+        'gnupg_',
+        'mailparse_',
+        'xdiff_',
+        'oci',
+        'ldap_',
+        'yaml_',
+        'collator_',
+        'ctype_',
+        'imap_',
+        'gmp_',
+        'uopz_',
+        'openssl_',
+        'tidy_',
+        'image',
+        'transliterator_',
+        'pg_',
+        'odbc_',
+        'sqlsrv_',
+        'mysqli_',
+        'apcu_',
+        'bc',
+        'sodium_',
+        'intl',
+    ];
+
+    /**
+     * Initialized in setup
+     * @var string Regex
+     */
+    private static $prefixRegex = '//';
 
     /**
      *
@@ -37,11 +70,30 @@ class InternalCallMapHandlerTest extends TestCase
      */
     private $skipUndefinedParams = false;
 
-    /**
-     * These are items that very likely need updating to PHP8.1
-     * @var bool whether to skip params that are specified in the callmap as `resource`
-     */
-    private $skipResources = false;
+    private static $codebase;
+    private static $callMap;
+
+
+
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$prefixRegex = '/^(' . implode('|', self::$ignoredPrefixes) . ')/';
+        $project_analyzer = new ProjectAnalyzer(
+            new TestConfig(),
+            new Providers(
+                new FakeFileProvider(),
+                new FakeParserCacheProvider()
+            )
+        );
+        self::$codebase = $project_analyzer->getCodebase();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$codebase = null;
+
+    }
 
     /**
      * @covers \Psalm\Internal\Codebase\InternalCallMapHandler::getCallMap
@@ -63,7 +115,6 @@ class InternalCallMapHandlerTest extends TestCase
     public function callMapEntryProvider(): iterable
     {
 
-
         $project_analyzer = new ProjectAnalyzer(
             new TestConfig(),
             new Providers(
@@ -72,20 +123,16 @@ class InternalCallMapHandlerTest extends TestCase
             )
         );
         $callMap = InternalCallMapHandler::getCallMap();
-        unset($project_analyzer);
-
         foreach($callMap as $function => $entry) {
             // Skip class methods
             if (strpos($function, '::') !== false || !function_exists($function)) {
-                continue;
-            }
-            if (!str_starts_with($function, 'memcache_')) {
                 continue;
             }
             // Skip functions with alternate signatures
             if (isset($callMap["$function'1"]) || preg_match("/\'\d$/", $function)) {
                 continue;
             }
+            // if ($function != 'fprintf') continue;
             yield "$function: " . json_encode($entry) => [$function, $entry];
         }
 
@@ -103,6 +150,12 @@ class InternalCallMapHandlerTest extends TestCase
     {
         if (!function_exists($functionName)) {
             $this->markTestSkipped("Function $functionName does not exist");
+        }
+        if (in_array($functionName, self::$ignoredFunctions)) {
+            $this->markTestSkipped("Function $functionName is ignored in config");
+        }
+        if (preg_match(self::$prefixRegex, $functionName)) {
+            $this->markTestSkipped("Function $functionName has ignored prefix");
         }
         $this->assertEntryIsCorrect($callMapEntry, $functionName);
 
@@ -128,27 +181,35 @@ class InternalCallMapHandlerTest extends TestCase
                 'byRef' => false,
                 'optional' => false,
                 'type' => $entry,
-                'name' => $key
             ];
-            // Strip prefixes.
-            if (strncmp($normalizedKey, '&rw_', 4) === 0) {
+            if (strncmp($normalizedKey, '&', 1) === 0) {
                 $normalizedEntry['byRef'] = true;
-                $normalizedEntry['refMode'] = 'rw';
-                $normalizedKey = substr($normalizedKey, 4);
-            } elseif (strncmp($normalizedKey, '&w_', 3) === 0) {
-                $normalizedEntry['byRef'] = true;
-                $normalizedEntry['refMode'] = 'w';
-                $normalizedKey = substr($normalizedKey, 3);
+                $normalizedKey = substr($normalizedKey, 1);
             }
+
             if (strncmp($normalizedKey, '...', 3) === 0) {
                 $normalizedEntry['variadic'] = true;
                 $normalizedKey = substr($normalizedKey, 3);
             }
+
+            // Read the reference mode
+            if ($normalizedEntry['byRef']) {
+                $parts = explode('_', $normalizedKey, 2);
+                if (count($parts) === 2) {
+                    $normalizedEntry['refMode'] = $parts[0];
+                    $normalizedKey = $parts[1];
+                } else {
+                    $normalizedEntry['refMode'] = 'rw';
+                }
+            }
+
+            // Strip prefixes.
             if (substr($normalizedKey, -1, 1) === "=") {
                 $normalizedEntry['optional'] = true;
                 $normalizedKey = substr($normalizedKey, 0, -1);
             }
 
+            $normalizedEntry['name'] = $normalizedKey;
             $normalizedEntries[$normalizedKey] = $normalizedEntry;
 
         }
@@ -156,7 +217,7 @@ class InternalCallMapHandlerTest extends TestCase
             if ($this->skipUndefinedParams && !isset($normalizedEntries[$parameter->getName()])) {
                 continue;
             } else {
-                $this->assertArrayHasKey($parameter->getName(), $normalizedEntries);
+                $this->assertArrayHasKey($parameter->getName(), $normalizedEntries, "Callmap is missing entry for param {$parameter->getName()} in $functionName: " . print_r($normalizedEntries, true));
             }
             $this->assertParameter($normalizedEntries[$parameter->getName()], $parameter, $functionName);
         }
@@ -169,9 +230,6 @@ class InternalCallMapHandlerTest extends TestCase
      */
     private function assertParameter(array $normalizedEntry, ReflectionParameter $param, string $functionName): void
     {
-        if (in_array($functionName, $this->ignoredFunctions)) {
-            $this->markTestSkipped('Function is ignored in config');
-        }
         $name = $param->getName();
         // $identifier = "Param $functionName - $name";
         try {
@@ -179,17 +237,14 @@ class InternalCallMapHandlerTest extends TestCase
         $this->assertSame($param->isVariadic(), $normalizedEntry['variadic'], "Expected param '{$name}' to " . ($param->isVariadic() ? "be" : "not be") . " variadic");
         $this->assertSame($param->isPassedByReference(), $normalizedEntry['byRef'], "Expected param '{$name}' to " . ($param->isPassedByReference() ? "be" : "not be") . " by reference");
         } catch(\Throwable $t) {
-            $this->markTestSkipped($t->getMessage());
+            $this->markTestSkipped("Exception: " . $t->getMessage());
         }
 
         $expectedType = $param->getType();
 
-        if ($expectedType instanceof ReflectionNamedType) {
+        if (isset($expectedType)) {
             $this->assertTypeValidity($expectedType, $normalizedEntry['type'], "Param '{$name}' has incorrect type");
-        } else {
-            // $this->markTestSkipped('Only simple named types are tested');
         }
-
 
 
     }
@@ -197,75 +252,11 @@ class InternalCallMapHandlerTest extends TestCase
     /**
      * Since string equality is too strict, we do some extra checking here
      */
-    private function assertTypeValidity(ReflectionNamedType $reflected, string $specified, string $message): void
+    private function assertTypeValidity(ReflectionType $reflected, string $specified, string $message): void
     {
         $expectedType = Reflection::getPsalmTypeFromReflectionType($reflected);
         $parsedType = Type::parseString($specified);
 
-        if (UnionTypeComparator::isContainedByInPhp($parsedType, $expectedType)) {
-            return;
-        }
-
-        // In case reflection returns mixed we assume any type specified in the callmap is more specific and correct
-        if ($reflected->getName() === 'mixed') {
-            return;
-        }
-
-
-        if ($reflected->getName() === 'callable' && $reflected->allowsNull()
-            && preg_match('/^(null\|callable\(.*\):.*|callable\(.*\):.*\|null)$/', $specified)
-        ) {
-            return;
-        }
-        // Trim leading namespace separator
-        $specified = ltrim($specified, "\\");
-        if ($reflected->getName() === 'array' && !$reflected->allowsNull()) {
-            if (preg_match('/^(array|list|non-empty-array)(<.*>|{.*})?$/', $specified)
-                || in_array($specified, ['string[]|int[]'])
-            ) {
-                return;
-            }
-        } elseif($reflected->getName() === 'array') {
-            // Optional array
-            if (preg_match('/^((array|list|non-empty-array)(<.*>|{.*})?\|null|null\|(array|list|non-empty-array)(<.*>|{.*})?)$/', $specified)) {
-                return;
-            }
-        }
-
-        if ($reflected->getName() === 'float' && in_array($specified, ['int|float', 'float|int'])) {
-            return;
-        }
-        if ($reflected->getName() === 'bool' && in_array($specified, ['true', 'false'])) {
-            return;
-        }
-        if ($reflected->getName() === 'callable' && preg_match('/^callable\(/', $specified)) {
-            return;
-        }
-
-        if ($reflected->getName() === 'string' && $specified === '?string' && $reflected->allowsNull()) {
-            return;
-        }
-        if ($reflected->getName() === 'string' && in_array($specified , ['class-string', 'numeric-string', 'string'])) {
-            return;
-        }
-        if ($reflected->getName() === 'int' &&  preg_match('/^(\d+|positive-int|int(<\d+,\d+>))?(\|(\d+|positive-int|int))*$/', $specified)) {
-
-        // in_array($specified , [
-        //     'positive-int', 'int', '0|positive-int', '256|512|1024|16384', '1|2|3|4|5|6|7'
-        //     ])) {
-            return;
-        }
-
-
-        if ($reflected->allowsNull()) {
-            $escaped = preg_quote($reflected->getName());
-            $this->assertMatchesRegularExpression("/^(\?{$escaped}|{$escaped}\|null|null\|{$escaped})$/", $specified, $message);
-            return;
-        }
-
-        if ($this->skipResources && $specified === 'resource') {
-            return;
-        }
-        $this->assertEqualsIgnoringCase($reflected->getName(), $specified, $message);
+        $this->assertTrue(UnionTypeComparator::isContainedBy(self::$codebase, $parsedType, $expectedType), $message);
     }
 }
