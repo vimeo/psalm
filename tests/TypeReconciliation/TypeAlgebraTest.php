e4f73beb0848c120207d2ed71a0e354d4e4b2e6f
Matthew Brown <github@muglug.com>
2022-07-17 14:06:07 -0400

    Add comment for skipped test

--- tests/TypeReconciliation/TypeAlgebraTest.php
+++ tests/TypeReconciliation/TypeAlgebraTest.php
@@ -212,6 +212,9 @@ class TypeAlgebraTest extends TestCase
                         }
                     }',
             ],
+            // skipping this old test because it breaks if the elseif becomes
+            // an else if, and I don't believe it's a pattern that happens often
+            // enough to warrant special-casing
             'SKIPPED-twoVarLogicNotNestedWithElseifCorrectlyReinforcedInIf' => [
                 'code' => '<?php
                     class A {}
