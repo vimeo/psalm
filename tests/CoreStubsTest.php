462ce7138ac926e832a37ed4be47b1a8ce70e1d2
Semyon <7ionmail@gmail.com>
2022-07-25 16:37:49 +0300

    Make DatePeriod implement Traversable oh PHP 7, rename constructor params

--- tests/CoreStubsTest.php
+++ tests/CoreStubsTest.php
@@ -33,7 +33,26 @@ class CoreStubsTest extends TestCase
             'error_levels' => [],
             'php_version' => '8.0',
         ];
-        yield 'Iterating over \DatePeriod (#5954)' => [
+        yield 'Iterating over \DatePeriod (#5954) PHP7 Traversable' => [
+            '<?php
+
+            $period = new DatePeriod(
+                new DateTimeImmutable("now"),
+                DateInterval::createFromDateString("1 day"),
+                new DateTime("+1 week")
+            );
+            $dt = null;
+            foreach ($period as $dt) {
+                echo $dt->format("Y-m-d");
+            }',
+            'assertions' => [
+                '$period' => 'DatePeriod<DateTimeImmutable>',
+                '$dt' => 'DateTimeInterface|null'
+            ],
+            'error_levels' => [],
+            'php_version' => '7.3',
+        ];
+        yield 'Iterating over \DatePeriod (#5954) PHP8 IteratorAggregate' => [
             '<?php
 
             $period = new DatePeriod(
@@ -49,6 +68,8 @@ class CoreStubsTest extends TestCase
                 '$period' => 'DatePeriod<DateTimeImmutable>',
                 '$dt' => 'DateTimeImmutable|null'
             ],
+            'error_levels' => [],
+            'php_version' => '8.0',
         ];
         yield 'Iterating over \DatePeriod (#5954), ISO string' => [
             '<?php
@@ -62,6 +83,26 @@ class CoreStubsTest extends TestCase
                 '$period' => 'DatePeriod<string>',
                 '$dt' => 'DateTime|null'
             ],
+            'error_levels' => [],
+            'php_version' => '8.0',
+        ];
+        yield 'DatePeriod implements only Traversable on PHP 7' => [
+            '<?php
+
+            $period = new DatePeriod("R4/2012-07-01T00:00:00Z/P7D");
+            if ($period instanceof IteratorAggregate) {}',
+            'assertions' => [],
+            'error_levels' => [],
+            'php_version' => '7.3',
+        ];
+        yield 'DatePeriod implements IteratorAggregate on PHP 8' => [
+            '<?php
+
+            $period = new DatePeriod("R4/2012-07-01T00:00:00Z/P7D");
+            if ($period instanceof IteratorAggregate) {}',
+            'assertions' => [],
+            'error_levels' => ['RedundantCondition'],
+            'php_version' => '8.0',
         ];
     }
 }
