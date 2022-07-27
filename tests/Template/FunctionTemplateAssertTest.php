37274b308f7ec573067c653eabef10b63ff5a605
Daniil Gentili <daniil@daniil.it>
2022-07-27 17:51:55 +0200

    Add temporary failing test

--- tests/Template/FunctionTemplateAssertTest.php
+++ tests/Template/FunctionTemplateAssertTest.php
@@ -1123,6 +1123,27 @@ class FunctionTemplateAssertTest extends TestCase
                     }',
                 'error_message' => 'RedundantCondition',
             ],
+            'SKIPPED-assertNotSameClasses' => [
+                'code' => '<?php
+                    /**
+                     * Asserts that two variables are the same.
+                     *
+                     * @template T
+                     * @param T      $expected
+                     * @param mixed  $actual
+                     * @psalm-assert =T $actual
+                     */
+                    function assertSame($expected, $actual) : void {}
+
+                    class a {}
+                    class b {}
+                    class c {}
+
+                    $expected = rand(0, 1) ? new a : new b;
+                    $actual = new c;
+                    assertSame($expected, $actual);',
+                'error_message' => 'TypeDoesNotContainType',
+            ],
             'assertNotSameDifferentTypesExplicitString' => [
                 'code' => '<?php
                     /**
