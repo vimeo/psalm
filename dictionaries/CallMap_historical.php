964f64a500b987c82bd5274d07d3b67e1d0b381b
Marco Pivetta <ocramius@gmail.com>
2022-08-05 12:49:45 +0200

    Removed `DateTimeImmutable::setISODate()` from the CallMap: fully covered by stub
    Also simplified the return type from `static|false` to `static`, since
    the method throws at all times, on failure.
    
    On PHP 7.x, it could only fail if an invalid type was passed in, which is
    not really valid anyway, from a type perspective.
    
    Ref (PHP 8.1.x): https://github.com/php/php-src/blob/32d55f74229e7913db0d59ef874a401744479b6a/ext/date/php_date.c#L3308-L3324
    Ref (PHP 7.0.33): https://github.com/php/php-src/blob/bf574c2b67a1f786e36cf679f41b758b973a82c4/ext/date/php_date.c#L3549-L3565

--- dictionaries/CallMap_historical.php
+++ dictionaries/CallMap_historical.php
@@ -1059,7 +1059,6 @@ return [
     'DateTimeImmutable::__wakeup' => ['void'],
     'DateTimeImmutable::createFromMutable' => ['static', 'datetime'=>'DateTime'],
     'DateTimeImmutable::getLastErrors' => ['array{warning_count:int,warnings:array<int,string>,error_count:int,errors:array<int,string>}'],
-    'DateTimeImmutable::setISODate' => ['static|false', 'year'=>'int', 'week'=>'int', 'day='=>'int'],
     'DateTimeImmutable::setTimestamp' => ['static|false', 'unixtimestamp'=>'int'],
     'DateTimeInterface::diff' => ['DateInterval', 'datetime2'=>'DateTimeInterface', 'absolute='=>'bool'],
     'DateTimeInterface::format' => ['string', 'format'=>'string'],
