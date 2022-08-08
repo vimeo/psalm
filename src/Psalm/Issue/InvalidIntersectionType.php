cd0035e0d16276c1040f3b77ecc5d505acd80115
Pete Walker <pete.walker@me.com>
2022-08-09 00:31:00 +0100

    chore: `bin/max_used_shortcode.php`

--- src/Psalm/Issue/InvalidIntersectionType.php
+++ src/Psalm/Issue/InvalidIntersectionType.php
@@ -5,5 +5,5 @@ namespace Psalm\Issue;
 final class InvalidIntersectionType extends CodeIssue
 {
     public const ERROR_LEVEL = 6;
-    public const SHORTCODE = 283;
+    public const SHORTCODE = 313;
 }
