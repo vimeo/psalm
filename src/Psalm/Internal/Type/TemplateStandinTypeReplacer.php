5765163a0e9436dd75d27951f473873a136f1e6d
Daniil Gentili <daniil@daniil.it>
2022-08-29 13:50:21 +0200

    Fix

--- src/Psalm/Internal/Type/TemplateStandinTypeReplacer.php
+++ src/Psalm/Internal/Type/TemplateStandinTypeReplacer.php
@@ -155,8 +155,18 @@ class TemplateStandinTypeReplacer
         }
 
         if ($replace) {
-            if (array_values($original_atomic_types) === $atomic_types) {
-                return $union_type;
+            if (count($original_atomic_types) === count($atomic_types)) {
+                $all_equal = true;
+                $k = 0;
+                foreach ($original_atomic_types as $t) {
+                    if (!$atomic_types[$k++]->equals($t, true)) {
+                        $all_equal = false;
+                        break;
+                    }
+                }
+                if ($all_equal) {
+                    return $union_type;
+                }
             }
 
             if (!$atomic_types) {
