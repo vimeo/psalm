d69e06242617d89a66bcc30843acdd30af1ce23c
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2022-09-20 10:59:46 +0200

    add RiskyCast tests

--- tests/ToStringTest.php
+++ tests/ToStringTest.php
@@ -517,6 +517,16 @@ class ToStringTest extends TestCase
                     echo (int) $foo;',
                 'error_message' => 'InvalidCast',
             ],
+            'riskyArrayToIntCast' => [
+                '<?php
+                    echo (int) array();',
+                'error_message' => 'RiskyCast',
+            ],
+            'riskyArrayToFloatCast' => [
+                '<?php
+                    echo (float) array(\'hello\');',
+                'error_message' => 'RiskyCast',
+            ],
         ];
     }
 }
