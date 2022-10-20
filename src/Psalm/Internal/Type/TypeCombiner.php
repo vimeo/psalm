00c8d5eb39a7a02047ff8b2bc6acca3daac5146c
Daniil Gentili <daniil@daniil.it>
2022-10-20 12:31:44 +0200

    Fix

--- src/Psalm/Internal/Type/TypeCombiner.php
+++ src/Psalm/Internal/Type/TypeCombiner.php
@@ -741,8 +741,9 @@ class TypeCombiner
                 $combination->array_min_counts[$min_prop_count] = true;
             }
 
-            foreach ($possibly_undefined_entries as $k => $possibly_undefined_type) {
-                $combination->objectlike_entries[$k] = $possibly_undefined_type->setPossiblyUndefined(true);
+            foreach ($possibly_undefined_entries as $k => $_) {
+                $combination->objectlike_entries[$k] = $combination->objectlike_entries[$k]
+                    ->setPossiblyUndefined(true);
             }
 
             if (!$type->is_list) {
