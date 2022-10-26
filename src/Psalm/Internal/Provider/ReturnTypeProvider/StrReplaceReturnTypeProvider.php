bde21cfd596d3cfdc907b8fcf58ac6ada2f5a4a9
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2022-10-26 11:57:03 +0200

    keep literal string for simple str_replace

--- src/Psalm/Internal/Provider/ReturnTypeProvider/StrReplaceReturnTypeProvider.php
+++ src/Psalm/Internal/Provider/ReturnTypeProvider/StrReplaceReturnTypeProvider.php
@@ -50,7 +50,21 @@ class StrReplaceReturnTypeProvider implements FunctionReturnTypeProviderInterfac
 
             $return_type = Type::getString();
 
-            if (in_array($function_id, ['preg_replace', 'preg_replace_callback'], true)) {
+            if (in_array($function_id, ['str_replace', 'str_ireplace'], true)
+                && $subject_type->isSingleLiteral()
+            ) {
+                $first_arg = $statements_source->node_data->getType($call_args[0]->value);
+                $second_arg = $statements_source->node_data->getType($call_args[1]->value);
+                if ($first_arg && $second_arg && $first_arg->isSingleLiteral() && $second_arg->isSingleLiteral()) {
+                    $replaced_string = call_user_func(
+                        $function_id,
+                        $first_arg->getSingleLiteral()->value,
+                        $second_arg->getSingleLiteral()->value,
+                        $subject_type->getSingleLiteral()->value
+                    );
+                    $return_type = Type::getString($replaced_string);
+                }
+            } elseif (in_array($function_id, ['preg_replace', 'preg_replace_callback'], true)) {
                 $return_type = new Union([new TString, new TNull()]);
 
                 $codebase = $statements_source->getCodebase();
