5ac60ddea0427991ffbd5c25c8d984aad2ffbbea
Daniil Gentili <daniil@daniil.it>
2022-11-24 19:31:53 +0100

    Add tests

--- tests/MagicPropertyTest.php
+++ tests/MagicPropertyTest.php
@@ -731,6 +731,41 @@ class MagicPropertyTest extends TestCase
                     $a->type = A::TYPE_B;
                 ',
             ],
+            'impureMethodTest' => [
+                'code' => '<?php
+                    /**
+                     * @property array<string, string> $errors
+                     *
+                     * @psalm-seal-properties
+                     */
+                    final class OrganizationObject {
+
+                        public function __get(string $key)
+                        {
+                            return [];
+                        }
+
+                        /**
+                         * @param mixed $a
+                         */
+                        public function __set(string $key, $a): void
+                        {
+                        }
+
+                        public function updateErrors(): void {
+                            /** @var array<string, string> */
+                            $errors = [];
+                            $this->errors = $errors;
+                        }
+                        /** @return array<string, string> */
+                        public function updateStatus(): array {
+                            $_ = $this->errors;
+                            $this->updateErrors();
+                            $errors = $this->errors;
+                            return $errors;
+                        }
+                    }'
+            ]
         ];
     }
 
