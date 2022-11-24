b7995a01c949c95cdfe98fc8d5f421785d1f91ec
Daniil Gentili <daniil@daniil.it>
2022-11-24 18:59:55 +0100

    Revert misguided change

--- src/Psalm/Internal/Analyzer/AlgebraAnalyzer.php
+++ src/Psalm/Internal/Analyzer/AlgebraAnalyzer.php
@@ -93,7 +93,7 @@ class AlgebraAnalyzer
                         break;
                     }
 
-                    if ($negated_clause_2->possibilities[$key] !== $keyed_possibilities) {
+                    if ($negated_clause_2->possibilities[$key] != $keyed_possibilities) {
                         $negated_clause_2_contains_1_possibilities = false;
                         break;
                     }
