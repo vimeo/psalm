a0e4468a9a1d0f2848d9d47bd7432086ffb1fc64
Matthew Brown <github@muglug.com>
2022-11-25 12:16:37 -0500

    Simplify assertions generated by an array_key_exists check (#8763)

--- tests/TypeReconciliation/ArrayKeyExistsTest.php
+++ tests/TypeReconciliation/ArrayKeyExistsTest.php
@@ -456,7 +456,38 @@ class ArrayKeyExistsTest extends TestCase
                             if ($cmp["administrative_area_level_1"] === "test") {
                                 $cmp["administrative_area_level_1"] = "";
                             }'
-            ]
+            ],
+            'arrayKeyExistsPoorPerformance' => [
+                'code' => '<?php
+                    class A {
+                        private const CRITICAL_ERRORS = [
+                            "category" => [],
+                            "name" => [],
+                            "geo" => [],
+                            "city" => [],
+                            "url" => [],
+                            "comment_critical" => [],
+                            "place" => [],
+                            "price" => [],
+                            "robot_error" => [],
+                            "manual" => [],
+                            "contacts" => [],
+                            "not_confirmed_by_other_source" => [],
+                        ];
+
+
+                        public function isCriticalError(int|string $key): bool {
+                            if (!\array_key_exists($key, A::CRITICAL_ERRORS)) {
+                                return false;
+                            }
+
+                            return true;
+                        }
+                    }',
+                'assertions' => [],
+                'ignored_issues' => [],
+                'php_version' => '8.0',
+            ],
         ];
     }
 
