2da4bf98d61d1677e28086fd5f37c045a1db827c
orklah <ronan_morin@hotmail.com>
2022-11-30 23:45:55 +0100

    add test

--- tests/ArrayFunctionCallTest.php
+++ tests/ArrayFunctionCallTest.php
@@ -2290,6 +2290,27 @@ class ArrayFunctionCallTest extends TestCase
                     }
                     ',
             ],
+            'mergeBetweenSealedArrayWithPossiblyUndefinedAndMixedArrayIsMixedArray' => [
+                'code' => '<?php
+
+                    function findit(Closure $x): void
+                    {
+                        $closure = new ReflectionFunction($x);
+
+                        $statics = [];
+
+                        if (rand(0, 1)) {
+                            $statics = ["this" => "a"];
+                        }
+                        $b = $statics + $closure->getStaticVariables();
+                        /** @psalm-check-type $b = array<array-key, mixed> */
+
+                        $_a = count($b);
+
+                        /** @psalm-check-type $_a = int<0, max> */
+                    }
+                    ',
+            ],
         ];
     }
 
