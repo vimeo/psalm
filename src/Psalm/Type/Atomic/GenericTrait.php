c05ce2d62225266c2e8ae782df934474ce81b185
Daniil Gentili <daniil@daniil.it>
2022-12-02 19:05:31 +0100

    Fixes

--- src/Psalm/Type/Atomic/GenericTrait.php
+++ src/Psalm/Type/Atomic/GenericTrait.php
@@ -229,8 +229,6 @@ trait GenericTrait
             );
         }
 
-        assert(count($type_params) === 2);
-
         return $type_params === $this->type_params ? null : $type_params;
     }
 
@@ -255,7 +253,6 @@ trait GenericTrait
 
             $type_params[$offset] = $type_param;
         }
-        assert(count($type_params) === 2);
 
         return $type_params === $this->type_params ? null : $type_params;
     }
