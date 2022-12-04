2490230f1fd728becffc95afb339190f3169ea37
orklah <ronan_morin@hotmail.com>
2022-12-04 13:37:29 +0100

    fix missing break handling in loop

--- tests/Loop/WhileTest.php
+++ tests/Loop/WhileTest.php
@@ -676,6 +676,21 @@ class WhileTest extends TestCase
                         }
                     }'
             ],
+            'breakInWhileTrueIsNotInfiniteLoop' => [
+                'code' => '<?php
+                    /** @return Generator<array-key, mixed> */
+                    function f()
+                    {
+                        if (rand(0,1)) {
+                            throw new Exception;
+                        }
+
+                        while (true) {
+                            yield 1;
+                            break;
+                        }
+                    }'
+            ],
         ];
     }
 
