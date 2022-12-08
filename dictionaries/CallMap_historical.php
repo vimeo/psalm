f6051bc41765ea70f308bd895f388e61dcbb27bd
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-12-08 15:49:56 +0100

    Precise count range

--- dictionaries/CallMap_historical.php
+++ dictionaries/CallMap_historical.php
@@ -10084,7 +10084,7 @@ return [
     'copy' => ['bool', 'from'=>'string', 'to'=>'string', 'context='=>'resource'],
     'cos' => ['float', 'num'=>'float'],
     'cosh' => ['float', 'num'=>'float'],
-    'count' => ['int', 'value'=>'Countable|array|SimpleXMLElement', 'mode='=>'int'],
+    'count' => ['int<0, max>', 'value'=>'Countable|array|SimpleXMLElement', 'mode='=>'int'],
     'count_chars' => ['array<int,int>|false', 'input'=>'string', 'mode='=>'0|1|2'],
     'count_chars\'1' => ['string|false', 'input'=>'string', 'mode='=>'3|4'],
     'crack_check' => ['bool', 'dictionary'=>'', 'password'=>'string'],
