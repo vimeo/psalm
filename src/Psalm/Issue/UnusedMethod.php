32eaf12d40c8d0e47540bfdac0f8fd12f72afbf1
Bruce Weirdan <weirdan@gmail.com>
2022-12-11 05:49:09 -0400

    Prevent duplicate (Possibly)UnusedMethod/(Possibly)UnusedProperty
    This also allows (Possibly)UnusedProperty to be suppressed with
    `referencedProperty` attribute in psalm.xml
    
    Fixes vimeo/psalm#8874

--- src/Psalm/Issue/UnusedMethod.php
+++ src/Psalm/Issue/UnusedMethod.php
@@ -2,8 +2,21 @@
 
 namespace Psalm\Issue;
 
+use Psalm\CodeLocation;
+
+use function strtolower;
+
 final class UnusedMethod extends MethodIssue
 {
     public const ERROR_LEVEL = -2;
     public const SHORTCODE = 76;
+
+    public function __construct(
+        string $message,
+        CodeLocation $code_location,
+        string $method_id
+    ) {
+        parent::__construct($message, $code_location, $method_id);
+        $this->dupe_key = strtolower($method_id);
+    }
 }
