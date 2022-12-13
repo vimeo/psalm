d3afac30851341e4662bc622571ba2b2e69d6901
Daniil Gentili <daniil@daniil.it>
2022-12-13 20:29:51 +0100

    Do not consider never when getting the max count

--- src/Psalm/Type/Atomic/TKeyedArray.php
+++ src/Psalm/Type/Atomic/TKeyedArray.php
@@ -423,7 +423,13 @@ class TKeyedArray extends Atomic
         if ($this->fallback_params) {
             return null;
         }
-        return count($this->properties);
+        $prop_max_count = 0;
+        foreach ($this->properties as $property) {
+            if (!$property->isNever()) {
+                $prop_max_count++;
+            }
+        }
+        return $prop_max_count;
     }
     /**
      * Whether all keys are always defined (ignores unsealedness).
