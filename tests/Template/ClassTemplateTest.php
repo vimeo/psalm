70d1a5a3c527ad8c1a007198f3dde1044446323b
Daniil Gentili <daniil@daniil.it>
2022-12-18 18:14:43 +0100

    Add test

--- tests/Template/ClassTemplateTest.php
+++ tests/Template/ClassTemplateTest.php
@@ -16,6 +16,49 @@ class ClassTemplateTest extends TestCase
     public function providerValidCodeParse(): iterable
     {
         return [
+            'templateIntersection' => [
+                'code' => '<?php
+                    interface EntityInterface
+                    {
+                        public function getId(): string;
+                    }
+
+                    /**
+                     * @phpstan-template T of EntityInterface
+                     */
+                    interface RepositoryInterface
+                    {
+                        /**
+                         * @return T|null
+                         */
+                    public function byId(string $id);
+                    }
+
+                    final class Foo implements EntityInterface
+                    {
+                        public function getId(): string
+                        {
+                            return "42";
+                        }
+                    }
+
+                    /**
+                     * @phpstan-implements RepositoryInterface<Foo>
+                     */
+                    final class FooRepository implements RepositoryInterface
+                    {
+                        /**
+                         * @var Foo[]
+                         */
+                        public array $elements = [];
+
+                        public function byId(string $id): ?Foo
+                        {
+                            return $this->elements[$id] ?? null;
+                        }
+                    }
+                '
+            ],
             'cachingIterator' => [
                 'code' => '<?php
 
