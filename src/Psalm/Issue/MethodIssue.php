3cb64b1419f1168d73317225e09583b0756afb68
Jack Worman <jworman@healthcall.com>
2022-12-20 07:49:32 -0600

    Preserve case-sensitive class name in method issues

--- src/Psalm/Issue/MethodIssue.php
+++ src/Psalm/Issue/MethodIssue.php
@@ -10,7 +10,10 @@ abstract class MethodIssue extends CodeIssue
 {
     /**
      * @var string
+     * @psalm-suppress PossiblyUnusedProperty Allows plugins to autoload the class.
      */
+    public string $fq_class_name;
+    /** @var lowercase-string */
     public $method_id;
 
     public function __construct(
@@ -19,6 +22,7 @@ abstract class MethodIssue extends CodeIssue
         string $method_id
     ) {
         parent::__construct($message, $code_location);
+        [$this->fq_class_name] = \explode('::', $method_id);
         $this->method_id = strtolower($method_id);
     }
 }
