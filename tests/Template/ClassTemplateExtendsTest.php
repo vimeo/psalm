02cd829b7ddcd14ec194638120ab237ad5986609
Daniil Gentili <daniil@daniil.it>
2022-12-20 13:28:47 +0100

    Fix bug

--- tests/Template/ClassTemplateExtendsTest.php
+++ tests/Template/ClassTemplateExtendsTest.php
@@ -16,6 +16,69 @@ class ClassTemplateExtendsTest extends TestCase
     public function providerValidCodeParse(): iterable
     {
         return [
+            'interface' => [
+                'code' => '<?php
+                    /**
+                     * Singleton interface
+                     *
+                     * @template T
+                     */
+                    interface ISingleton {
+
+                        /**
+                         * getInstance interface
+                         *
+                         * @return T
+                         */
+                        public static function getInstance();
+                    }
+
+                    /**
+                     * @psalm-consistent-constructor
+                     *
+                     * @implements ISingleton<Singleton&static>
+                     */
+                    abstract class Singleton implements ISingleton {
+
+                        /**
+                         * By default, disallow construction of child classes.
+                         */
+                        protected function __construct() {
+                        }
+
+                        /**
+                         * Instance array
+                         *
+                         * @var array<class-string<static>, static>
+                         */
+                        private static array $instances = [];
+
+                        /**
+                         * Clear all instances
+                         */
+                        public static function clear(): void {
+                            self::$instances = [];
+                        }
+
+                        /**
+                         * Get instance
+                         */
+                        public static function getInstance(): static {
+                            $class = static::class;
+                            return self::$instances[$class] ??= new static();
+                        }
+                    }
+
+                    class a extends Singleton {
+
+                    }
+
+                    $a = a::getInstance();
+                ',
+                'assertions' => [
+                    '$a===' => 'a'
+                ]
+            ],
             'phanTuple' => [
                 'code' => '<?php
                     namespace Phan\Library;
