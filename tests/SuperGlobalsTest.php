032f01114e0e854e2f519c34eb22ba1419e21b7f
fluffycondor <7ionmail@gmail.com>
2022-12-25 11:41:34 +0600

    Fix test

--- tests/SuperGlobalsTest.php
+++ tests/SuperGlobalsTest.php
@@ -14,6 +14,9 @@ class SuperGlobalsTest extends TestCase
             'code' => '<?php
                 /** @return non-empty-list<non-falsy-string> */
                 function returnsList(): array {
+                    if (!isset($http_response_header)) {
+                        throw new \RuntimeException();
+                    }
                     return $http_response_header;
                 }
             ',
