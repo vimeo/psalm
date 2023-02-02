fdbff4dcb20bf3e37cf621d6738841c68b54b6f1
Matt Brown <github@muglug.com>
2023-02-02 09:56:02 -0500

    Add failing test

--- tests/TypeReconciliation/TypeAlgebraTest.php
+++ tests/TypeReconciliation/TypeAlgebraTest.php
@@ -1211,6 +1211,23 @@ class TypeAlgebraTest extends TestCase
                 'ignored_issues' => [],
                 'php_version' => '8.0',
             ],
+            'subclassAfterNegation' => [
+                'code' => '<?php
+                    abstract class Base {}
+                    class A extends Base {}
+                    class AChild extends A {}
+                    class B extends Base {
+                        public string $s = "";
+                    }
+
+                    function foo(Base $base): void {
+                        if (!$base instanceof A || $base instanceof AChild) {
+                            if ($base instanceof B && rand(0, 1)) {
+                                echo $base->s;
+                            }
+                        }
+                    }'
+            ],
         ];
     }
 
