efb5aed87919e500d5595d8c8a2cb8def60eeb18
Daniil Gentili <daniil@daniil.it>
2023-04-20 13:00:28 +0200

    Avoid wrong assertions when working with objects returned by methods

--- tests/AssertAnnotationTest.php
+++ tests/AssertAnnotationTest.php
@@ -2007,6 +2007,30 @@ class AssertAnnotationTest extends TestCase
                     function requiresString(string $_str): void {}
                 ',
             ],
+            'assertionOnPropertyReturnedByMethod' => [
+                'code' => '<?php
+                    class a {
+                        public ?int $id = null;
+                        /**
+                         * @psalm-mutation-free
+                         *
+                         * @psalm-assert-if-true !null $this->id
+                         */
+                        public function isExists(): bool {
+                            return $this->id !== null;
+                        }
+                    }
+
+                    class b {
+                        public ?int $id = null;
+                        public function __construct(private a $a) {
+                            if ($this->getA()->isExists()) {
+                                /** @psalm-check-type-exact $this->id = ?int */
+                            }
+                        }
+                        public function getA(): a { return $this->a; }
+                    }',
+            ],
             'assertWithEmptyStringOnKeyedArray' => [
                 'code' => '<?php
                     class A
