b8672949ece2a5d1312cf8447a37cfc1aa8dc253
Daniil Gentili <daniil@daniil.it>
2023-04-21 12:33:08 +0200

    Fix #9692

--- tests/PropertyTypeTest.php
+++ tests/PropertyTypeTest.php
@@ -2687,6 +2687,27 @@ class PropertyTypeTest extends TestCase
                 'ignored_issues' => [],
                 'php_version' => '8.1',
             ],
+            'intersectionPropertyAccess' => [
+                'code' => '<?php
+
+                    /** @property int $test1 */
+                    class a {
+                        public function __get(string $name)
+                        {
+                            return 0;
+                        }
+                    }
+
+                    /** @var a&object{test2: "lmao"} */
+                    $r = null;
+
+                    $test1 = $r->test1;
+                    $test2 = $r->test2;',
+                'assertions' => [
+                    '$test1===' => 'int',
+                    '$test2===' => "'lmao'",
+                ],
+            ],
         ];
     }
 
