ac8d48941356c902eeadf15c6dc480e1c79d211c
klimick <klimichkartorgnusov@gmail.com>
2023-05-03 22:22:33 +0300

    Fix ParadoxicalCondition with complex match expression

--- src/Psalm/Internal/Analyzer/Statements/Expression/MatchAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/Expression/MatchAnalyzer.php
@@ -110,10 +110,11 @@ class MatchAnalyzer
                         $stmt->cond->getAttributes(),
                     );
                 }
-            } elseif ($stmt->cond instanceof PhpParser\Node\Expr\FuncCall
-                || $stmt->cond instanceof PhpParser\Node\Expr\MethodCall
-                || $stmt->cond instanceof PhpParser\Node\Expr\StaticCall
+            } elseif ($stmt->cond instanceof PhpParser\Node\Expr\ConstFetch
+                && $stmt->cond->name->toString() === 'true'
             ) {
+                // do nothing
+            } else {
                 $switch_var_id = '$__tmp_switch__' . (int) $stmt->cond->getAttribute('startFilePos');
 
                 $condition_type = $statements_analyzer->node_data->getType($stmt->cond) ?? Type::getMixed();
--- tests/MatchTest.php
+++ tests/MatchTest.php
@@ -95,6 +95,20 @@ class MatchTest extends TestCase
                 'ignored_issues' => [],
                 'php_version' => '8.1',
             ],
+            'multipleIdenticalChecksInOneArm' => [
+                'code' => '<?php
+                    function foo(?string $t1, string $t2): string
+                    {
+                        return match ($t1 ?? $t2) {
+                            "type1", "type2", "type3" => "1 or 2 or 3",
+                            "type4", "type5", "type6" => "4 or 5 or 6",
+                            default => "rest",
+                        };
+                    }',
+                'assertions' => [],
+                'ignored_issues' => [],
+                'php_version' => '8.1',
+            ],
             'multipleInstanceOfConditionsInOneArm' => [
                 'code' => '<?php
                     interface Foo {}
