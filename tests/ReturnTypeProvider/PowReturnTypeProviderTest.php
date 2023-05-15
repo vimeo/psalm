100907b61fe2da1f82650d4aed715134096ee6e4
RobChett <robchett@gmail.com>
2023-05-15 01:17:27 +0100

    Improve return types of pow()

---
+++ tests/ReturnTypeProvider/PowReturnTypeProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Psalm\Tests\ReturnTypeProvider;
+
+use Psalm\Tests\TestCase;
+use Psalm\Tests\Traits\ValidCodeAnalysisTestTrait;
+
+class PowReturnTypeProviderTest extends TestCase
+{
+    use ValidCodeAnalysisTestTrait;
+
+    public function providerValidCodeParse(): iterable
+    {
+        yield 'test' => [
+            'code' => '<?php
+                function getInt(): int {
+                    return 1;
+                }
+                function getFloat(): float {
+                    return 1.0;
+                }
+                $int = getInt();
+                $float = getFloat();
+
+                $a = pow($int, $int);
+                $b = pow($int, $float);
+                $c = pow($float, $int);
+                $d = pow(1000, 1000);
+                $e = pow(0, 1000);
+                $f = pow(1000, 0);
+            ',
+            'assertions' => [
+                '$a===' => 'int',
+                '$b===' => 'float',
+                '$c===' => 'float',
+                '$d===' => 'float(INF)',
+                '$e===' => '0',
+                '$f===' => '1',
+            ],
+        ];
+    }
+}
