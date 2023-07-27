ee505259aab463c39b8b802c6db27a4f6c04c94f
Thomas Bley <mails@thomasbley.de>
2023-07-27 15:59:31 +0200

    #9974 extended test with PDO::ATTR_DEFAULT_FETCH_MODE for future implementation

--- tests/MethodCallTest.php
+++ tests/MethodCallTest.php
@@ -507,10 +507,13 @@ class MethodCallTest extends TestCase
 
                     $db = new PDO("sqlite::memory:");
                     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
                     $stmt = $db->prepare("select \"a\" as a");
                     $stmt->setFetchMode(PDO::FETCH_CLASS, A::class);
                     $stmt2 = $db->prepare("select \"a\" as a");
                     $stmt2->setFetchMode(PDO::FETCH_ASSOC);
+                    $stmt3 = $db->prepare("select \"a\" as a");
+                    $stmt3->setFetchMode(PDO::ATTR_DEFAULT_FETCH_MODE);
                     $stmt->execute();
                     $stmt2->execute();
                     /** @psalm-suppress MixedAssignment */
@@ -525,7 +528,9 @@ class MethodCallTest extends TestCase
                     $h = $stmt2->fetch();
                     $i = $stmt2->fetchAll();
                     $j = $stmt2->fetch(PDO::FETCH_BOTH);
-                    $k = $stmt2->fetchAll(PDO::FETCH_BOTH);',
+                    $k = $stmt2->fetchAll(PDO::FETCH_BOTH);
+                    /** @psalm-suppress MixedAssignment */
+                    $l = $stmt3->fetch();',
                 'assertions' => [
                     '$a' => 'mixed',
                     '$b' => 'array<array-key, mixed>|false',
@@ -538,6 +543,7 @@ class MethodCallTest extends TestCase
                     '$i' => 'array<array-key, mixed>|false',
                     '$j' => 'array<array-key, null|scalar>|false',
                     '$k' => 'list<array<array-key, null|scalar>>',
+                    '$l' => 'mixed',
                 ],
             ],
             'datePeriodConstructor' => [
