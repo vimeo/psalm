9b58ae24c52402dade82b87cf2173090eef84833
Mark McEver <mark@fastmail.net>
2023-08-18 15:44:26 -0500

    Fix test plugin name mismatch

--- tests/fixtures/PluginHookExitStatus/ExitStatusPlugin.php
+++ tests/fixtures/PluginHookExitStatus/ExitStatusPlugin.php
@@ -3,7 +3,7 @@
 use Psalm\Plugin\EventHandler\AfterFileAnalysisInterface;
 use Psalm\Plugin\EventHandler\Event\AfterFileAnalysisEvent;
 
-class ExitPlugin implements AfterFileAnalysisInterface
+class ExitStatusPlugin implements AfterFileAnalysisInterface
 {
     public static function afterAnalyzeFile(AfterFileAnalysisEvent $event): void {
         // The following causes a zero exit status, which we explicitly want to ensure does NOT result in a zero exit status from Psalm itself.
