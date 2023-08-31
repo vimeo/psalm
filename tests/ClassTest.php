d0c4d170b0f0dc7ed92402ba5b652a1560e944ef
robchett <robchett@gmail.com>
2023-08-31 19:12:58 +0100

    Apply psalm-inheritors to interfaces too

--- tests/ClassTest.php
+++ tests/ClassTest.php
@@ -857,7 +857,6 @@ class ClassTest extends TestCase
                      */
                     class BaseClass {}
                     class FooClass extends BaseClass {}
-                    $a = new FooClass();
                     PHP,
             ],
             'unionInheritorIsAllowed' => [
@@ -868,9 +867,7 @@ class ClassTest extends TestCase
                      */
                     class BaseClass {}
                     class FooClass extends BaseClass {}
-                    $a = new FooClass();
                     class BarClass extends FooClass {}
-                    $b = new BarClass();
                     PHP,
             ],
             'multiInheritorIsAllowed' => [
@@ -881,9 +878,7 @@ class ClassTest extends TestCase
                      */
                     class BaseClass {}
                     class FooClass extends BaseClass {}
-                    $a = new FooClass();
                     class BarClass extends FooClass {}
-                    $b = new BarClass();
                     PHP,
             ],
             'skippedInheritorIsAllowed' => [
@@ -894,9 +889,7 @@ class ClassTest extends TestCase
                      */
                     class BaseClass {}
                     class FooClass extends BaseClass {}
-                    $a = new FooClass();
                     class BarClass extends FooClass {}
-                    $b = new BarClass();
                     PHP,
             ],
             'CompositeInheritorIsAllowed' => [
@@ -908,7 +901,6 @@ class ClassTest extends TestCase
                     class BaseClass {}
                     interface FooInterface {}
                     class BarClass extends BaseClass implements FooInterface {}
-                    $b = new BarClass();
                     PHP,
             ],
             'InterfaceInheritorIsAllowed' => [
@@ -919,12 +911,10 @@ class ClassTest extends TestCase
                      */
                     interface BaseInterface {}
                     class FooClass implements BaseInterface {}
-                    $a = new FooClass();
                     class BarClass implements BaseInterface {}
-                    $b = new BarClass();
                     PHP,
-                ],
-                'MultiInterfaceInheritorIsAllowed' => [
+            ],
+            'MultiInterfaceInheritorIsAllowed' => [
                     'code' => <<<'PHP'
                     <?php
                     /**
@@ -936,7 +926,16 @@ class ClassTest extends TestCase
                      */
                     interface InterfaceB {}
                     class FooClass implements InterfaceA, InterfaceB {}
-                    $a = new FooClass();
+                    PHP,
+                ],
+            'InterfaceOfInterfaceInheritorIsAllowed' => [
+                        'code' => <<<'PHP'
+                    <?php
+                    /**
+                     * @psalm-inheritors InterfaceB
+                     */
+                    interface InterfaceA {}
+                    interface InterfaceB extends InterfaceA {}
                     PHP,
             ],
         ];
@@ -1405,6 +1404,18 @@ class ClassTest extends TestCase
                      */
                     interface BaseInterface {}
                     class BazClass implements BaseInterface {}
+                    PHP,
+                'error_message' => 'InheritorViolation',
+                'ignored_issues' => [],
+            ],
+            'interfaceCannotImplementIfNotInInheritors' => [
+                'code' => <<<'PHP'
+                    <?php
+                    /**
+                     * @psalm-inheritors FooClass|BarClass
+                     */
+                    interface BaseInterface {}
+                    interface BazInterface extends BaseInterface {}
                     PHP,
                 'error_message' => 'InheritorViolation',
                 'ignored_issues' => [],
