3b775f46171ce6ef8b1b4f124b71b5aaa84cd66a
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2023-09-11 13:34:40 +0200

    Allow class-string<T> where T is a union

--- tests/ClassLikeStringTest.php
+++ tests/ClassLikeStringTest.php
@@ -879,6 +879,27 @@ class ClassLikeStringTest extends TestCase
                     $foo->baz = TypeTwo::class;
                     $foo->baz = TypeTwo::class;',
             ],
+            'classStringOfUnionTypeParameter' => [
+                'code' => '<?php
+
+                    class A {}
+                    class B {}
+
+                    /**
+                     * @template T as A|B
+                     *
+                     * @param class-string<T> $class
+                     * @return class-string<T>
+                     */
+                    function test(string $class): string {
+                        return $class;
+                    }
+
+                    $r = test(A::class);',
+                'assertions' => [
+                    '$r' => 'class-string<A>',
+                ],
+            ],
         ];
     }
 
