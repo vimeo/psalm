a3df6505f0bd58b812c4c40109361cb0bd4e22d4
klimick <klimichkartorgnusov@gmail.com>
2023-10-09 16:49:38 +0300

    Type check nested templates

--- tests/CallableTest.php
+++ tests/CallableTest.php
@@ -1844,6 +1844,40 @@ class CallableTest extends TestCase
                 'ignored_issues' => [],
                 'php_version' => '8.0',
             ],
+            'inferTypeWithNestedTemplatesAndExplicitTypeHint' => [
+                'code' => '<?php
+                    /**
+                     * @template TResult
+                     */
+                    interface Message {}
+
+                    /**
+                     * @implements Message<list<int>>
+                     */
+                    final class GetListOfNumbers implements Message {}
+
+                    /**
+                     * @template TResult
+                     * @template TMessage of Message<TResult>
+                     */
+                    final class Envelope {}
+
+                    /**
+                     * @template TResult
+                     * @template TMessage of Message<TResult>
+                     * @param class-string<TMessage> $_message
+                     * @param callable(TMessage, Envelope<TResult, TMessage>): TResult $_handler
+                     */
+                    function addHandler(string $_message, callable $_handler): void {}
+
+                    addHandler(GetListOfNumbers::class, function (Message $_message, Envelope $_envelope) {
+                        /**
+                         * @psalm-check-type-exact $_message = GetListOfNumbers
+                         * @psalm-check-type-exact $_envelope = Envelope<list<int>, GetListOfNumbers>
+                         */
+                        return [1, 2, 3];
+                    });',
+            ],
         ];
     }
 
