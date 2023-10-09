f6d4a980637aacaf6c6dc6b092522387a8a5cd92
robchett <robchett@gmail.com>
2023-10-09 22:05:38 +0100

    Fix falsable issue with phpversion

--- src/Psalm/ErrorBaseline.php
+++ src/Psalm/ErrorBaseline.php
@@ -244,7 +244,7 @@ final class ErrorBaseline
             $filesNode->setAttribute('php-version', implode(';' . "\n\t", [...[
                 ('php:' . PHP_VERSION),
             ], ...array_map(
-                static fn(string $extension): string => $extension . ':' . phpversion($extension),
+                static fn(string $extension): string => $extension . ':' . (string) phpversion($extension),
                 $extensions,
             )]));
         }
