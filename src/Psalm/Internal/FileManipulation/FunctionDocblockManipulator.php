fa97e6ddf53683182aea20971f6e745838289f5b
Daniil Gentili <daniil@daniil.it>
2023-10-22 21:26:19 +0200

    Fix

--- src/Psalm/Internal/FileManipulation/FunctionDocblockManipulator.php
+++ src/Psalm/Internal/FileManipulation/FunctionDocblockManipulator.php
@@ -45,7 +45,6 @@ final class FunctionDocblockManipulator
     private static array $manipulators = [];
 
     private readonly int $docblock_start;
-    private Closure|Function_|ClassMethod|ArrowFunction $stmt;
 
     private readonly int $docblock_end;
 
