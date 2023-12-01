edf2d1e4b6798d7bc4a088f63b902deae8c533bc
Daniil Gentili <daniil@daniil.it>
2023-12-01 15:03:17 +0100

    Fix

--- tests/TypeReconciliation/ArrayKeyExistsTest.php
+++ tests/TypeReconciliation/ArrayKeyExistsTest.php
@@ -56,6 +56,19 @@ class ArrayKeyExistsTest extends TestCase
                     }
                 ',
             ],
+            'arrayKeyExistsNoSideEffects' => [
+                'code' => '<?php
+                    function getMethodName(array $ddata = []): void {
+                        if (\array_key_exists("redirect", $ddata)) {
+                            return;
+                        }
+                        if (random_int(0, 1)) {
+                            $ddata["type"] = "test";
+                        }
+                        /** @psalm-check-type-exact $ddata = array<array-key, mixed> */
+                    }
+                '
+            ]
             'arrayKeyExistsTwice' => [
                 'code' => '<?php
                     function two(array $a): void {
