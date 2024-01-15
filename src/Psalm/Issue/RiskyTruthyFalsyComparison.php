f34ec2c81edf5824cda15dab7f0032a97152656d
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2024-01-15 10:09:49 +0100

    change error level to 2

--- src/Psalm/Issue/RiskyTruthyFalsyComparison.php
+++ src/Psalm/Issue/RiskyTruthyFalsyComparison.php
@@ -6,7 +6,7 @@ use Psalm\CodeLocation;
 
 final class RiskyTruthyFalsyComparison extends CodeIssue
 {
-    public const ERROR_LEVEL = 4;
+    public const ERROR_LEVEL = 2;
     public const SHORTCODE = 356;
 
     public function __construct(string $message, CodeLocation $code_location, ?string $dupe_key)
