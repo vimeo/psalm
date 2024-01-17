0504394e8b72733aa1e399bf0dba1547bbbccb8b
Daniil Gentili <daniil@daniil.it>
2024-01-17 12:52:36 +0100

    Fixup

--- src/Psalm/Internal/PhpTraverser/CustomTraverser.php
+++ src/Psalm/Internal/PhpTraverser/CustomTraverser.php
@@ -29,7 +29,7 @@ final class CustomTraverser extends NodeTraverser
      * @param Node $node node to traverse
      * @return Node Result of traversal (may be original node or new one)
      */
-    protected function traverseNode(Node $node): Node
+    protected function traverseNode(Node $node): void
     {
         foreach ($node->getSubNodeNames() as $name) {
             $subNode = &$node->$name;
@@ -60,7 +60,7 @@ final class CustomTraverser extends NodeTraverser
                 }
 
                 if ($traverseChildren) {
-                    $subNode = $this->traverseNode($subNode);
+                    $this->traverseNode($subNode);
                     if ($this->stopTraversal) {
                         break;
                     }
@@ -88,8 +88,6 @@ final class CustomTraverser extends NodeTraverser
                 }
             }
         }
-
-        return $node;
     }
 
     /**
