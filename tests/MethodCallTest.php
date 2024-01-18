f7edaa66acbb0f6ac2861ad95251b0019e603746
Daniil Gentili <daniil@daniil.it>
2024-01-18 20:33:54 +0100

    Fix #10552

--- tests/MethodCallTest.php
+++ tests/MethodCallTest.php
@@ -1230,6 +1230,27 @@ class MethodCallTest extends TestCase
                     $x = new Foo();
                     $x->bar($x);',
             ],
+            'conditional' => [
+                'code' => '<?php
+
+                    class Old {
+                        public function x(): void {}
+                    }
+                    class Child1 extends Old {}
+                    class Child2 extends Old {}
+                    
+                    /**
+                     * @template IsClient of bool
+                     */
+                    class A {
+                        /**
+                         * @psalm-param (IsClient is true ? Child1 : Child2) $var
+                         */
+                        public function test(Old $var): void {
+                            $var->x();
+                        }
+                    }',
+            ],
         ];
     }
 
