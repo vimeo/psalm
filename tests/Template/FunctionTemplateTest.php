c307bcbc7669c930c8f6dd8afd74537ba272385f
klimick <klimichkartorgnusov@gmail.com>
2024-01-22 07:09:57 +0300

    Try to fix template replacement edge case

--- tests/Template/FunctionTemplateTest.php
+++ tests/Template/FunctionTemplateTest.php
@@ -14,6 +14,68 @@ class FunctionTemplateTest extends TestCase
     public function providerValidCodeParse(): iterable
     {
         return [
+            'extractTypeParameterValue' => [
+                'code' => '<?php
+                    /**
+                     * @template T
+                     */
+                    interface Type {}
+
+                    /**
+                     * @implements Type<int>
+                     */
+                    final readonly class IntType implements Type {}
+
+                    /**
+                     * @template T
+                     * @implements Type<list<T>>
+                     */
+                    final readonly class ListType implements Type
+                    {
+                        /**
+                         * @param Type<T> $type
+                         */
+                        public function __construct(
+                            public Type $type,
+                        ) {
+                        }
+                    }
+
+                    /**
+                     * @template T
+                     * @param Type<T> $type
+                     * @return T
+                     */
+                    function extractType(Type $type): mixed
+                    {
+                        throw new \RuntimeException("Should never be called at runtime");
+                    }
+
+                    /**
+                     * @template T
+                     * @param Type<T> $t
+                     * @return ListType<T>
+                     */
+                    function listType(Type $t): ListType
+                    {
+                        return new ListType($t);
+                    }
+
+                    function intType(): IntType
+                    {
+                        return new IntType();
+                    }
+
+                    $listType = listType(intType());
+                    $list = extractType($listType);
+                ',
+                'assertions' => [
+                    '$listType===' => 'ListType<int>',
+                    '$list' => 'list<int>',
+                ],
+                'ignored_issues' => [],
+                'php_version' => '8.2',
+            ],
             'validTemplatedType' => [
                 'code' => '<?php
                     namespace FooFoo;
