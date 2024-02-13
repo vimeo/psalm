f5fb9498de11c06e639d31d6fa7006f01dcdbac4
Markus Staab <markus.staab@redaxo.de>
2024-02-13 09:11:11 +0100

    Use IssueBuffer::maybeAdd() instead of throwing

--- src/Psalm/Internal/PhpVisitor/Reflector/FunctionLikeDocblockParser.php
+++ src/Psalm/Internal/PhpVisitor/Reflector/FunctionLikeDocblockParser.php
@@ -239,7 +239,12 @@ final class FunctionLikeDocblockParser
                 if (count($param_parts) >= 2) {
                     $info->taint_sink_params[] = ['name' => $param_parts[1], 'taint' => $param_parts[0]];
                 } else {
-                    throw new IncorrectDocblockException('@psalm-taint-sink expects 2 arguments');
+                    IssueBuffer::maybeAdd(
+                        new InvalidDocblock(
+                            '@psalm-taint-sink expects 2 arguments',
+                            $code_location,
+                        ),
+                    );
                 }
             }
         }
@@ -282,7 +287,12 @@ final class FunctionLikeDocblockParser
                 if ($param_parts[0]) {
                     $info->taint_source_types[] = $param_parts[0];
                 } else {
-                    throw new IncorrectDocblockException('@psalm-taint-source expects 1 argument');
+                    IssueBuffer::maybeAdd(
+                        new InvalidDocblock(
+                            '@psalm-taint-source expects 1 argument',
+                            $code_location,
+                        ),
+                    );
                 }
             }
         } elseif (isset($parsed_docblock->tags['return-taint'])) {
