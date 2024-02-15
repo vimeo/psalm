03fcf68d9e570876255c7b32b3b179f8bfaa3740
Bruce Weirdan <weirdan@gmail.com>
2024-02-15 23:23:40 +0100

    Allow multiple spaces between type name and type definition

--- tests/TypeAnnotationTest.php
+++ tests/TypeAnnotationTest.php
@@ -916,6 +916,22 @@ class TypeAnnotationTest extends TestCase
                     }
                 PHP,
             ],
+            'typeWithMultipleSpaces' => [
+                'code' => <<<'PHP'
+                    <?php
+                    /**
+                     * @psalm-type Foo      =     string
+                     * @psalm-type Bar           int
+                     */
+                    class A {
+                        /**
+                         * @psalm-param Foo $foo
+                         * @psalm-param Bar $bar
+                         */
+                        public function bar(string $foo, int $bar): void {}
+                    }
+                PHP,
+            ],
         ];
     }
 
