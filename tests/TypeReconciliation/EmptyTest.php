4b707d1233610f5b81a87cef575c3ce8d4e80aac
Evan Shaw <evan.shaw@lightspeedhq.com>
2024-03-04 21:39:18 +1300

    Additional array fetch test case

--- tests/TypeReconciliation/EmptyTest.php
+++ tests/TypeReconciliation/EmptyTest.php
@@ -771,6 +771,13 @@ class EmptyTest extends TestCase
                     }',
                 'error_message' => 'RedundantConditionGivenDocblockType',
             ],
+            'redundantEmptyArrayFetch' => [
+                'code' => '<?php
+                    /** @var array<true> $a */;
+                    assert(isset($a["a"]));
+                    if (empty($a["a"])) {}',
+                'error_message' => 'DocblockTypeContradiction',
+            ],
         ];
     }
 }
