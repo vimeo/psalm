d8bea796439a25376867fd1dccf1f065b425e5eb
Ivan Sidorov <nobxod@gmail.com>
2024-03-26 08:37:21 +0000

    Part 1. Fail tests of named mixins with object methods
    Problems:
    ```
    1) Psalm\Tests\MixinsDeepTest::testValidCode with data set "NamedMixinsWithoutT_WithObjectMethods"
    Psalm\Exception\CodeException: MixedAssignment - src/somefile.php:22:1 - Unable to determine the type that $a is being assigned to
    
    2) Psalm\Tests\MixinsDeepTest::testValidCode with data set "NamedMixinsWithT_WithObjectMethods"
    Psalm\Exception\CodeException: MixedAssignment - src/somefile.php:37:1 - Unable to determine the type that $a is being assigned to
    ```

---
+++ tests/MixinsDeepTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Psalm\Tests;
+
+use Psalm\Tests\Traits\ValidCodeAnalysisTestTrait;
+
+class MixinsDeepTest extends TestCase
+{
+    use ValidCodeAnalysisTestTrait;
+
+    public function providerValidCodeParse(): iterable
+    {
+        return [
+            'NamedMixinsWithoutT_WithObjectMethods' => [
+                'code' => <<<'PHP'
+                    <?php
+                    abstract class Foo {
+                        abstract public function getString(): string;
+                    }
+
+                    /**
+                     * @mixin Foo
+                     */
+                    abstract class Bar {
+                        abstract public function getInt(): int;
+                        public function __call(string $name, array $arguments) {}
+                    }
+
+                    /**
+                     * @mixin Bar
+                     */
+                    class Baz {
+                        public function __call(string $name, array $arguments) {}
+                    }
+
+                    $baz = new Baz();
+                    $a = $baz->getString();
+                    $b = $baz->getInt();
+                    PHP,
+                'assertions' => [
+                    '$a' => 'string',
+                    '$b' => 'int',
+                ],
+            ],
+            'NamedMixinsWithT_WithObjectMethods' => [
+                'code' => <<<'PHP'
+                    <?php
+                    /**
+                     * @template T
+                     */
+                    abstract class Foo {
+                        /**
+                         * @return T
+                         */
+                        abstract public function getString();
+                    }
+
+                    /**
+                     * @template T1
+                     * @template T2
+                     * @mixin Foo<T1>
+                     */
+                    abstract class Bar {
+                        /**
+                         * @return T2
+                         */
+                        abstract public function getInt();
+
+                        public function __call(string $name, array $arguments) {}
+                    }
+
+                    /**
+                     * @template T1
+                     * @template T2
+                     * @mixin Bar<T1, T2>
+                     */
+                    class Baz {
+                        public function __call(string $name, array $arguments) {}
+                    }
+
+                    /** @var Baz<string, int> */
+                    $baz = new Baz();
+                    $a = $baz->getString();
+                    $b = $baz->getInt();
+                    PHP,
+                'assertions' => [
+                    '$a' => 'string',
+                    '$b' => 'int',
+                ],
+            ],
+        ];
+    }
+}
