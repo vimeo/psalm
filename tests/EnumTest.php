cd2f769e56563747feddfb9e312b320e7db49119
M.Cozzolino <michaelcozzolino@outlook.it>
2024-04-13 16:34:00 +0200

    Fix tests

--- tests/EnumTest.php
+++ tests/EnumTest.php
@@ -436,10 +436,6 @@ class EnumTest extends TestCase
                     interface ExtendedUnitEnum extends \UnitEnum {}
                     static fn (ExtendedUnitEnum $tag): string => $tag->name;
 
-                    /**
-                     * @template T of int|string
-                     * @extends BackedEnum<T>
-                     */
                     interface ExtendedBackedEnum extends \BackedEnum {}
                     static fn (ExtendedBackedEnum $tag): string|int => $tag->value;
                     ',
@@ -517,10 +513,6 @@ class EnumTest extends TestCase
             ],
             'methodInheritanceByInterfaces' => [
                 'code' => '<?php
-                    /**
-                     * @template T of int|string
-                     * @extends BackedEnum<T>
-                     */
                     interface I extends BackedEnum {}
                     /** @var I $i */
                     $a = $i::cases();
@@ -644,10 +636,6 @@ class EnumTest extends TestCase
                     interface I {}
 
                     interface UE extends UnitEnum {}
-                    /**
-                     * @template T of int|string
-                     * @extends BackedEnum<T>
-                     */
                     interface BE extends BackedEnum {}
 
                     function f(I $i): void {
