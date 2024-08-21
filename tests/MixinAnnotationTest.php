bdd364140ee77ef2934f54eff32735d797debbcb
Ivan Sidorov <nobxod@gmail.com>
2024-08-21 20:38:59 +0000

    Creating Fail and Fail-Like Tests
    Failed only "undefinedMixinClassWithPropertyAssignment_WithMagicMethod".
    
    Test with data "undefinedMixinClassWithPropertyAssignment_WithMagicMethod_Similar" is passed.
    
    Problem:
    
    1) Psalm\Tests\MixinAnnotationTest::testInvalidCodeWithIssues with data set "undefinedMixinClassWithPropertyAssignment_WithMagicMethod"
    Failed asserting that issue with "UndefinedMagicPropertyAssignment" was emitted.
    Other issues reported:
      - UndefinedDocblockClass - src/somefile.php:6:27 - Docblock-defined class, interface or enum named B does not exist
    Failed asserting that false is true.
    
    2) Psalm\Tests\MixinAnnotationTest::testInvalidCode with data set "undefinedMixinClassWithPropertyAssignment_WithMagicMethod"
    Failed asserting that exception message 'UndefinedDocblockClass - src/somefile.php:6:27 - Docblock-defined class, interface or enum named B does not exist' matches '/\bUndefinedMagicPropertyAssignment\b/'.

--- tests/MixinAnnotationTest.php
+++ tests/MixinAnnotationTest.php
@@ -639,6 +639,34 @@ class MixinAnnotationTest extends TestCase
                     (new A)->foo = "bar";',
                 'error_message' => 'UndefinedPropertyAssignment',
             ],
+            'undefinedMixinClassWithPropertyAssignment_WithMagicMethod' => [
+                'code' => '<?php
+                    /**
+                     * @property string $baz
+                     * @mixin B
+                     */
+                    class A {
+                        public function __set(string $name, string $value) {}
+                    }
+
+                    (new A)->foo = "bar";',
+                'error_message' => 'UndefinedMagicPropertyAssignment',
+            ],
+            'undefinedMixinClassWithPropertyAssignment_WithMagicMethod_Similar' => [
+                'code' => '<?php
+                    /**
+                     * @property string $baz
+                     * @mixin B
+                     */
+                    class A {
+                        public function __set(string $name, string $value) {}
+                    }
+
+                    /* this is difference */
+                    $a = new A();
+                    $a->foo = "bar";',
+                'error_message' => 'UndefinedMagicPropertyAssignment',
+            ],
             'undefinedMixinClassWithMethodCall' => [
                 'code' => '<?php
                     /** @mixin B */
