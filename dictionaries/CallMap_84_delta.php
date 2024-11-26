192b17ff342ee283188a6baf0cb9b63bda3ade6f
Daniil Gentili <daniil@daniil.it>
2024-11-26 17:37:59 +0000

    Add

--- dictionaries/CallMap_84_delta.php
+++ dictionaries/CallMap_84_delta.php
@@ -1,31 +1,40 @@
 <?php // phpcs:ignoreFile
+namespace Phan\Language\Internal;
 
-/**
- * This contains the information needed to convert the function signatures for php 8.3 to php 8.2 (and vice versa)
- *
- * This file has three sections.
- * The 'added' section contains function/method names from FunctionSignatureMap (And alternates, if applicable) that do not exist in php 8.2
- * The 'removed' section contains the signatures that were removed in php 8.3
- * The 'changed' section contains functions for which the signature has changed for php 8.3.
- *     Each function in the 'changed' section has an 'old' and a 'new' section,
- *     representing the function as it was in PHP 8.2 and in PHP 8.3, respectively
- *
- * @see CallMap.php
- * @see https://php.watch/versions/8.3
- *
- * @phan-file-suppress PhanPluginMixedKeyNoKey (read by Phan when analyzing this file)
- */
-return [
-  'added' => [],
-  'removed' => [],
-  'changed' => [
-    'locale_set_default' => [
-      'old' => ['bool', 'locale'=>'string'],
-      'new' => ['true', 'locale'=>'string'],
-    ],
-    'DateInterval::createFromDateString' => [
-      'old' => ['DateInterval|false', 'datetime'=>'string'],
-      'new' => ['DateInterval', 'datetime'=>'string'],
-    ]
-  ]
-];
+return array (
+  'added' => 
+  array (
+  ),
+  'removed' => 
+  array (
+  ),
+  'changed' => 
+  array (
+    'locale_set_default' => 
+    array (
+      'old' => 
+      array (
+        0 => 'bool',
+        'locale' => 'string',
+      ),
+      'new' => 
+      array (
+        0 => 'true',
+        'locale' => 'string',
+      ),
+    ),
+    'DateInterval::createFromDateString' => 
+    array (
+      'old' => 
+      array (
+        0 => 'DateInterval|false',
+        'datetime' => 'string',
+      ),
+      'new' => 
+      array (
+        0 => 'DateInterval',
+        'datetime' => 'string',
+      ),
+    ),
+  ),
+);
