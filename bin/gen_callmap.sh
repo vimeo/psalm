3a59eea853af39c132b2a39201d18a4504aa44a8
Daniil Gentili <daniil@daniil.it>
2025-01-17 19:55:54 +0100

    Add remaining versions

--- bin/gen_callmap.sh
+++ bin/gen_callmap.sh
@@ -3,7 +3,7 @@
 VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4"
 
 for f in $VERSIONS; do
-    docker build --build-arg VERSION=$f . -f bin/Dockerfile -t psalm_test_$f &
+    docker build --build-arg VERSION=$f . -f bin/Dockerfile_$f -t psalm_test_$f &
 done
 
 wait
