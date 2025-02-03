12a58a944c43e0b15c59c777234f8dfac4aeedb2
Daniil Gentili <daniil@daniil.it>
2025-02-03 13:57:55 +0100

    Fix

--- src/Psalm/Internal/Analyzer/Statements/Expression/YieldFromAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/Expression/YieldFromAnalyzer.php
@@ -10,6 +10,11 @@ use Psalm\Internal\Analyzer\Statements\Block\ForeachAnalyzer;
 use Psalm\Internal\Analyzer\Statements\ExpressionAnalyzer;
 use Psalm\Internal\Analyzer\StatementsAnalyzer;
 use Psalm\Type;
+use Psalm\Type\Atomic\TArray;
+use Psalm\Type\Atomic\TGenericObject;
+use Psalm\Type\Atomic\TKeyedArray;
+
+use function strtolower;
 
 /**
  * @internal
@@ -52,7 +57,27 @@ final class YieldFromAnalyzer
                 return false;
             }
 
-            $statements_analyzer->node_data->setType($stmt, Type::getNull());
+            $yield_from_type = null;
+
+            foreach ($stmt_expr_type->getAtomicTypes() as $atomic_type) {
+                if ($yield_from_type === null) {
+                    if ($atomic_type instanceof TGenericObject
+                        && strtolower($atomic_type->value) === 'generator'
+                        && isset($atomic_type->type_params[3])
+                    ) {
+                        $yield_from_type = $atomic_type->type_params[3];
+                    } elseif ($atomic_type instanceof TArray) {
+                        $yield_from_type = Type::getNull();
+                    } elseif ($atomic_type instanceof TKeyedArray) {
+                        $yield_from_type = Type::getNull();
+                    }
+                } else {
+                    $yield_from_type = Type::getMixed();
+                }
+            }
+
+            // this should be whatever the generator above returns, but *not* the return type
+            $statements_analyzer->node_data->setType($stmt, $yield_from_type ?: Type::getMixed());
         }
 
         $context->inside_call = $was_inside_call;
