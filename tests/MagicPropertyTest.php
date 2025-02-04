493ea0fd9957ebaa830e237f2054248839d83898
Daniil Gentili <daniil@daniil.it>
2025-02-04 11:59:04 +0100

    Fix #11098

--- tests/MagicPropertyTest.php
+++ tests/MagicPropertyTest.php
@@ -201,6 +201,23 @@ class MagicPropertyTest extends TestCase
                         }
                     }',
             ],
+            'magicGetterOnArrayValue' => [
+                'code' => '<?php
+                    /**
+                     * @property string $send_type
+                     * @psalm-seal-properties
+                     */
+                    final class b {
+                        public function __set(string $key, mixed $value) {}
+                    }
+
+                    /**
+                     * @param array<string, b> $objs
+                     */
+                    function test(string $k, array $objs): void {
+                        $objs[$k]->send_type = "test";
+                    }',
+            ],
             'propertyDocblockAssignmentToMixed' => [
                 'code' => '<?php
                     /**
