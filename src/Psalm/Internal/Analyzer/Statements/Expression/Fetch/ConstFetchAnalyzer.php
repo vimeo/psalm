63be24fc25d99155a6b068fc9e954f90b7677c9c
Daniil Gentili <daniil@daniil.it>
2025-02-06 21:40:34 +0100

    Bump descriptions

--- src/Psalm/Internal/Analyzer/Statements/Expression/Fetch/ConstFetchAnalyzer.php
+++ src/Psalm/Internal/Analyzer/Statements/Expression/Fetch/ConstFetchAnalyzer.php
@@ -106,7 +106,7 @@ final class ConstFetchAnalyzer
                 } elseif ($context->check_consts) {
                     IssueBuffer::maybeAdd(
                         new UndefinedConstant(
-                            'Const ' . $const_name . ' is not defined',
+                            'Const ' . $const_name . ' is not defined, consider enabling the allConstantsGlobal config option if scanning legacy codebases',
                             new CodeLocation($statements_analyzer->getSource(), $stmt),
                         ),
                         $statements_analyzer->getSuppressedIssues(),
