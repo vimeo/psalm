44e58e83d477b9d1db43f728d46e6241c8124148
Daniil Gentili <daniil@daniil.it>
2025-02-06 12:28:06 +0100

    Add test

--- tests/TypeCombinationTest.php
+++ tests/TypeCombinationTest.php
@@ -163,6 +163,21 @@ class TypeCombinationTest extends TestCase
                     '$x===' => 'list<non-empty-string>',
                 ],
             ],
+            'nonemptyliteralstring' => [
+                'code' => '<?php
+                    /** @var non-empty-literal-string */
+                    $instance = "test";
+                    $provider = "test";
+                    $key = "";
+                    if (random_int(0, 1)) {
+                        $key = "{$instance}_{$provider}";
+                        /** @psalm-check-type-exact $key=non-empty-literal-string */;
+                    }
+                    /** @psalm-check-type-exact $key=literal-string */;
+
+                    $_ = $key !== "" ? "test{$key}]" : "test";
+                ',
+            ]
         ];
     }
 
