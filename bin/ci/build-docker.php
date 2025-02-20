fde42c39953114ef291b18aa081303fe1beec5b2
Daniil Gentili <daniil@daniil.it>
2025-02-20 18:38:46 +0100

    Fix

---
+++ bin/ci/build-docker.php
@@ -0,0 +1,36 @@
+<?php // phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols, Generic.Files.LineLength.TooLong
+
+
+declare(strict_types=1);
+
+$commit = getenv('GITHUB_SHA');
+$ref = getenv('REF');
+$is_tag = getenv('EVENT_NAME') === 'release';
+
+echo "Waiting for commit $commit on $ref...".PHP_EOL;
+
+function r(string $cmd): void
+{
+    echo "> $cmd\n";
+    passthru($cmd);
+}
+
+$composer_branch = $is_tag ? $ref : "dev-$ref";
+
+$cur = 0;
+while (true) {
+    $json = json_decode(file_get_contents("https://repo.packagist.org/p/vimeo/psalm.json?v=$cur"), true);
+    if ($json["packages"]["vimeo/psalm"][$composer_branch]["source"]["reference"] === $commit) {
+        return;
+    }
+    sleep(1);
+    $cur++;
+}
+
+passthru("docker build . -t ghcr.io/vimeo/psalm:$branch --build-arg PSALM_REV=$compose_branch -f bin/docker/Dockerfile");
+passthru("docker push ghcr.io/vimeo/psalm:$branch");
+
+if ($is_tag) {
+    passthru("docker tag ghcr.io/vimeo/psalm:$branch ghcr.io/vimeo/psalm:latest");
+    passthru("docker push ghcr.io/vimeo/psalm:latest");
+}
