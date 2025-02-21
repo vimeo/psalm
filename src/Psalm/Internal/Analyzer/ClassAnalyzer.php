5bc56f8dc7669f9b5c0a95089b9d3086d9763832
Andrew Nagy <564256+tm1000@users.noreply.github.com>
2025-02-20 22:40:37 -0800

    styles

--- src/Psalm/Internal/Analyzer/ClassAnalyzer.php
+++ src/Psalm/Internal/Analyzer/ClassAnalyzer.php
@@ -773,7 +773,7 @@ final class ClassAnalyzer extends ClassLikeAnalyzer
                                 // If template_covariants is set template_types should also be set
                                 assert($parent_storage->template_types !== null);
                                 $pt_name = array_keys($parent_storage->template_types)[$pt_offset] ?? null;
-                                if($pt_name === null) {
+                                if ($pt_name === null) {
                                     continue;
                                 }
                                 if (isset($template_standins->lower_bounds[$pt_name][$parent_class])) {
