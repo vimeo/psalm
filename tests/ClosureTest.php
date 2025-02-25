9988d0dc70136bb59cc8175d100d36b62b24bec8
Daniil Gentili <daniil@daniil.it>
2025-02-25 10:28:10 +0100

    Add test

--- tests/ClosureTest.php
+++ tests/ClosureTest.php
@@ -601,6 +601,63 @@ final class ClosureTest extends TestCase
                     '$result' => 'list{stdClass}',
                 ],
             ],
+            'templateShenanigans' => [
+                'code' => '<?php
+                    class inner {}
+                    class b {
+                        public inner $key;
+                    }
+
+                    /**
+                     * @template-covariant TKey as array-key
+                     * @template TValue as b
+                     */
+                    class a {
+                        /**
+                         * @template TMappedValue
+                         *
+                         * @param (\Closure(TValue): TMappedValue)|true $callback Callback or null
+                         *
+                         * @return list<$callback is true ? array : TMappedValue>
+                         */
+                        public function toArray1(Closure|true $callback): array {
+                            return [];
+                        }
+                        /**
+                         * @template TMappedValue
+                         * @template T as (\Closure(TValue): TMappedValue)
+                         *
+                         * @param T $callback Callback or null
+                         *
+                         * @return list<TMappedValue>
+                         */
+                        public function toArray2(Closure $callback): array {
+                            return [];
+                        }
+                        /**
+                         * @template TMappedValue
+                         *
+                         * @param (\Closure(TValue): TMappedValue) $callback Callback or null
+                         *
+                         * @return list<TMappedValue>
+                         */
+                        public function toArray3(Closure $callback): array {
+                            return [];
+                        }
+                    }
+
+                    $a = (new a)->toArray1(static fn ($obj) => $obj->key);
+
+                    $b = (new a)->toArray2(static fn ($obj) => $obj->key);
+
+                    $c = (new a)->toArray3(static fn ($obj) => $obj->key);',
+
+                'assertions' => [
+                    '$a===' => 'list<inner>',
+                    '$b===' => 'list<inner>',
+                    '$c===' => 'list<inner>',
+                ],
+            ],
             'CallableWithArrayReduce' => [
                 'code' => '<?php
                     /**
