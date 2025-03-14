dc4da2e2c9d52ff2f645bb21a93c66652ae5cff5
Daniil Gentili <daniil@daniil.it>
2025-03-14 20:57:03 +0100

    Tmp

--- bin/ci/build-docker.php
+++ bin/ci/build-docker.php
@@ -53,7 +53,7 @@ $composer_branch = escapeshellarg($composer_branch);
 
 passthru("docker buildx build --push --platform linux/amd64,linux/arm64/v8 --cache-from ghcr.io/$user/psalm:$ref --cache-to type=inline . -t ghcr.io/$user/psalm:$ref --build-arg PSALM_REV=$composer_branch -f bin/docker/Dockerfile");
 
-if ($is_tag) {
+if ($is_tag || true) {
     passthru("docker tag ghcr.io/$user/psalm:$ref ghcr.io/$user/psalm:latest");
     passthru("docker push ghcr.io/$user/psalm:latest");
 }
