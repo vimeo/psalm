a9c0b40f0832af8e98e1f35c0dc7da81ef3aad5a
Daniil Gentili <daniil@daniil.it>
2025-03-16 20:21:17 +0100

    Finalize

--- bin/ci/push-docker.php
+++ bin/ci/push-docker.php
@@ -18,8 +18,8 @@ $is_tag = getenv('EVENT_NAME') === 'release';
 
 $ref = escapeshellarg($ref);
 
-r("docker pull ghcr.io/$user/psalm:$ref-arm64");
-r("docker pull ghcr.io/$user/psalm:$ref-amd64");
+r("docker pull ghcr.io/$user/psalm:$ref-arm64 --platform arm64");
+r("docker pull ghcr.io/$user/psalm:$ref-amd64 --platform amd64");
 
 r("docker buildx imagetools create -t ghcr.io/$user/psalm:$ref ghcr.io/$user/psalm:$ref-arm64 ghcr.io/$user/psalm:$ref-amd64");
 r("docker push ghcr.io/$user/psalm:$ref");
