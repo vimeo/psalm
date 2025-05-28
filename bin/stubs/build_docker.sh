db94d5583d9689ae87ad472714883ac87ff187e1
Daniil Gentili <daniil@daniil.it>
2025-05-28 10:51:12 +0200

    Skip cache

--- bin/stubs/build_docker.sh
+++ bin/stubs/build_docker.sh
@@ -4,7 +4,7 @@ VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4"
 
 for f in $VERSIONS; do
     {
-        docker buildx build --push --cache-to type=inline --cache-from type=registry,ref=ghcr.io/$ACTOR/psalm:internal_stubs_$f --build-arg VERSION=$f . -f bin/stubs/Dockerfile_$f -t ghcr.io/$ACTOR/psalm:internal_stubs_$f
+        docker buildx build --push --build-arg VERSION=$f . -f bin/stubs/Dockerfile_$f -t ghcr.io/$ACTOR/psalm:internal_stubs_$f
     } &
     if [ "$f" == "7.1" ] || [ "$f" == "7.3" ] || [ "$f" == "8.0" ] || [ "$f" == "8.2" ] || [ "$f" == "8.4" ]; then wait; fi
 done
