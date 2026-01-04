ea1ef20c0660e52ac11668733e876f305ad71df0
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2026-01-04 23:38:10 +0100

    Fix false positive PropertyNotSetInConstructor for overridden and stubbed properties/classes
    - Improve and deduplicate logic of previous PR https://github.com/vimeo/psalm/pull/10817
    - Fix https://github.com/vimeo/psalm/issues/11261
    - Fix https://github.com/vimeo/psalm/issues/2319 - for stubbed classes, which generally do not contain any statements, assume that when calling parent::__construct(); all inherited, non-initialized properties are intialized. This assumption makes sense, since otherwise it would mean the stubbed class itself has PropertyNotSetInConstructor errors

--- tests/PropertyTypeTest.php
+++ tests/PropertyTypeTest.php
@@ -607,6 +607,65 @@ final class PropertyTypeTest extends TestCase
                 'ignored_issues' => [],
                 'php_version' => '8.0',
             ],
+            'promotedPropertyOverwrittenNoConstructor' => [
+                'code' => '<?php
+                    class A
+                    {
+                        public function __construct(
+                            public array $abc,
+                        ) {}
+                    }
+
+                    class B extends A
+                    {
+                        public array $abc;
+                    }',
+                'assertions' => [],
+                'ignored_issues' => [],
+                'php_version' => '8.0',
+            ],
+            'promotedPropertyOverwrittenNoConstructorDeep' => [
+                'code' => '<?php
+                    class A
+                    {
+                        public function __construct(
+                            public array $abc,
+                        ) {}
+                    }
+
+                    class B extends A
+                    {
+                    }
+
+                    class C extends B
+                    {
+                        public array $abc;
+                    }',
+                'assertions' => [],
+                'ignored_issues' => [],
+                'php_version' => '8.0',
+            ],
+            'promotedPropertyOverwrittenNoConstructorDeep2' => [
+                'code' => '<?php
+                    class A
+                    {
+                        public function __construct(
+                            public array $abc,
+                        ) {}
+                    }
+
+                    class B extends A
+                    {
+                        public array $abc;
+                    }
+
+                    class C extends B
+                    {
+                    }',
+                'assertions' => [],
+                'ignored_issues' => [],
+                'php_version' => '8.0',
+            ],
             'propertyWithoutTypeSuppressingIssueAndAssertingNull' => [
                 'code' => '<?php
                     class A {
@@ -3606,6 +3665,25 @@ final class PropertyTypeTest extends TestCase
                     }
 
                     class B extends A
+                    {
+                        public function __construct() {}
+                    }',
+                'error_message' => 'PropertyNotSetInConstructor',
+                'ignored_issues' => [],
+                'php_version' => '8.0',
+            ],
+            'promotedPropertyNotSetInExtendedConstructorDeep' => [
+                'code' => '<?php
+                    class A
+                    {
+                        public function __construct(
+                            public string $name,
+                        ) {}
+                    }
+
+                    class B extends A {}
+
+                    class C extends B
                     {
                         public function __construct() {}
                     }',
