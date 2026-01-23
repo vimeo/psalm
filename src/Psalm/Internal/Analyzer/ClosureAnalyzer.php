3f9617b1f44e1648555c80668a5c29c5b3b34106
Daniil Gentili <daniil@daniil.it>
2026-01-23 21:44:13 +0100

    fixup

--- src/Psalm/Internal/Analyzer/ClosureAnalyzer.php
+++ src/Psalm/Internal/Analyzer/ClosureAnalyzer.php
@@ -11,6 +11,7 @@ use Psalm\Context;
 use Psalm\Internal\DataFlow\DataFlowNode;
 use Psalm\Internal\PhpVisitor\ShortClosureVisitor;
 use Psalm\Issue\DuplicateParam;
+use Psalm\Issue\ImpureFunctionCall;
 use Psalm\Issue\PossiblyUndefinedVariable;
 use Psalm\Issue\UndefinedVariable;
 use Psalm\IssueBuffer;
@@ -211,11 +212,13 @@ final class ClosureAnalyzer extends FunctionLikeAnalyzer
             $context->vars_in_scope[$key] = $value;
         }
 
-        if ($closure_analyzer->inferred_mutations !== Mutations::NONE
-            && $statements_analyzer->getSource() instanceof FunctionLikeAnalyzer
-        ) {
-            $statements_analyzer->getSource()->inferred_mutations = $closure_analyzer->inferred_mutations;
-        }
+        $statements_analyzer->signalMutation(
+            $closure_analyzer->inferred_mutations,
+            $context,
+            'closure',
+            ImpureFunctionCall::class,
+            $stmt,
+        );
 
         if (!$statements_analyzer->node_data->getType($stmt)) {
             $statements_analyzer->node_data->setType($stmt, Type::getClosure());
