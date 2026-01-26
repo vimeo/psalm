e98565e9000cc687092eb62da4f29e762d4809c6
Daniil Gentili <daniil@daniil.it>
2026-01-26 21:46:05 +0100

    Minor property hook improvements

--- src/Psalm/Storage/PropertyStorage.php
+++ src/Psalm/Storage/PropertyStorage.php
@@ -67,10 +67,8 @@ final class PropertyStorage implements HasAttributesInterface
 
     public ?string $description = null;
 
-    /**
-     * @var array<string, PropertyHookStorage>
-     */
-    public array $hooks = [];
+    public ?PropertyHookStorage $hook_get = null;
+    public ?PropertyHookStorage $hook_set = null;
 
     public function getInfo(): string
     {
