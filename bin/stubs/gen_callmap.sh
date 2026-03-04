582979a9332f99ecab305bff822defdc979e83d6
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2026-03-04 04:51:04 +0100

    Improve callmap generator scripts
    * Speed up building of docker by always running 2 versions at the same time, instead of in batches of 2
    * Remove hardcoded PHP versions for docker and stubs scripts and dynamically load the supported PHP versions from available dockerfiles
    * Add option to build docker images locally without pushing to Github container registry
    * Add option to rebuild only specified docker image, only missing docker images or all docker images
    * avoid downloading the same container for autogen callmap and final callmap
    * use local container if remote does not exist and build container locally if not available locally yet for a version
    * install composer if vendor does not exist to allow quick, standalone recreation of stubs in CI or other machine
    * fix callmap could not be generated due to wrong working directory

--- bin/stubs/gen_callmap.sh
+++ bin/stubs/gen_callmap.sh
@@ -1,14 +1,36 @@
 #!/bin/bash -e
 
-VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4 8.5"
-LAST="${VERSIONS##* }"
+ACTOR="${1:-danog}"
 
-for f in $VERSIONS; do
-    MSYS_NO_PATHCONV=1 docker run --pull always --quiet --platform linux/amd64 --rm -v "${PWD}:/app" "ghcr.io/danog/psalm:internal_stubs_${f}" php /app/bin/stubs/gen_base_callmap.php "$f" &
-
-    if [[ "$f" == "$LAST" ]]; then
-        wait
-
-        MSYS_NO_PATHCONV=1 docker run --pull always --quiet --platform linux/amd64 --rm -v "${PWD}:/app" "ghcr.io/danog/psalm:internal_stubs_${f}" php /app/bin/stubs/gen_callmap.php
+for file in bin/stubs/Dockerfile_*; do
+    # if none found - use shopt -s nullglob alternatively
+    if [[ ! -e "$file" ]]; then
+        break
     fi
+
+    f="${file#bin/stubs/Dockerfile_}"
+    echo "Starting ${f}"
+
+    pull="pull"
+    image="ghcr.io/${ACTOR}/psalm:internal_stubs_${f}"
+    if [[ "$ACTOR" == local ]] || ! docker manifest inspect "$image" > /dev/null 2>&1; then
+        # check locally
+        pull=""
+        image="psalm:internal_stubs_${f}"
+        if ! docker image inspect "$image" > /dev/null 2>&1; then
+            bash bin/stubs/build_docker.sh local "$f"
+        fi
+    fi
+
+    MSYS_NO_PATHCONV=1 docker run ${pull:+"--pull" always} --quiet --platform linux/amd64 --rm -v "${PWD}:/app" "$image" php /app/bin/stubs/gen_base_callmap.php "$f" &
 done
+
+wait
+
+if [[ ! -d vendor ]]
+then
+    MSYS_NO_PATHCONV=1 docker run --pull always --quiet --platform linux/amd64 --rm -v "${PWD}:/app" -w /app composer:latest composer install --no-progress
+fi
+
+# don't --pull but reuse the image we just downloaded
+MSYS_NO_PATHCONV=1 docker run --quiet --platform linux/amd64 --rm -v "${PWD}:/app" -w /app "$image" php /app/bin/stubs/gen_callmap.php
