b55cb9adc7edf52ec147ef1605d46485a51bbda2
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2026-03-04 00:00:24 +0000

    generate the final callmap in docker too to make it independent of a local PHP

--- bin/stubs/gen_callmap.sh
+++ bin/stubs/gen_callmap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
 VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4 8.5"
-
+LAST="${VERSIONS##* }"
 
 for f in $VERSIONS; do
     docker run --pull always --platform linux/amd64 --rm -v $PWD:/app ghcr.io/danog/psalm:internal_stubs_$f php /app/bin/stubs/gen_base_callmap.php "$f" &
+
+    if [[ "$f" == "$LAST" ]]; then
+        wait
+
+        docker run --pull always --platform linux/amd64 --rm -v $PWD:/app ghcr.io/danog/psalm:internal_stubs_$f php /app/bin/stubs/gen_callmap.php
+    fi
 done
-
-wait
-
-php bin/stubs/gen_callmap.php
